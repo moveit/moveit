c14c227f1e0a032851cdd4f3f35ffac83970562c
Ioan Sucan <isucan@willowgarage.com>
2011-10-17 21:16:56 -0700

    stub functions that will be needed later

--- ompl_interface/include/ompl_interface/state_space.h
+++ ompl_interface/include/ompl_interface/state_space.h
@@ -45,33 +45,75 @@
 namespace ompl_interface
 {
     
+    /// Construction of OMPL state space from kinematic joints
     class KMStateSpace
     {
     public:
+	
+	/// Construct the OMPL state space that corresponds to a set of joints. Record all the constructed spaces in the collection \e ssc
 	KMStateSpace(ompl::StateSpaceCollection &ssc, const std::vector<const planning_models::KinematicModel::JointModel*> &joints);
 	
+	/// Get the constructed OMPL state space
 	const ompl::base::StateSpacePtr& getOMPLSpace(void) const;
 	
-	void copyToKinematicState(const std::vector<planning_models::KinematicState::JointState*> &js, const ompl::base::State *state);
+	/// Copy the data from a set of joint states to an OMPL state. The join states \b must be specified in the same order as the joint models in the constructor
+	void copyToKinematicState(const std::vector<planning_models::KinematicState::JointState*> &js, const ompl::base::State *state) const;
 	
-	void copyToOMPLState(ompl::base::State *state, const std::vector<const planning_models::KinematicState::JointState*> &js);
+	/// Copy the data from an OMPL state to a set of joint states. The join states \b must be specified in the same order as the joint models in the constructor
+	void copyToOMPLState(ompl::base::State *state, const std::vector<const planning_models::KinematicState::JointState*> &js) const;
 	
-	double* getOMPLStateValueAddress(const std::string &joint_name, ompl::base::State *state);
-	const double* getOMPLStateValueAddress(const std::string &joint_name, const ompl::base::State *state);
+	/// Get the double value that corresponds to a joint name, directly from an OMPL state
+	double* getOMPLStateValueAddress(const std::string &joint_name, ompl::base::State *state) const;
+
+	/// Get the double value that corresponds to a joint name, directly from an OMPL state
+	const double* getOMPLStateValueAddress(const std::string &joint_name, const ompl::base::State *state) const;
 	
+	const std::vector<const planning_models::KinematicModel::JointModel*> getJointModels(void) const
+	{
+	    return joints_;
+	}
+	
+	/// Set the planning volume for the SE2 and/or SE3 components of the state space
 	void setPlanningVolume(double minX, double maxX, double minY, double maxY, double minZ, double maxZ);
     
     private:
 	
 	void constructSpace(ompl::StateSpaceCollection &ssc, const std::vector<const planning_models::KinematicModel::JointModel*> &joints);
 	
+	/// The sequence of joint models that make up the state space
 	std::vector<const planning_models::KinematicModel::JointModel*> joints_;
+
+	/// The order in which the joints were used to construct the OMPL state space
 	std::vector<std::size_t>                                        joint_mapping_;	   
+	
+	/// The state space that corresponds to the specified joints
 	ompl::base::StateSpacePtr                                       space_;
+
+	/// Keep the subspace components of the state space around for convenience
 	std::vector<ompl::base::StateSpacePtr>                          all_components_;
+
+	/// A simple tool that allows fast addressing of state values by name
 	ompl::StateAddress                                              state_address_;
     };
     
+    /*
+    class KinematicStateConvertor
+    {
+    public:
+	KinematicStateConvertor(const KMStateSpace &kss, const planning_models::KinematicState &full_state) : kss_(kss), full_state_(full_state)
+	{
+	}
+	
+	const planning_models::KinematicState& convertToKinematicState(const ompl::base::State *state) const;
+	
+    private:
+
+	const KMStateSpace                                                     &kss_;	
+	mutable std::map<boost::thread::id, planning_models::KinematicStatePtr> states_;
+	mutable boost::mutex                                                    lock_;
+    };
+    */
+	
 }
 
 #endif
