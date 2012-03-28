5c7e19ab62e1486a34b1e09abf6efa4d5ab4eb3a
Ioan Sucan <isucan@willowgarage.com>
2012-03-28 13:02:07 +0300

    script to set config vars

---
+++ moveit_warehouse/src/db_path_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# This is a simple ROS node used only to configure the param server
+# This is used in conjunction with ROS_HOME & roslaunch
+# to set the default path for database storage
+
+import roslib; roslib.load_manifest('moveit_warehouse')
+import rospy
+import os
+
+if __name__ == '__main__':
+    rospy.init_node('moveit_warehouse')
+    path_base = os.getcwd() + "/moveit_warehouse/"
+    rospy.set_param('~database_path_base', path_base)
+    rospy.set_param('~default_database', path_base + "default")
