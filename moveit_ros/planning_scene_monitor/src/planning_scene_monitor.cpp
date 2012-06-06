11511101f295bac916193a6e38f70222b371effb
Ioan Sucan <isucan@willowgarage.com>
2012-06-06 16:50:53 -0700

    no longer using deprecated function

--- moveit_ros/planning_scene_monitor/src/planning_scene_monitor.cpp
+++ moveit_ros/planning_scene_monitor/src/planning_scene_monitor.cpp
@@ -500,9 +500,9 @@ void planning_scene_monitor::PlanningSceneMonitor::configureDefaultCollisionMatr
                scene_->getKinematicModel()->getLinkModelNamesWithCollisionGeometry(), false);
   
   // allow collisions for pairs that have been disabled
-  const std::vector<std::pair<std::string, std::string> >&dc = scene_->getSrdfModel()->getDisabledCollisions();
+  const std::vector<srdf::Model::DisabledCollision> &dc = scene_->getSrdfModel()->getDisabledCollisionPairs();
   for (std::size_t i = 0 ; i < dc.size() ; ++i)
-    acm.setEntry(dc[i].first, dc[i].second, true);
+    acm.setEntry(dc[i].link1_, dc[i].link2_, true);
   
   // read overriding values from the param server
   
