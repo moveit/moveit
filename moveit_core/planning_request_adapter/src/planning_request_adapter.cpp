d202ccea482c48a00f976c626bdd6da19e7f8c90
Ioan Sucan <isucan@willowgarage.com>
2012-06-19 14:24:11 -0700

    move code to trajectory_processing

--- moveit_core/planning_request_adapter/src/planning_request_adapter.cpp
+++ moveit_core/planning_request_adapter/src/planning_request_adapter.cpp
@@ -35,7 +35,6 @@
 #include <planning_request_adapter/planning_request_adapter.h>
 #include <planning_models/conversions.h>
 #include <boost/bind.hpp>
-#include <ros/console.h>
 
 namespace planning_request_adapter
 {
@@ -54,91 +53,6 @@ bool planning_request_adapter::PlanningRequestAdapter::adaptAndPlan(const planni
                                                                     moveit_msgs::GetMotionPlan::Response &res) const
 {
   return adaptAndPlan(boost::bind(&callPlannerInterfaceSolve, planner.get(), _1, _2, _3), planning_scene, req, res);
-}
-
-void planning_request_adapter::PlanningRequestAdapter::addPrefixState(const planning_models::KinematicState &prefix, moveit_msgs::GetMotionPlan::Response &res,
-                                                                      double dt_offset, const planning_models::TransformsConstPtr &transforms) const
-{
-  planning_models::kinematicStateToRobotState(prefix, res.trajectory_start);
-  ros::Duration dt(dt_offset);
-  
-  if (!res.trajectory.joint_trajectory.points.empty() && !res.trajectory.joint_trajectory.joint_names.empty())
-  {
-    trajectory_msgs::JointTrajectoryPoint new_start = res.trajectory.joint_trajectory.points.front();
-    std::vector<double> vals;
-    for (std::size_t i = 0 ; i < res.trajectory.joint_trajectory.joint_names.size() ; ++i)
-    {
-      const planning_models::KinematicState::JointState *js = prefix.getJointState(res.trajectory.joint_trajectory.joint_names[i]);
-      if (!js)
-        break;
-      if (js->getVariableValues().size() != 1)
-      {
-        ROS_ERROR("Unexpected number of joint values. Got %u when 1 should have been found", (unsigned int)js->getVariableValues().size());
-        break;
-      }
-      vals.push_back(js->getVariableValues()[0]);
-    }
-    
-    if (vals.size() == new_start.positions.size())
-    {
-      new_start.positions = vals;
-      
-      //insert extra point at the start of trajectory
-      res.trajectory.joint_trajectory.points.insert(res.trajectory.joint_trajectory.points.begin(), new_start);
-      
-      // add duration offset for the trajectory
-      for (std::size_t j = 1 ; j < res.trajectory.joint_trajectory.points.size() ; ++j)
-        res.trajectory.joint_trajectory.points[j].time_from_start += dt;
-    }
-    else
-      ROS_ERROR("The number of joints in the solution reported by the planner does not match with the known set of joints");
-  }
-  
-  if (!res.trajectory.multi_dof_joint_trajectory.points.empty() && !res.trajectory.multi_dof_joint_trajectory.joint_names.empty())
-  {
-    moveit_msgs::MultiDOFJointTrajectoryPoint new_start = res.trajectory.multi_dof_joint_trajectory.points.front();
-    std::vector<Eigen::Affine3d> poses;
-    for (std::size_t i = 0 ; i < res.trajectory.multi_dof_joint_trajectory.joint_names.size() ; ++i)
-    {
-      const planning_models::KinematicState::JointState *js = prefix.getJointState(res.trajectory.multi_dof_joint_trajectory.joint_names[i]);
-      if (!js)
-        break;
-      if (res.trajectory.multi_dof_joint_trajectory.child_frame_ids.size() <= i ||
-          js->getJointModel()->getChildLinkModel()->getName() != res.trajectory.multi_dof_joint_trajectory.child_frame_ids[i])
-      {
-        ROS_ERROR("Unmatched multi-dof joint: '%s'", js->getJointModel()->getChildLinkModel()->getName().c_str());
-        break;
-      }
-      const Eigen::Affine3d& t = js->getVariableTransform();
-      if (res.trajectory.multi_dof_joint_trajectory.frame_ids.size() >= i &&
-          res.trajectory.multi_dof_joint_trajectory.frame_ids[i] != prefix.getKinematicModel()->getModelFrame())
-      {
-        if (transforms)
-          poses.push_back(transforms->getTransform(prefix, res.trajectory.multi_dof_joint_trajectory.frame_ids[i]) * t);
-        else
-        {
-          ROS_ERROR("Transform to frame '%s' is not known", res.trajectory.multi_dof_joint_trajectory.frame_ids[i].c_str());
-          break;
-        }
-      }
-      else
-        poses.push_back(t);
-    }
-    if (poses.size() == new_start.poses.size())
-    {
-      for (std::size_t i = 0 ; i < poses.size() ; ++i)
-        planning_models::msgFromPose(poses[i], new_start.poses[i]);
-      
-      //insert extra point at the start of trajectory
-      res.trajectory.multi_dof_joint_trajectory.points.insert(res.trajectory.multi_dof_joint_trajectory.points.begin(), new_start);
-      
-      // add duration offset for the trajectory
-      for (std::size_t j = 1 ; j < res.trajectory.joint_trajectory.points.size() ; ++j)
-        res.trajectory.multi_dof_joint_trajectory.points[j].time_from_start += dt;
-    }      
-    else
-      ROS_ERROR("The number of mulit-dof joints in the solution reported by the planner does not match with the known set of joints");
-  }
 }
 
 namespace planning_request_adapter
@@ -171,10 +85,13 @@ bool planning_request_adapter::PlanningRequestAdapterChain::adaptAndPlan(const p
                                                                          const moveit_msgs::GetMotionPlan::Request &req, 
                                                                          moveit_msgs::GetMotionPlan::Response &res) const
 {
+  // if there are no adapters, run the planner directly 
   if (adapters_.empty())
     return planner->solve(planning_scene, req, res);
   else
   {
+    // if there are adapters, construct a function pointer for each, in order,
+    // so that in the end we have a nested sequence of function pointers that call the adapters in the correct order.
     PlannerFn fn = boost::bind(&callAdapter1, adapters_.back().get(), planner, _1, _2, _3);
     for (int i = adapters_.size() - 2 ; i >= 0 ; --i)
       fn = boost::bind(&callAdapter2, adapters_[i].get(), fn, _1, _2, _3);
