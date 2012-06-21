bc3d046e5db49bf340ffa481d69ccf8c7c09f43f
Ioan Sucan <isucan@willowgarage.com>
2012-06-20 22:32:20 -0700

    add implementation of simple wrapper for MoveGroup

---
+++ moveit_ros/move_group_interface/include/move_group_interface/move_group.h
@@ -0,0 +1,123 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2012, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Ioan Sucan */
+
+#ifndef MOVEIT_MOVE_GROUP_INTERFACE_MOVE_GROUP_
+#define MOVEIT_MOVE_GROUP_INTERFACE_MOVE_GROUP_
+
+#include <planning_models/kinematic_state.h>
+#include <moveit_msgs/RobotTrajectory.h>
+#include <moveit_msgs/RobotState.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <boost/shared_ptr.hpp>
+#include <tf/tf.h>
+
+namespace move_group_interface
+{
+
+class MoveGroup
+{
+public:
+  
+  static const std::string ROBOT_DESCRIPTION;
+  
+  struct Options
+  {
+    Options(const std::string &group_name) : group_name_(group_name),
+                                             robot_description_(ROBOT_DESCRIPTION)
+    {
+    }
+    std::string group_name_;
+    std::string robot_description_;
+  };
+  
+  struct Plan
+  {
+    moveit_msgs::RobotState start_state_;
+    moveit_msgs::RobotTrajectory trajectory_;
+  };
+  
+  MoveGroup(const Options &opt, const boost::shared_ptr<tf::Transformer> &tf = boost::shared_ptr<tf::Transformer>()); 
+  MoveGroup(const std::string &group, const boost::shared_ptr<tf::Transformer> &tf = boost::shared_ptr<tf::Transformer>());
+
+  ~MoveGroup(void);
+  
+  bool move(void);
+  
+  bool plan(Plan &plan);
+  
+  void setJointValueTarget(const std::vector<double> &group_variable_values);
+
+  void setJointValueTarget(const std::map<std::string, double> &variable_values);
+
+  void setJointValueTarget(const planning_models::KinematicState &kinematic_state);
+
+  void setJointValueTarget(const planning_models::KinematicState::JointStateGroup &joint_state_group);
+  
+  void setJointValueTarget(const planning_models::KinematicState::JointState &joint_state);
+
+  void setJointValueTarget(const std::string &joint_name, const std::vector<double> &values);
+
+  void setJointValueTarget(const std::string &joint_name, double value);
+  
+  void setJointValueTarget(const sensor_msgs::JointState &state);
+
+  void setPoseTarget(const Eigen::Affine3d &end_effector_pose);
+  
+  void setPoseTarget(const geometry_msgs::Pose &target);
+
+  void setPoseTarget(const geometry_msgs::PoseStamped &target);
+
+  void setPoseReferenceFrame(const std::string &pose_reference_frame);
+
+  const std::string& getPoseReferenceFrame(void) const;
+
+  void setEndEffectorLink(const std::string &link_name);
+
+  const std::string& getEndEffectorLink(void) const;
+
+  void setRandomTarget(void);
+  
+  void setNamedTarget(const std::string &name);
+  
+private:
+  
+  class MoveGroupImpl;
+  MoveGroupImpl *impl_;
+  
+};
+
+}
+#endif
---
+++ moveit_ros/move_group_interface/src/move_group.cpp
@@ -0,0 +1,365 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2012, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Ioan Sucan */
+
+#include <stdexcept>
+#include "move_group_interface/move_group.h"
+#include <planning_models_loader/kinematic_model_loader.h>
+#include <planning_scene_monitor/current_state_monitor.h>
+#include <moveit_msgs/MoveGroupAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <kinematic_constraints/utils.h>
+#include <eigen_conversions/eigen_msg.h>
+#include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
+#include <ros/console.h>
+
+namespace move_group_interface
+{
+
+const std::string MoveGroup::ROBOT_DESCRIPTION = "robot_description";    // name of the robot description (a param name, so it can be changed externally)
+
+class MoveGroup::MoveGroupImpl
+{
+public: 
+  
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  MoveGroupImpl(const Options &opt, const boost::shared_ptr<tf::Transformer> &tf) : opt_(opt), tf_(tf)
+  {
+    action_client_.reset(new actionlib::SimpleActionClient<moveit_msgs::MoveGroupAction>("move_group", false));
+    planning_models_loader::KinematicModelLoader::Options km_opt(opt.robot_description_);
+    km_opt.load_kinematics_solvers_ = false;
+    kinematic_model_loader_.reset(new planning_models_loader::KinematicModelLoader(km_opt));
+    if (!getKinematicModel())
+      ROS_FATAL_STREAM("Unable to construct robot model. Make sure all needed information is on the parameter server.");
+    if (!getKinematicModel()->hasJointModelGroup(opt.group_name_))
+      ROS_FATAL_STREAM("Group '" + opt.group_name_ + "' was not found.");
+    
+    joint_state_target_.reset(new planning_models::KinematicState(getKinematicModel()));
+    joint_state_target_->setToDefaultValues();
+    use_joint_state_target_ = true;
+    
+    planning_models::KinematicModel::JointModelGroup *joint_model_group = getKinematicModel()->getJointModelGroup(opt.group_name_);
+    if (joint_model_group->isChain())
+      end_effector_ = joint_model_group->getLinkModelNames().back();
+    pose_target_.setIdentity();
+    pose_reference_frame_ = getKinematicModel()->getModelFrame();
+    
+    current_state_monitor_.reset(new planning_scene_monitor::CurrentStateMonitor(kinematic_model_loader_->getModel(), tf_));
+    action_client_->waitForServer();
+    ROS_INFO_STREAM("Ready to take MoveGroup commands for group " << opt.group_name_ << ".");
+  }
+
+  const boost::shared_ptr<tf::Transformer>& getTF(void) const
+  {
+    return tf_;
+  }
+  
+  const Options& getOptions(void) const
+  {
+    return opt_;
+  }
+  
+  const planning_models::KinematicModelPtr& getKinematicModel(void)
+  {
+    return kinematic_model_loader_->getModel();
+  }
+  
+  planning_models::KinematicState::JointStateGroup* getJointStateTarget(void)
+  {
+    return joint_state_target_->getJointStateGroup(opt_.group_name_);
+  }
+  
+  void setEndEffector(const std::string &end_effector)
+  {
+    if (getKinematicModel()->getJointModelGroup(opt_.group_name_)->hasLinkModel(end_effector))
+      end_effector_ = end_effector;
+    else
+      ROS_ERROR_STREAM("Link '" << end_effector << "' does not belong to group " << opt_.group_name_);
+  }
+  
+  const std::string &getEndEffector(void) const
+  {
+    return end_effector_;
+  }
+  
+  void setPoseTarget(const Eigen::Affine3d &eef_pose)
+  {
+    pose_target_ = eef_pose;
+  }
+  
+  const Eigen::Affine3d& getPoseTarget(void) const
+  {
+    return pose_target_;
+  }
+  
+  void setPoseReferenceFrame(const std::string &pose_reference_frame)
+  {
+    pose_reference_frame_ = pose_reference_frame;
+  }
+  
+  const std::string& getPoseReferenceFrame(void) const
+  {
+    return pose_reference_frame_;
+  }
+  
+  void useJointStateTarget(void)
+  {
+    use_joint_state_target_ = true;
+  }
+  
+  void usePoseTarget(void)
+  {
+    use_joint_state_target_ = false;
+  }
+
+  bool plan(Plan &plan)
+  {
+    moveit_msgs::MoveGroupGoal goal;
+    constructGoal(goal);
+    goal.plan_only = true;
+    action_client_->sendGoal(goal); 
+    if (!action_client_->waitForResult())
+    {
+      ROS_INFO_STREAM("MoveGroup action returned early");
+    }
+    if (action_client_->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      plan.trajectory_ = action_client_->getResult()->planned_trajectory;
+      plan.start_state_ = action_client_->getResult()->trajectory_start;
+      return true;
+    }
+    else
+    {
+      ROS_WARN_STREAM("Fail: " << action_client_->getState().toString() << ": " << action_client_->getState().getText());
+      return false;
+    }
+  }
+  
+  bool move(void)
+  {
+    moveit_msgs::MoveGroupGoal goal;
+    constructGoal(goal);
+    goal.plan_only = false;
+    action_client_->sendGoal(goal);
+    if (!action_client_->waitForResult())
+    {
+      ROS_INFO_STREAM("MoveGroup action returned early");
+    }
+    if (action_client_->getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      return true;
+    else
+    {
+      ROS_WARN_STREAM("Fail: " << action_client_->getState().toString() << ": " << action_client_->getState().getText());
+      return false;
+    }
+  }
+  
+  void constructGoal(moveit_msgs::MoveGroupGoal &goal)
+  {
+    goal.request.group_name = opt_.group_name_;
+    goal.request.num_planning_attempts = 1;
+    goal.request.allowed_planning_time = ros::Duration(5.0);
+
+    if (use_joint_state_target_)
+    {    
+      goal.request.goal_constraints.resize(1);
+      goal.request.goal_constraints[0] = kinematic_constraints::constructGoalConstraints(getJointStateTarget());
+    }
+    else
+    {
+      geometry_msgs::PoseStamped pose;
+      tf::poseEigenToMsg(pose_target_, pose.pose);
+      pose.header.frame_id = pose_reference_frame_;
+      pose.header.stamp = ros::Time::now();
+      goal.request.goal_constraints.resize(1);
+      goal.request.goal_constraints[0] = kinematic_constraints::constructGoalConstraints(end_effector_, pose);
+    }    
+  }
+  
+private:
+  
+  Options opt_;
+  planning_models_loader::KinematicModelLoaderPtr kinematic_model_loader_;
+  planning_scene_monitor::CurrentStateMonitorPtr current_state_monitor_;
+  boost::scoped_ptr<actionlib::SimpleActionClient<moveit_msgs::MoveGroupAction> > action_client_;
+  boost::shared_ptr<tf::Transformer> tf_;
+  
+  planning_models::KinematicStatePtr joint_state_target_;
+  Eigen::Affine3d pose_target_;
+  std::string end_effector_;
+  std::string pose_reference_frame_;
+  
+  bool use_joint_state_target_;
+};
+
+MoveGroup::MoveGroup(const std::string &group_name, const boost::shared_ptr<tf::Transformer> &tf)
+{  
+  impl_ = new MoveGroupImpl(Options(group_name), tf ? tf : boost::shared_ptr<tf::Transformer>(new tf::TransformListener()));
+}
+
+MoveGroup::MoveGroup(const Options &opt, const boost::shared_ptr<tf::Transformer> &tf)
+{
+  impl_ = new MoveGroupImpl(opt, tf ? tf : boost::shared_ptr<tf::Transformer>(new tf::TransformListener()));
+}
+
+MoveGroup::~MoveGroup(void)
+{
+  delete impl_;
+}
+
+bool MoveGroup::move(void)
+{
+  return impl_->move();
+}
+
+bool MoveGroup::plan(Plan &plan)
+{
+  return impl_->plan(plan);
+}
+
+void MoveGroup::setRandomTarget(void)
+{ 
+  impl_->getJointStateTarget()->setToRandomValues();
+  impl_->useJointStateTarget();
+}
+
+void MoveGroup::setNamedTarget(const std::string &name)
+{ 
+  impl_->getJointStateTarget()->setToDefaultState(name);
+  impl_->useJointStateTarget();
+}
+
+void MoveGroup::setJointValueTarget(const std::vector<double> &joint_values)
+{
+  impl_->getJointStateTarget()->setStateValues(joint_values);
+  impl_->useJointStateTarget();
+}
+
+void MoveGroup::setJointValueTarget(const std::map<std::string, double> &joint_values)
+{
+  impl_->getJointStateTarget()->setStateValues(joint_values);
+  impl_->useJointStateTarget();
+}
+
+void MoveGroup::setJointValueTarget(const planning_models::KinematicState &kinematic_state)
+{
+  setJointValueTarget(*kinematic_state.getJointStateGroup(impl_->getOptions().group_name_));
+}
+
+void MoveGroup::setJointValueTarget(const planning_models::KinematicState::JointStateGroup &joint_state_group)
+{  
+  std::map<std::string, double> variable_values;
+  joint_state_group.getGroupStateValues(variable_values);
+  setJointValueTarget(variable_values);
+}
+
+void MoveGroup::setJointValueTarget(const planning_models::KinematicState::JointState &joint_state)
+{
+  setJointValueTarget(joint_state.getName(), joint_state.getVariableValues());
+}
+
+void MoveGroup::setJointValueTarget(const std::string &joint_name, double value)
+{ 
+  std::vector<double> values(1, value);
+  setJointValueTarget(joint_name, values);
+}
+
+void MoveGroup::setJointValueTarget(const std::string &joint_name, const std::vector<double> &values)
+{ 
+  planning_models::KinematicState::JointState *joint_state = impl_->getJointStateTarget()->getJointState(joint_name);
+  if (joint_state)
+    if (!joint_state->setVariableValues(values))
+      ROS_ERROR("Unable to set target");
+  impl_->useJointStateTarget();
+}
+
+void MoveGroup::setJointValueTarget(const sensor_msgs::JointState &state)
+{
+  impl_->getJointStateTarget()->setStateValues(state); 
+  impl_->useJointStateTarget();
+}
+
+void MoveGroup::setPoseTarget(const Eigen::Affine3d &pose)
+{
+  impl_->setPoseTarget(pose);
+  impl_->usePoseTarget();
+}
+
+const std::string& MoveGroup::getEndEffectorLink(void) const
+{
+  return impl_->getEndEffector();
+}
+
+void MoveGroup::setEndEffectorLink(const std::string &link_name)
+{
+  impl_->setEndEffector(link_name);  
+  impl_->usePoseTarget();
+}
+
+void MoveGroup::setPoseTarget(const geometry_msgs::Pose &target)
+{
+  Eigen::Affine3d m;
+  tf::poseMsgToEigen(target, m);
+  setPoseTarget(m);
+}
+
+void MoveGroup::setPoseTarget(const geometry_msgs::PoseStamped &target)
+{
+  // avoid using the TransformListener API, since out pointer is for the base class only;
+  tf::Pose pose;
+  tf::poseMsgToTF(target.pose, pose);
+  tf::Stamped<tf::Pose> stamped_pose(pose, target.header.stamp, target.header.frame_id);
+  tf::Stamped<tf::Pose> stamped_pose_out;
+  impl_->getTF()->transformPose(impl_->getPoseReferenceFrame(), stamped_pose, stamped_pose_out);
+
+  geometry_msgs::Pose pose_out;
+  tf::poseTFToMsg(stamped_pose_out, pose_out);
+  setPoseTarget(pose_out);
+}
+
+void MoveGroup::setPoseReferenceFrame(const std::string &pose_reference_frame)
+{
+  impl_->setPoseReferenceFrame(pose_reference_frame);
+}
+
+const std::string& MoveGroup::getPoseReferenceFrame(void) const
+{
+  return impl_->getPoseReferenceFrame();
+}
+
+
+}
