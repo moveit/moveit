9ea0ffc6c6008514d0bd2171688185c250eaf268
E. Gil Jones <gjones@willowgarage.com>
2012-06-26 11:59:01 -0700

    Adding ability to attach and detach objects, though detach currently segfaults

--- moveit_visualization_ros/include/moveit_visualization_ros/attach_object_addition_dialog.h
+++ moveit_visualization_ros/include/moveit_visualization_ros/attach_object_addition_dialog.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Willow Garage, Inc.
+ * Copyright (c) 2012, Willow Garage, Inc.
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -29,44 +29,50 @@
 
 // Author: E. Gil Jones
 
-#include <moveit_visualization_ros/interactive_object_visualization_qt_wrapper.h>
-#include <moveit_visualization_ros/qt_helper_functions.h>
+#ifndef _ATTACH_OBJECT_ADDITION_DIALOG_H_
+#define _ATTACH_OBJECT_ADDITION_DIALOG_H_
 
-namespace moveit_visualization_ros {
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QSpinBox>
+#include <QComboBox>
+#include <QListWidget>
+#include <planning_models/kinematic_model.h>
 
-void InteractiveObjectVisualizationQtWrapper::addCubeSignalled() {
-  ROS_INFO_STREAM("Trying to add");
-  addCube();
-}
-
-void InteractiveObjectVisualizationQtWrapper::callUpdateCallback() {
-  InteractiveObjectVisualization::callUpdateCallback();
-  updatePlanningSceneSignal(planning_scene_diff_);
-}
-
-void InteractiveObjectVisualizationQtWrapper::addCollisionObjectSignalled(const moveit_msgs::CollisionObject& obj,
-                                                                          const QColor& color)
+namespace moveit_visualization_ros
 {
-  addObject(obj,
-            convertQColorToRGBA(color));
-}
 
-void InteractiveObjectVisualizationQtWrapper::loadPlanningSceneSignalled(moveit_msgs::PlanningScenePtr planning_scene) {
-  updateOriginalPlanningScene(planning_scene);
-}
-
-void InteractiveObjectVisualizationQtWrapper::deleteSignalled(std::string name) {
-  deleteObject(name);
-}
-
-void InteractiveObjectVisualizationQtWrapper::attachCollisionObjectSignalled(const std::string& name,
-                                                                             const std::string& link_name,
-                                                                             const std::vector<std::string>& touch_links)
+class AttachObjectAdditionDialog: public QDialog
 {
-  attachObject(name,
-               link_name,
-               touch_links);
-}
+  Q_OBJECT
+
+  public:
+  
+  AttachObjectAdditionDialog(QWidget* parent,
+                             const planning_models::KinematicModelConstPtr& kmodel);
+public Q_SLOTS:
+
+  virtual void accept();
+  void attachObject(const std::string& name);
+  void addTouchLinkClicked();
+  void removeTouchLinkClicked();
+
+
+Q_SIGNALS:
+
+  void attachCollisionObjectRequested(const std::string& name,
+                                      const std::string& link_name,
+                                      const std::vector<std::string>& touch_links);
+protected:
+
+  std::string object_to_attach_;
+
+  QComboBox* attach_link_box_;
+  QListWidget* possible_touch_links_;
+  QListWidget* added_touch_links_;
+
+};
 
 }
-
+#endif
