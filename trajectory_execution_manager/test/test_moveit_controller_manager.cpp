ec6c8a87d3f4215832ad79e501dd2ef11bbd0aef
Ioan Sucan <isucan@willowgarage.com>
2012-07-04 12:51:44 -0700

    first take at a trajectory execution manager; needs more work

---
+++ trajectory_execution_manager/test/test_moveit_controller_manager.cpp
@@ -0,0 +1,172 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2012, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#include <moveit_controller_manager/moveit_controller_manager.h>
+
+namespace test_moveit_controller_manager
+{
+  
+class TestMoveItControllerHandle : public moveit_controller_manager::MoveItControllerHandle
+{
+public:
+  
+  TestMoveItControllerHandle(const std::string &name) : MoveItControllerHandle(name)
+  {
+  }
+
+  virtual bool sendTrajectory(const moveit_msgs::RobotTrajectory &trajectory)
+  {
+    return true;
+  }
+  
+  virtual bool cancelExecution(void)
+  {
+    return true;
+  }
+  
+  virtual void waitForExecution(void)
+  {
+  }
+  
+  virtual moveit_controller_manager::ExecutionStatus::Value getLastExecutionStatus(void)
+  {
+    return moveit_controller_manager::ExecutionStatus::SUCCEEDED;
+  }
+  
+};
+
+class TestMoveItControllerManager : public moveit_controller_manager::MoveItControllerManager
+{
+public:
+  static const int LOADED = 1;
+  static const int ACTIVE = 2;
+  
+  TestMoveItControllerManager(void)
+  {
+    controllers_["right_arm"] = 0;
+    controllers_["left_arm"] = ACTIVE;
+    controllers_["arms"] = LOADED;
+    controllers_["base"] = 0;
+    controllers_["head"] = 0;
+    controllers_["left_arm_head"] = LOADED;
+    
+    controller_joints_["righ_arm"].push_back("rj1");
+    controller_joints_["righ_arm"].push_back("rj2");
+
+    controller_joints_["left_arm"].push_back("lj1");
+    controller_joints_["left_arm"].push_back("lj2");
+    controller_joints_["left_arm"].push_back("lj3");
+
+    controller_joints_["arms"].insert(controller_joints_["arms"].end(), controller_joints_["left_arm"].begin(), controller_joints_["left_arm"].end());
+    controller_joints_["arms"].insert(controller_joints_["arms"].end(), controller_joints_["right_arm"].begin(), controller_joints_["right_arm"].end());
+    
+    controller_joints_["base"].push_back("basej");
+    controller_joints_["head"].push_back("headj");
+
+    controller_joints_["left_arm_head"].insert(controller_joints_["left_arm_head"].end(), controller_joints_["left_arm"].begin(), controller_joints_["left_arm"].end());
+    controller_joints_["left_arm_head"].insert(controller_joints_["left_arm_head"].end(), controller_joints_["head"].begin(), controller_joints_["head"].end());
+  }
+  
+  virtual moveit_controller_manager::MoveItControllerHandlePtr getControllerHandle(const std::string &name)
+  {
+    return moveit_controller_manager::MoveItControllerHandlePtr(new TestMoveItControllerHandle(name));
+  }
+  
+  virtual void getControllersList(std::vector<std::string> &names)
+  {
+    names.clear();
+    for (std::map<std::string, int>::const_iterator it = controllers_.begin() ; it != controllers_.end() ; ++it)
+      names.push_back(it->first);
+  }
+  
+  virtual void getActiveControllers(std::vector<std::string> &names)
+  {
+    names.clear();
+    for (std::map<std::string, int>::const_iterator it = controllers_.begin() ; it != controllers_.end() ; ++it)
+      if (it->second & ACTIVE)
+        names.push_back(it->first);
+  }
+  
+  virtual void getLoadedControllers(std::vector<std::string> &names)
+  {
+    names.clear();
+    for (std::map<std::string, int>::const_iterator it = controllers_.begin() ; it != controllers_.end() ; ++it)
+      if (it->second & LOADED)
+        names.push_back(it->first);
+  }
+  
+  virtual void getControllerJoints(const std::string &name, std::vector<std::string> &joints)
+  {
+    joints = controller_joints_[name];
+  }
+  
+  virtual moveit_controller_manager::MoveItControllerManager::ControllerState getControllerState(const std::string &name)
+  {
+    moveit_controller_manager::MoveItControllerManager::ControllerState state;
+    state.active_ = controllers_[name] & ACTIVE;
+    state.loaded_ = controllers_[name] & LOADED;
+    return state;
+  }
+  
+  /// Load a controller, but do not activate it by default
+  virtual bool loadController(const std::string &name)
+  {
+    controllers_[name] |= LOADED;
+    return true;
+  }
+  
+  virtual bool unloadController(const std::string &name)
+  {
+    controllers_[name] &= ~LOADED;
+    return true;
+  }
+  
+  virtual bool switchControllers(const std::vector<std::string> &load, const std::vector<std::string> &unload)
+  {
+    for (std::size_t i = 0 ; i < load.size() ; ++i)
+      loadController(load[i]);
+    for (std::size_t i = 0 ; i < unload.size() ; ++i)
+      unloadController(unload[i]);
+    return true;
+  }
+  
+protected:
+  
+  std::map<std::string, int> controllers_;
+  std::map<std::string, std::vector<std::string> > controller_joints_;
+  
+};
+
+
+}
