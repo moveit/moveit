141d46dc0447316a0e4bdb0297d3c79635ed7065
Adam Leeper <aleeper@willowgarage.com>
2012-10-25 22:21:29 -0700

    moves refineNormals to new file in collision_detection

--- collision_detection/include/collision_detection/collision_common.h
+++ collision_detection/include/collision_detection/collision_common.h
@@ -128,7 +128,7 @@ namespace collision_detection
       return aabb_min < other.aabb_min;
     }
   };
-  
+
   /** \brief Representation of a collision checking result */
   struct CollisionResult
   {
