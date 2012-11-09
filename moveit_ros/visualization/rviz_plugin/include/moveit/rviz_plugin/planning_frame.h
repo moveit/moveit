143990583c487dbc234f705d20d176843bdcd0e4
Mario Prats <mprats@willowgarage.com>
2012-11-09 09:28:04 -0800

    Ask the user whether to overwrite the object if it already exists

--- moveit_ros/visualization/rviz_plugin/include/moveit/rviz_plugin/planning_frame.h
+++ moveit_ros/visualization/rviz_plugin/include/moveit/rviz_plugin/planning_frame.h
@@ -149,6 +149,7 @@ private:
   void populateConstraintsList(void);
   void populateConstraintsList(const std::vector<std::string> &constr);
   void configureForPlanning(void);
+  void addObject(collision_detection::CollisionWorldPtr world, const std::string &id, const shapes::ShapeConstPtr &shape, const Eigen::Affine3d &pose);
   
   ros::NodeHandle nh_;
   ros::Publisher planning_scene_publisher_;
