ccdd970f20b973de63b4042986b2333079afd875
Ioan Sucan <isucan@willowgarage.com>
2012-12-28 20:16:43 +0200

    minor fix

--- kinematic_constraints/src/utils.cpp
+++ kinematic_constraints/src/utils.cpp
@@ -106,70 +106,70 @@ moveit_msgs::Constraints kinematic_constraints::mergeConstraints(const moveit_ms
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const kinematic_state::JointStateGroup *jsg,
                                                                          double tolerance)
 {
-    return constructGoalConstraints(jsg, tolerance, tolerance);
+  return constructGoalConstraints(jsg, tolerance, tolerance);
 }
 
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const kinematic_state::JointStateGroup *jsg,
                                                                          double tolerance_below, double tolerance_above)
 {
-    moveit_msgs::Constraints goal;
-
-    std::map<std::string, double> vals;
-    jsg->getVariableValues(vals);
-
-    goal.joint_constraints.resize(vals.size());
-    unsigned int i = 0;
-    for (std::map<std::string, double>::iterator it = vals.begin() ; it != vals.end(); ++it, ++i)
-    {
-        goal.joint_constraints[i].joint_name = it->first;
-        goal.joint_constraints[i].position = it->second;
-        goal.joint_constraints[i].tolerance_above = tolerance_below;
-        goal.joint_constraints[i].tolerance_below = tolerance_above;
-        goal.joint_constraints[i].weight = 1.0;
-    }
-
-    return goal;
+  moveit_msgs::Constraints goal;
+  
+  std::map<std::string, double> vals;
+  jsg->getVariableValues(vals);
+  
+  goal.joint_constraints.resize(vals.size());
+  unsigned int i = 0;
+  for (std::map<std::string, double>::iterator it = vals.begin() ; it != vals.end(); ++it, ++i)
+  {
+    goal.joint_constraints[i].joint_name = it->first;
+    goal.joint_constraints[i].position = it->second;
+    goal.joint_constraints[i].tolerance_above = tolerance_below;
+    goal.joint_constraints[i].tolerance_below = tolerance_above;
+    goal.joint_constraints[i].weight = 1.0;
+  }
+  
+  return goal;
 }
 
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const std::string &link_name, const geometry_msgs::PoseStamped &pose,
                                                                          double tolerance_pos, double tolerance_angle)
 {
-    moveit_msgs::Constraints goal;
-
-    goal.position_constraints.resize(1);
-    moveit_msgs::PositionConstraint &pcm = goal.position_constraints[0];
-    pcm.link_name = link_name;
-    pcm.target_point_offset.x = 0;
-    pcm.target_point_offset.y = 0;
-    pcm.target_point_offset.z = 0;
-    pcm.constraint_region.primitives.resize(1);
-    shape_msgs::SolidPrimitive &bv = pcm.constraint_region.primitives[0];
-    bv.type = shape_msgs::SolidPrimitive::SPHERE;
-    bv.dimensions.resize(shape_tools::SolidPrimitiveDimCount<shape_msgs::SolidPrimitive::SPHERE>::value);
-    bv.dimensions[shape_msgs::SolidPrimitive::SPHERE_RADIUS] = tolerance_pos;
-    
-    pcm.header = pose.header;
-    pcm.constraint_region.primitive_poses.resize(1);
-    pcm.constraint_region.primitive_poses[0].position = pose.pose.position;
-    
-    // orientation of constraint region does not affect anything, since it is a sphere
-    pcm.constraint_region.primitive_poses[0].orientation.x = 0.0;
-    pcm.constraint_region.primitive_poses[0].orientation.y = 0.0;
-    pcm.constraint_region.primitive_poses[0].orientation.z = 0.0;
-    pcm.constraint_region.primitive_poses[0].orientation.w = 1.0;
-    pcm.weight = 1.0;
-
-    goal.orientation_constraints.resize(1);
-    moveit_msgs::OrientationConstraint &ocm = goal.orientation_constraints[0];
-    ocm.link_name = link_name;
-    ocm.header = pose.header;
-    ocm.orientation = pose.pose.orientation;
-    ocm.absolute_x_axis_tolerance = tolerance_angle;
-    ocm.absolute_y_axis_tolerance = tolerance_angle;
-    ocm.absolute_z_axis_tolerance = tolerance_angle;
-    ocm.weight = 1.0;
-
-    return goal;
+  moveit_msgs::Constraints goal;
+  
+  goal.position_constraints.resize(1);
+  moveit_msgs::PositionConstraint &pcm = goal.position_constraints[0];
+  pcm.link_name = link_name;
+  pcm.target_point_offset.x = 0;
+  pcm.target_point_offset.y = 0;
+  pcm.target_point_offset.z = 0;
+  pcm.constraint_region.primitives.resize(1);
+  shape_msgs::SolidPrimitive &bv = pcm.constraint_region.primitives[0];
+  bv.type = shape_msgs::SolidPrimitive::SPHERE;
+  bv.dimensions.resize(shape_tools::SolidPrimitiveDimCount<shape_msgs::SolidPrimitive::SPHERE>::value);
+  bv.dimensions[shape_msgs::SolidPrimitive::SPHERE_RADIUS] = tolerance_pos;
+  
+  pcm.header = pose.header;
+  pcm.constraint_region.primitive_poses.resize(1);
+  pcm.constraint_region.primitive_poses[0].position = pose.pose.position;
+  
+  // orientation of constraint region does not affect anything, since it is a sphere
+  pcm.constraint_region.primitive_poses[0].orientation.x = 0.0;
+  pcm.constraint_region.primitive_poses[0].orientation.y = 0.0;
+  pcm.constraint_region.primitive_poses[0].orientation.z = 0.0;
+  pcm.constraint_region.primitive_poses[0].orientation.w = 1.0;
+  pcm.weight = 1.0;
+  
+  goal.orientation_constraints.resize(1);
+  moveit_msgs::OrientationConstraint &ocm = goal.orientation_constraints[0];
+  ocm.link_name = link_name;
+  ocm.header = pose.header;
+  ocm.orientation = pose.pose.orientation;
+  ocm.absolute_x_axis_tolerance = tolerance_angle;
+  ocm.absolute_y_axis_tolerance = tolerance_angle;
+  ocm.absolute_z_axis_tolerance = tolerance_angle;
+  ocm.weight = 1.0;
+  
+  return goal;
 }
 
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const std::string &link_name, const geometry_msgs::PoseStamped &pose,
@@ -192,56 +192,57 @@ moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const s
     ocm.absolute_y_axis_tolerance = tolerance_angle[1];
     ocm.absolute_z_axis_tolerance = tolerance_angle[2];
   }
+  return goal;
 }
 
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const std::string &link_name, const geometry_msgs::QuaternionStamped &quat, double tolerance)
 {
-    moveit_msgs::Constraints goal;
-    goal.orientation_constraints.resize(1);
-    moveit_msgs::OrientationConstraint &ocm = goal.orientation_constraints[0];
-    ocm.link_name = link_name;
-    ocm.header = quat.header;
-    ocm.orientation = quat.quaternion;
-    ocm.absolute_x_axis_tolerance = tolerance;
-    ocm.absolute_y_axis_tolerance = tolerance;
-    ocm.absolute_z_axis_tolerance = tolerance;
-    ocm.weight = 1.0;
-    return goal;
+  moveit_msgs::Constraints goal;
+  goal.orientation_constraints.resize(1);
+  moveit_msgs::OrientationConstraint &ocm = goal.orientation_constraints[0];
+  ocm.link_name = link_name;
+  ocm.header = quat.header;
+  ocm.orientation = quat.quaternion;
+  ocm.absolute_x_axis_tolerance = tolerance;
+  ocm.absolute_y_axis_tolerance = tolerance;
+  ocm.absolute_z_axis_tolerance = tolerance;
+  ocm.weight = 1.0;
+  return goal;
 }
 
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const std::string &link_name, const geometry_msgs::PointStamped &goal_point, double tolerance)
 {
-    geometry_msgs::Point p;
-    p.x = 0;
-    p.y = 0;
-    p.z = 0;
-    return constructGoalConstraints(link_name, p, goal_point, tolerance );
+  geometry_msgs::Point p;
+  p.x = 0;
+  p.y = 0;
+  p.z = 0;
+  return constructGoalConstraints(link_name, p, goal_point, tolerance );
 }
 
 moveit_msgs::Constraints kinematic_constraints::constructGoalConstraints(const std::string &link_name, const geometry_msgs::Point &reference_point, const geometry_msgs::PointStamped &goal_point, double tolerance)
 {
-    moveit_msgs::Constraints goal;
-    goal.position_constraints.resize(1);
-    moveit_msgs::PositionConstraint &pcm = goal.position_constraints[0];
-    pcm.link_name = link_name;
-    pcm.target_point_offset.x = reference_point.x;
-    pcm.target_point_offset.y = reference_point.y;
-    pcm.target_point_offset.z = reference_point.z;
-    pcm.constraint_region.primitives.resize(1);
-    pcm.constraint_region.primitives[0].type = shape_msgs::SolidPrimitive::SPHERE;
-    pcm.constraint_region.primitives[0].dimensions.resize(shape_tools::SolidPrimitiveDimCount<shape_msgs::SolidPrimitive::SPHERE>::value);
-    pcm.constraint_region.primitives[0].dimensions[shape_msgs::SolidPrimitive::SPHERE_RADIUS] = tolerance;
-
-    pcm.header = goal_point.header;
-    pcm.constraint_region.primitive_poses.resize(1);
-    pcm.constraint_region.primitive_poses[0].position = goal_point.point;
-
-    // orientation of constraint region does not affect anything, since it is a sphere
-    pcm.constraint_region.primitive_poses[0].orientation.x = 0.0;
-    pcm.constraint_region.primitive_poses[0].orientation.y = 0.0;
-    pcm.constraint_region.primitive_poses[0].orientation.z = 0.0;
-    pcm.constraint_region.primitive_poses[0].orientation.w = 1.0;
-    pcm.weight = 1.0;
-
-    return goal;
+  moveit_msgs::Constraints goal;
+  goal.position_constraints.resize(1);
+  moveit_msgs::PositionConstraint &pcm = goal.position_constraints[0];
+  pcm.link_name = link_name;
+  pcm.target_point_offset.x = reference_point.x;
+  pcm.target_point_offset.y = reference_point.y;
+  pcm.target_point_offset.z = reference_point.z;
+  pcm.constraint_region.primitives.resize(1);
+  pcm.constraint_region.primitives[0].type = shape_msgs::SolidPrimitive::SPHERE;
+  pcm.constraint_region.primitives[0].dimensions.resize(shape_tools::SolidPrimitiveDimCount<shape_msgs::SolidPrimitive::SPHERE>::value);
+  pcm.constraint_region.primitives[0].dimensions[shape_msgs::SolidPrimitive::SPHERE_RADIUS] = tolerance;
+  
+  pcm.header = goal_point.header;
+  pcm.constraint_region.primitive_poses.resize(1);
+  pcm.constraint_region.primitive_poses[0].position = goal_point.point;
+  
+  // orientation of constraint region does not affect anything, since it is a sphere
+  pcm.constraint_region.primitive_poses[0].orientation.x = 0.0;
+  pcm.constraint_region.primitive_poses[0].orientation.y = 0.0;
+  pcm.constraint_region.primitive_poses[0].orientation.z = 0.0;
+  pcm.constraint_region.primitive_poses[0].orientation.w = 1.0;
+  pcm.weight = 1.0;
+  
+  return goal;
 }
