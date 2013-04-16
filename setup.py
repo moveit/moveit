5137f9ae71b2201d7542902e306c3660f1619164
Ioan Sucan <isucan@willowgarage.com>
2013-04-16 12:16:29 +0300

    clean up commander

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
 d['packages'] = ['moveit_commander']
-d['scripts'] = ['bin/moveit_commander_cmdline.py', 'bin/moveit_planning_scene_from_ork.py']
+d['scripts'] = ['bin/moveit_commander_cmdline.py']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
