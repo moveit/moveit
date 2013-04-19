965d61aa80350c2f5e322453e35fdba59c0f4bd2
Ioan Sucan <isucan@willowgarage.com>
2013-04-19 22:48:27 +0300

    cleaner initialization of roscpp from python

--- planning_interface/py_bindings_tools/src/roscpp_initializer.cpp
+++ planning_interface/py_bindings_tools/src/roscpp_initializer.cpp
@@ -35,19 +35,45 @@
 /* Author: Ioan Sucan */
 
 #include "moveit/py_bindings_tools/roscpp_initializer.h"
+#include "moveit/py_bindings_tools/py_conversions.h"
 #include <boost/thread.hpp>
 #include <boost/shared_ptr.hpp>
 #include <ros/ros.h>
+
+static std::vector<std::string>& ROScppArgs()
+{
+  static std::vector<std::string> args;
+  return args;
+}
+
+static std::string& ROScppNodeName()
+{
+  static std::string node_name("moveit_python_wrappers");
+  return node_name;
+}
+
+void moveit_py_bindings_tools::roscpp_set_arguments(const std::string &node_name, boost::python::list &argv)
+{
+  ROScppNodeName() = node_name;
+  ROScppArgs() = stringFromList(argv);
+}
+
+namespace
+{
 
 struct InitProxy
 {
   InitProxy()
   {
-    char **fake_argv = new char*[1];
-    fake_argv[0] = strdup("moveit_python_wrappers");
-    int fake_argc = 1;
-    ros::init(fake_argc, fake_argv, "moveit_python_wrappers", ros::init_options::AnonymousName | ros::init_options::NoSigintHandler);
-    delete[] fake_argv[0];
+    const std::vector<std::string> &args = ROScppArgs();
+    int fake_argc = args.size();
+    char **fake_argv = new char*[args.size()];
+    for (std::size_t i = 0 ; i < args.size() ; ++i)
+      fake_argv[i] = strdup(args[i].c_str());
+    
+    ros::init(fake_argc, fake_argv, ROScppNodeName(), ros::init_options::AnonymousName | ros::init_options::NoSigintHandler);  
+    for (int i = 0 ; i < fake_argc ; ++i)
+      delete[] fake_argv[i];
     delete[] fake_argv;
   }
   
@@ -58,24 +84,75 @@ struct InitProxy
   }
 };
 
-moveit_py_bindings_tools::ROScppInitializer::ROScppInitializer()
-{
+}
+
+static void roscpp_init_or_stop(bool init)
+{ 
   // ensure we do not accidentally initialize ROS multiple times per process
   static boost::mutex lock;
   boost::mutex::scoped_lock slock(lock);
   
   // once per process, we start a spinner
   static bool once = true;
-  if (once)
+  static boost::scoped_ptr<InitProxy> proxy;
+  static boost::scoped_ptr<ros::AsyncSpinner> spinner;
+  
+  // initialize only once
+  if (once && init)
   {
     once = false;
-    static boost::shared_ptr<InitProxy> proxy;
-    
+
     // if ROS (cpp) is not initialized, we initialize it
     if (!ros::isInitialized())
+    {
       proxy.reset(new InitProxy());
-    
-    static ros::AsyncSpinner spinner(1);
-    spinner.start();
+      spinner.reset(new ros::AsyncSpinner(1));
+      spinner->start();
+    }
   }
+  
+  // shutdown if needed
+  if (!init)
+  {
+    once = false;
+    proxy.reset();
+    spinner.reset();
+  }
+}
+
+void moveit_py_bindings_tools::roscpp_init()
+{
+  roscpp_init_or_stop(true);  
+}
+
+void moveit_py_bindings_tools::roscpp_init(const std::string &node_name, boost::python::list &argv)
+{
+  roscpp_set_arguments(node_name, argv);
+  roscpp_init();
+}
+
+void moveit_py_bindings_tools::roscpp_init(boost::python::list &argv)
+{
+  ROScppArgs() = stringFromList(argv);
+  roscpp_init();
+}
+
+void moveit_py_bindings_tools::roscpp_shutdown()
+{
+  roscpp_init_or_stop(false);
+}
+
+moveit_py_bindings_tools::ROScppInitializer::ROScppInitializer()
+{
+  roscpp_init();
+}
+
+moveit_py_bindings_tools::ROScppInitializer::ROScppInitializer(boost::python::list &argv)
+{
+  roscpp_init(argv);
+}
+
+moveit_py_bindings_tools::ROScppInitializer::ROScppInitializer(const std::string &node_name, boost::python::list &argv)
+{
+  roscpp_init(node_name, argv);
 }
