af4e845416a910f9ec2f540949ee735bc7f1f308
ipa-fxm <moveit@raw3-1-pc1.(none)>
2013-04-27 00:03:24 +0200

    (really felix this time) -- added add_mesh

--- src/moveit_commander/planning_scene_interface.py
+++ src/moveit_commander/planning_scene_interface.py
@@ -30,12 +30,14 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 #
-# Author: Ioan Sucan
+# Author: Ioan Sucan, Felix Messmer
 
 import rospy
 from moveit_msgs.msg import CollisionObject, AttachedCollisionObject
-from geometry_msgs.msg import PoseStamped
-from shape_msgs.msg import SolidPrimitive, Plane
+from geometry_msgs.msg import PoseStamped, Point
+from shape_msgs.msg import SolidPrimitive, Plane, Mesh, MeshTriangle
+
+from pyassimp import pyassimp
 
 # This is going to have more functionality; (feel free to add some!)
 # This class will include simple Python code for publishing messages for a planning scene
@@ -76,6 +78,38 @@ class PlanningSceneInterface:
         co.primitives = [box]
         co.primitive_poses = [pose.pose]
         return co
+    
+    def make_mesh(self, name, pose, filename):
+        co = CollisionObject()
+        scene = pyassimp.load(filename)
+        if not scene.meshes:
+            raise "There are no meshes in the file"
+        co.operation = CollisionObject.ADD
+        co.id = name
+        co.header = pose.header
+        
+        mesh = Mesh()
+        for face in scene.meshes[0].faces:
+            triangle = MeshTriangle()
+            if len(face.indices) == 3:
+                triangle.vertex_indices = [face.indices[0], face.indices[1], face.indices[2]]
+            mesh.triangles.append(triangle)
+        for vertex in scene.meshes[0].vertices:
+            point = Point()
+            point.x = vertex[0]
+            point.y = vertex[1]
+            point.z = vertex[2]
+            mesh.vertices.append(point)
+        co.meshes = [mesh]
+        co.mesh_poses = [pose.pose]
+        pyassimp.release(scene)
+        return co
+    
+    def add_mesh(self, name, pose, filename):
+        """
+        Add a mesh to the planning scene
+        """
+        self._pub_co.publish(self.make_mesh(name, pose, filename))
 
     def add_box(self, name, pose, size = (1, 1, 1)):
         """
@@ -95,6 +129,15 @@ class PlanningSceneInterface:
         co.planes = [p]
         co.plane_poses = [pose.pose]
         self._pub_co.publish(co)
+        
+    def attach_mesh(self, link, name, pose, filename, touch_links = []):
+        aco = AttachedCollisionObject()
+        aco.object = self.make_mesh(name, pose, filename)
+        aco.link_name = link
+        aco.touch_links = [link]
+        if len(touch_links) > 0:
+            aco.touch_links = touch_links
+        self._pub_aco.publish(aco)
 
     def attach_box(self, link, name, pose, size = (1, 1, 1), touch_links = []):
         aco = AttachedCollisionObject()
