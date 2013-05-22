dd941cca83fa85c07ba89c581ac99ca9ceb5db1a
Ioan Sucan <isucan@willowgarage.com>
2013-05-22 14:00:53 +0300

    add convenience function

--- moveit_core/trajectory_processing/include/moveit/trajectory_processing/trajectory_tools.h
+++ moveit_core/trajectory_processing/include/moveit/trajectory_processing/trajectory_tools.h
@@ -47,6 +47,7 @@ namespace trajectory_processing
 {
 
 bool isTrajectoryEmpty(const moveit_msgs::RobotTrajectory &trajectory);
+std::size_t trajectoryWaypointCount(const moveit_msgs::RobotTrajectory &trajectory);
 
 }
 
