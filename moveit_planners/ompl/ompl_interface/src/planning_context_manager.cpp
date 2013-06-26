5b3111b73fecf66b88c4d4cd44afbdfb5d4190f0
Dave Coleman <davetcoleman@gmail.com>
2013-06-26 10:12:18 -0700

    Cleaned up code

--- moveit_planners/ompl/ompl_interface/src/planning_context_manager.cpp
+++ moveit_planners/ompl/ompl_interface/src/planning_context_manager.cpp
@@ -59,8 +59,6 @@
 #include <moveit/ompl_interface/parameterization/joint_space/joint_model_state_space_factory.h>
 #include <moveit/ompl_interface/parameterization/work_space/pose_model_state_space_factory.h>
 
-//typedef std::map<std::string, ompl_interface::PlanningConfigurationSettings> PlanningConfigurationMap; // TODO: remove this!
-
 namespace ompl_interface
 {
 class PlanningContextManager::LastPlanningContext
