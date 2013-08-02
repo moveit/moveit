a6c51be6b256008e413bfff458de76066e4b8bf1
Ioan Sucan <isucan@willowgarage.com>
2013-08-02 17:48:46 +0300

    make headers and author definitions aligned the same way; white space fixes

--- include/moveit/setup_assistant/tools/srdf_writer.h
+++ include/moveit/setup_assistant/tools/srdf_writer.h
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -33,6 +33,7 @@
  *********************************************************************/
 
 /* Author: Dave Coleman */
+
 #ifndef MOVEIT_MOVEIT_SETUP_ASSISTANT_TOOLS_SRDF_WRITER_
 #define MOVEIT_MOVEIT_SETUP_ASSISTANT_TOOLS_SRDF_WRITER_
 
