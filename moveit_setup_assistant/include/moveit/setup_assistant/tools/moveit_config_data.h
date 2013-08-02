455d83b4818601ee2c0204f2e4ab7b6546c33fe3
Ioan Sucan <isucan@willowgarage.com>
2013-08-02 17:48:46 +0300

    make headers and author definitions aligned the same way; white space fixes

--- moveit_setup_assistant/include/moveit/setup_assistant/tools/moveit_config_data.h
+++ moveit_setup_assistant/include/moveit/setup_assistant/tools/moveit_config_data.h
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -78,7 +78,7 @@ struct GroupMetaData
 
 /** \brief This class is shared with all widgets and contains the common configuration data
     needed for generating each robot's MoveIt configuration package.
-    
+
     All SRDF data is contained in a subclass of this class -
     srdf_writer.cpp. This class also contains the functions for writing
     out the configuration files. */
