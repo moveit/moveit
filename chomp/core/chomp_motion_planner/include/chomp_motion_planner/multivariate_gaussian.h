67b40de193a25c14b5304927ce39059b53d1f438
Ioan Sucan <isucan@willowgarage.com>
2013-08-02 17:47:11 +0300

    make headers and author definitions aligned the same way; white space fixes

--- chomp/core/chomp_motion_planner/include/chomp_motion_planner/multivariate_gaussian.h
+++ chomp/core/chomp_motion_planner/include/chomp_motion_planner/multivariate_gaussian.h
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -32,7 +32,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/** \author Mrinal Kalakrishnan */
+/* Author: Mrinal Kalakrishnan */
 
 #ifndef MULTIVARIATE_GAUSSIAN_H_
 #define MULTIVARIATE_GAUSSIAN_H_
--- chomp/core/chomp_motion_planner/src/chomp_cost.cpp
+++ chomp/core/chomp_motion_planner/src/chomp_cost.cpp
@@ -14,7 +14,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -32,7 +32,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/** \author Mrinal Kalakrishnan */
+/* Author: Mrinal Kalakrishnan */
 
 #include <chomp_motion_planner/chomp_cost.h>
 #include <chomp_motion_planner/chomp_utils.h>
