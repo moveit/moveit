261e025ecb529f0c438b61e3a57baaee05326d8f
Mario Prats <mprats@willowgarage.com>
2013-08-05 16:31:01 -0700

    Allow user to start the benchmark pipeline from the GUI

---
+++ benchmarks_gui/include/benchmark_processing_thread.h
@@ -0,0 +1,29 @@
+#ifndef BENCHMARK_PROCESSING_THREAD_H
+#define BENCHMARK_PROCESSING_THREAD_H
+
+#include <QtCore/QThread>
+#include <QProgressDialog>
+
+#include <moveit/benchmarks/benchmark_execution.h>
+
+class BenchmarkProcessingThread : public QThread
+{
+  Q_OBJECT
+
+public:
+  BenchmarkProcessingThread(const moveit_benchmarks::BenchmarkExecution &be, const moveit_benchmarks::BenchmarkType &bt, QObject *parent = 0);
+  ~BenchmarkProcessingThread();
+
+  void startAndShow();
+
+private:
+  boost::shared_ptr<QProgressDialog> progress_dialog_;
+
+  moveit_benchmarks::BenchmarkExecution be_;
+  moveit_benchmarks::BenchmarkType bt_;
+
+protected:
+  void run();
+};
+
+#endif // BENCHMARK_PROCESSING_THREAD_H
