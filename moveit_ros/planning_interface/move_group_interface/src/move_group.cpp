9a7a721b285e1a3a33800a2659a27f8d6b7aa392
Adolfo Rodriguez Tsouroukdissian <adolfo.rodriguez@pal-robotics.com>
2014-03-11 12:37:13 +0100

    Fix initialization reorder warning.

--- moveit_ros/planning_interface/move_group_interface/src/move_group.cpp
+++ moveit_ros/planning_interface/move_group_interface/src/move_group.cpp
@@ -83,7 +83,10 @@ class MoveGroup::MoveGroupImpl
 {
 public:
 
-  MoveGroupImpl(const Options &opt, const boost::shared_ptr<tf::Transformer> &tf, const ros::Duration &wait_for_server) : opt_(opt), tf_(tf), node_handle_(opt.node_handle_)
+  MoveGroupImpl(const Options &opt, const boost::shared_ptr<tf::Transformer> &tf, const ros::Duration &wait_for_server)
+    : opt_(opt),
+      node_handle_(opt.node_handle_),
+      tf_(tf)
   {
     robot_model_ = opt.robot_model_ ? opt.robot_model_ : getSharedRobotModel(opt.robot_description_);
     if (!getRobotModel())
