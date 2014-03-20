d18885c783e5e4886322aeb97d70a6f099829a10
Sachin Chitta <sachinchitta@users.noreply.github.com>
2014-03-19 20:46:19 -0700

    Removing line that's not being used.

--- moveit_ros/planning_interface/move_group_interface/src/move_group.cpp
+++ moveit_ros/planning_interface/move_group_interface/src/move_group.cpp
@@ -1067,7 +1067,6 @@ moveit::planning_interface::MoveItErrorCode moveit::planning_interface::MoveGrou
 
 moveit::planning_interface::MoveItErrorCode moveit::planning_interface::MoveGroup::move()
 {
-  moveit_msgs::MoveItErrorCodes error_code;  
   return impl_->move(true);
 }
 
