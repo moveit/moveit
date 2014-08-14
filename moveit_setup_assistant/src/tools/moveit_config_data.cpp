ffd309456d8d9ad8b63ccb51957f373faa10e32b
Dave Coleman <davetcoleman@gmail.com>
2014-08-14 19:16:56 +0900

    Added a new 'velocity_scaling_factor' parameter to evenly reduce max joint velocity for all joints. Added documentation.

--- moveit_setup_assistant/src/tools/moveit_config_data.cpp
+++ moveit_setup_assistant/src/tools/moveit_config_data.cpp
@@ -540,6 +540,10 @@ bool MoveItConfigData::outputJointLimitsYAML( const std::string& file_path )
   emitter << YAML::Key << "joint_limits";
   emitter << YAML::Value << YAML::BeginMap;
 
+  // Set an optional velocity scaling factor, off by default
+  emitter << YAML::Key << "velocity_scaling_factor";
+  emitter << YAML::Value << 1.0;
+
   // Union all the joints in groups. Uses a custom comparator to allow the joints to be sorted by name
   std::set<const robot_model::JointModel*, joint_model_compare> joints;
 
@@ -604,7 +608,11 @@ bool MoveItConfigData::outputJointLimitsYAML( const std::string& file_path )
     ROS_ERROR_STREAM( "Unable to open file for writing " << file_path );
     return false;
   }
-
+  // Add documentation into joint_limits.yaml
+  output_stream << "# joint_limits.yaml allows the dynamics properties specified in the URDF to be overwritten or augmented as needed" << std::endl;
+  output_stream << "# Specific joint properties can be changed with the keys [max_position, min_position, max_velocity, max_acceleration]" << std::endl;
+  output_stream << "# Joint limits can be turned off with [has_velocity_limits, has_acceleration_limits]" << std::endl;
+  output_stream << "# Finally, as a final step velocity limits can be scaled using a global value [velocity_scaling_factor]" << std::endl;
   output_stream << emitter.c_str();
   output_stream.close();
 
