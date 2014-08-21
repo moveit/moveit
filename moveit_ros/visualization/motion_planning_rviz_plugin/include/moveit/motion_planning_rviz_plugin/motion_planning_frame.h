garaemon/moveit_ros

adding PoseStamped topic to move the interactive marker from other ros nodes such as joystick programs.

visualization/motion_planning_rviz_plugin/include/moveit/motion_planning_rviz_plugin/motion_planning_frame.h
@@ -56,6 +56,7 @@
 
 
 #include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
 #include <map>
 #include <string>
 
@@ -259,11 +260,20 @@ private:
   void waitForAction(const T &action, const ros::NodeHandle &node_handle, const ros::Duration &wait_for_server, const std::string &name);
   void listenDetectedObjects(const object_recognition_msgs::RecognizedObjectArrayPtr &msg);  
   ros::Subscriber object_recognition_subscriber_;  
-
+  
+  ros::Subscriber plan_subscriber_;
+  ros::Subscriber execute_subscriber_;
+  ros::Subscriber update_start_state_subscriber_;
+  ros::Subscriber update_goal_state_subscriber_;
   //General
   void changePlanningGroupHelper();
   void importResource(const std::string &path);
   void loadStoredStates(const std::string& pattern);
+
+  void planCallback(const std_msgs::EmptyConstPtr& msg);
+  void executeCallback(const std_msgs::EmptyConstPtr& msg);
+  void updateStartStateCallback(const std_msgs::EmptyConstPtr& msg);
+  void updateGoalStateCallback(const std_msgs::EmptyConstPtr& msg);
 
   /* Selects or unselects a item in a list by the item name */
   void setItemSelectionInList(const std::string &item_name, bool selection, QListWidget *list);

visualization/motion_planning_rviz_plugin/src/motion_planning_frame.cpp
@@ -176,6 +176,14 @@ MotionPlanningFrame::MotionPlanningFrame(MotionPlanningDisplay *pdisplay, rviz::
   {
     ROS_ERROR("%s", ex.what());
   }
+
+  ros::NodeHandle nh;
+  plan_subscriber_ = nh.subscribe("/rviz/moveit/plan", 1, &MotionPlanningFrame::planCallback, this);
+  execute_subscriber_ = nh.subscribe("/rviz/moveit/execute", 1, &MotionPlanningFrame::executeCallback, this);
+  update_start_state_subscriber_ = nh.subscribe("/rviz/moveit/update_start_state",1,
+                                                &MotionPlanningFrame::updateStartStateCallback, this);
+  update_goal_state_subscriber_ = nh.subscribe("/rviz/moveit/update_goal_state",1,
+                                               &MotionPlanningFrame::updateGoalStateCallback, this);
 }
 
 MotionPlanningFrame::~MotionPlanningFrame()

visualization/motion_planning_rviz_plugin/src/motion_planning_frame_planning.cpp
@@ -295,4 +295,39 @@ void MotionPlanningFrame::configureForPlanning()
   configureWorkspace();
 }
 
+void MotionPlanningFrame::planCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  planButtonClicked();
+}
+
+void MotionPlanningFrame::executeCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  executeButtonClicked();
+}
+
+void MotionPlanningFrame::updateStartStateCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  if (move_group_ && planning_display_) {
+    robot_state::RobotState state = *planning_display_->getQueryStartState();
+    const planning_scene_monitor::LockedPlanningSceneRO &ps = planning_display_->getPlanningSceneRO();
+    if (ps) {
+      state = ps->getCurrentState();
+      planning_display_->setQueryStartState(state);
+    }
+  }
+}
+
+void MotionPlanningFrame::updateGoalStateCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  if (move_group_ && planning_display_) {
+    robot_state::RobotState state = *planning_display_->getQueryStartState();
+    const planning_scene_monitor::LockedPlanningSceneRO &ps = planning_display_->getPlanningSceneRO();
+    if (ps) {
+      state = ps->getCurrentState();
+      planning_display_->setQueryGoalState(state);
+    }
+  }
+}
+
+  
 }

moveit/moveit

moveit_ros/visualization/motion_planning_rviz_plugin/include/moveit/motion_planning_rviz_plugin/motion_planning_frame.h
@@ -57,6 +57,7 @@
 
 
 #include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
 #include <map>
 #include <string>
 
@@ -260,11 +261,20 @@ private:
   void waitForAction(const T &action, const ros::NodeHandle &node_handle, const ros::Duration &wait_for_server, const std::string &name);
   void listenDetectedObjects(const object_recognition_msgs::RecognizedObjectArrayPtr &msg);  
   ros::Subscriber object_recognition_subscriber_;  
-
+  
+  ros::Subscriber plan_subscriber_;
+  ros::Subscriber execute_subscriber_;
+  ros::Subscriber update_start_state_subscriber_;
+  ros::Subscriber update_goal_state_subscriber_;
   //General
   void changePlanningGroupHelper();
   void importResource(const std::string &path);
   void loadStoredStates(const std::string& pattern);
+
+  void remotePlanCallback(const std_msgs::EmptyConstPtr& msg);
+  void remoteExecuteCallback(const std_msgs::EmptyConstPtr& msg);
+  void remoteUpdateStartStateCallback(const std_msgs::EmptyConstPtr& msg);
+  void remoteUpdateGoalStateCallback(const std_msgs::EmptyConstPtr& msg);
 
   /* Selects or unselects a item in a list by the item name */
   void setItemSelectionInList(const std::string &item_name, bool selection, QListWidget *list);

moveit_ros/visualization/motion_planning_rviz_plugin/src/motion_planning_frame.cpp
@@ -176,6 +176,14 @@ MotionPlanningFrame::MotionPlanningFrame(MotionPlanningDisplay *pdisplay, rviz::
   {
     ROS_ERROR("%s", ex.what());
   }
+
+  ros::NodeHandle nh;
+  plan_subscriber_ = nh.subscribe("/rviz/moveit/plan", 1, &MotionPlanningFrame::remotePlanCallback, this);
+  execute_subscriber_ = nh.subscribe("/rviz/moveit/execute", 1, &MotionPlanningFrame::remoteExecuteCallback, this);
+  update_start_state_subscriber_ = nh.subscribe("/rviz/moveit/update_start_state",1,
+                                                &MotionPlanningFrame::remoteUpdateStartStateCallback, this);
+  update_goal_state_subscriber_ = nh.subscribe("/rviz/moveit/update_goal_state",1,
+                                               &MotionPlanningFrame::remoteUpdateGoalStateCallback, this);
 }
 
 MotionPlanningFrame::~MotionPlanningFrame()

moveit_ros/visualization/motion_planning_rviz_plugin/src/motion_planning_frame_planning.cpp
@@ -328,4 +328,39 @@ void MotionPlanningFrame::configureForPlanning()
   configureWorkspace();
 }
 
+void MotionPlanningFrame::remotePlanCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  planButtonClicked();
+}
+
+void MotionPlanningFrame::remoteExecuteCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  executeButtonClicked();
+}
+
+void MotionPlanningFrame::remoteUpdateStartStateCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  if (move_group_ && planning_display_) {
+    robot_state::RobotState state = *planning_display_->getQueryStartState();
+    const planning_scene_monitor::LockedPlanningSceneRO &ps = planning_display_->getPlanningSceneRO();
+    if (ps) {
+      state = ps->getCurrentState();
+      planning_display_->setQueryStartState(state);
+    }
+  }
+}
+
+void MotionPlanningFrame::remoteUpdateGoalStateCallback(const std_msgs::EmptyConstPtr& msg)
+{
+  if (move_group_ && planning_display_) {
+    robot_state::RobotState state = *planning_display_->getQueryStartState();
+    const planning_scene_monitor::LockedPlanningSceneRO &ps = planning_display_->getPlanningSceneRO();
+    if (ps) {
+      state = ps->getCurrentState();
+      planning_display_->setQueryGoalState(state);
+    }
+  }
+}
+
+  
 }