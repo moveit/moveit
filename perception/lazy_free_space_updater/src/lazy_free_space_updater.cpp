95eb72a064a07d1991e10a8c2bae9d5e331c1796
Jonathan Bohren <jbo@jhu.edu>
2014-12-15 16:42:41 -0500

    perception: moving lazy_free_space_updater into it's own library

--- perception/lazy_free_space_updater/src/lazy_free_space_updater.cpp
+++ perception/lazy_free_space_updater/src/lazy_free_space_updater.cpp
@@ -34,7 +34,7 @@
 
 /* Author: Ioan Sucan */
 
-#include <moveit/depth_image_octomap_updater/lazy_free_space_updater.h>
+#include <moveit/lazy_free_space_updater/lazy_free_space_updater.h>
 #include <ros/console.h>
 
 namespace occupancy_map_monitor
