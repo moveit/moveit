16a9ce00844f7e9eb205cfad80eb7dc821ace9ac
Dave Coleman <davetcoleman@gmail.com>
2015-06-16 21:43:54 -0700

    Deprecate shape_tools

--- moveit_ros/planning/planning_scene_monitor/demos/demo_scene.cpp
+++ moveit_ros/planning/planning_scene_monitor/demos/demo_scene.cpp
@@ -35,7 +35,7 @@
 /* Author: Ioan Sucan */
 
 #include <moveit/planning_scene_monitor/planning_scene_monitor.h>
-#include <shape_tools/solid_primitive_dims.h>
+#include <geometric_shapes/solid_primitive_dims.h>
 
 static const std::string ROBOT_DESCRIPTION="robot_description";
 
