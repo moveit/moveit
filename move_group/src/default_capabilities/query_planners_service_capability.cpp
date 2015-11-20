92bc975627eb388cd1f068d1caef6afe4766e146
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2015-11-20 23:16:28 +0100

    moved planner params services into existing capability QueryPlannerInterfaces

--- move_group/src/default_capabilities/query_planners_service_capability.cpp
+++ move_group/src/default_capabilities/query_planners_service_capability.cpp
@@ -32,7 +32,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/* Author: Ioan Sucan */
+/* Author: Ioan Sucan, Robert Haschke */
 
 #include "query_planners_service_capability.h"
 #include <moveit/planning_pipeline/planning_pipeline.h>
@@ -46,6 +46,11 @@ move_group::MoveGroupQueryPlannersService::MoveGroupQueryPlannersService():
 void move_group::MoveGroupQueryPlannersService::initialize()
 {
   query_service_ = root_node_handle_.advertiseService(QUERY_PLANNERS_SERVICE_NAME, &MoveGroupQueryPlannersService::queryInterface, this);
+
+  get_service_ = root_node_handle_.advertiseService(GET_PLANNER_PARAMS_SERVICE_NAME,
+                                                    &MoveGroupQueryPlannersService::getParams, this);
+  set_service_ = root_node_handle_.advertiseService(SET_PLANNER_PARAMS_SERVICE_NAME,
+                                                    &MoveGroupQueryPlannersService::setParams, this);
 }
 
 bool move_group::MoveGroupQueryPlannersService::queryInterface(moveit_msgs::QueryPlannerInterfaces::Request &req, moveit_msgs::QueryPlannerInterfaces::Response &res)
@@ -59,6 +64,61 @@ bool move_group::MoveGroupQueryPlannersService::queryInterface(moveit_msgs::Quer
     pi_desc.name = planner_interface->getDescription();
     planner_interface->getPlanningAlgorithms(pi_desc.planner_ids);
     res.planner_interfaces.push_back(pi_desc);
+  }
+  return true;
+}
+
+bool move_group::MoveGroupQueryPlannersService::getParams(moveit_msgs::GetPlannerParams::Request &req,
+                                                          moveit_msgs::GetPlannerParams::Response &res)
+{
+  const planning_interface::PlannerManagerPtr &planner_interface = context_->planning_pipeline_->getPlannerManager();
+  if (planner_interface)
+  {
+    std::map<std::string, std::string> config;
+
+    const planning_interface::PlannerConfigurationMap&
+        configs = planner_interface->getPlannerConfigurations();
+
+    planning_interface::PlannerConfigurationMap::const_iterator
+        it = configs.find(req.planner_config); // fetch default params first
+    config.insert(it->second.config.begin(), it->second.config.end());
+
+    if (!req.group.empty()) { // merge in group-specific params
+      it = configs.find(req.group + "[" + req.planner_config + "]");
+      config.insert(it->second.config.begin(), it->second.config.end());
+    }
+
+    for (std::map<std::string, std::string>::const_iterator
+         it = config.begin(), end = config.end(); it != end; ++it) {
+      res.params.keys.push_back(it->first);
+      res.params.values.push_back(it->second);
+    }
+  }
+  return true;
+}
+
+bool move_group::MoveGroupQueryPlannersService::setParams(moveit_msgs::SetPlannerParams::Request &req,
+                                                          moveit_msgs::SetPlannerParams::Response &res)
+{
+  const planning_interface::PlannerManagerPtr &planner_interface = context_->planning_pipeline_->getPlannerManager();
+  if (req.params.keys.size() != req.params.values.size())
+    return false;
+
+  if (planner_interface)
+  {
+    planning_interface::PlannerConfigurationMap
+        configs = planner_interface->getPlannerConfigurations();
+    std::string config_name = req.group.empty() ?
+          req.planner_config : req.group + "[" + req.planner_config + "]";
+
+    planning_interface::PlannerConfigurationSettings &config = configs[config_name];
+    config.group = req.group;
+    config.name = config_name;
+    if (req.replace) config.config.clear();
+    for (unsigned int i=0, end=req.params.keys.size(); i < end; ++i)
+      config.config[req.params.keys[i]] = req.params.values[i];
+
+    planner_interface->setPlannerConfigurations(configs);
   }
   return true;
 }
