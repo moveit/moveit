7250785649d25f08fff45f12f60201c3b693a192
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2016-07-19 22:57:39 +0200

    ABI compatibility
    move new class members to end of class definition

--- moveit_ros/planning/planning_scene_monitor/include/moveit/planning_scene_monitor/planning_scene_monitor.h
+++ moveit_ros/planning/planning_scene_monitor/include/moveit/planning_scene_monitor/planning_scene_monitor.h
@@ -411,14 +411,9 @@ protected:
   planning_scene::PlanningSceneConstPtr scene_const_;
   planning_scene::PlanningScenePtr      parent_scene_; /// if diffs are monitored, this is the pointer to the parent scene
   boost::shared_mutex                   scene_update_mutex_; /// mutex for stored scene
-  ros::Time                             last_update_time_; /// Last time the scene was updated
-  ros::Time                             last_robot_motion_time_; /// Last time the robot has moved
-  bool                                  enforce_next_state_update_;
 
   ros::NodeHandle                       nh_;
   ros::NodeHandle                       root_nh_;
-  ros::CallbackQueue                    callback_queue_;
-  boost::scoped_ptr<ros::AsyncSpinner>  spinner_;
   boost::shared_ptr<tf::Transformer>    tf_;
   std::string                           robot_description_;
 
@@ -471,6 +466,7 @@ protected:
   /// lock access to update_callbacks_
   boost::recursive_mutex update_lock_;
   std::vector<boost::function<void(SceneUpdateType)> > update_callbacks_; /// List of callbacks to trigger when updates are received
+  ros::Time last_update_time_; /// Last time the state was updated
 
 private:
 
@@ -521,6 +517,11 @@ private:
 
   class DynamicReconfigureImpl;
   DynamicReconfigureImpl *reconfigure_impl_;
+
+  ros::CallbackQueue                    callback_queue_;
+  boost::scoped_ptr<ros::AsyncSpinner>  spinner_;
+  ros::Time                             last_robot_motion_time_; /// Last time the robot has moved
+  bool                                  enforce_next_state_update_;
 };
 
 typedef boost::shared_ptr<PlanningSceneMonitor> PlanningSceneMonitorPtr;
