9ea30d5e69dad48ed7af1bfe1c686eb75ba4dcbb
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-08-18 15:50:50 +0900

    Remove no need including of ExecuteKnownTrajectory.h

--- moveit_ros/planning_interface/move_group_interface/src/move_group.cpp
+++ moveit_ros/planning_interface/move_group_interface/src/move_group.cpp
@@ -51,7 +51,6 @@
 #include <moveit_msgs/PickupAction.h>
 #include <moveit_msgs/ExecutePathAction.h>
 #include <moveit_msgs/PlaceAction.h>
-#include <moveit_msgs/ExecuteKnownTrajectory.h>
 #include <moveit_msgs/QueryPlannerInterfaces.h>
 #include <moveit_msgs/GetCartesianPath.h>
 
