623587d611edafc19a1ba39b11fe2a548f5a9505
Chittaranjan Srinivas Swaminathan <ksatyaki@users.noreply.github.com>
2016-09-22 02:24:58 +0200

    Fix CHOMP planner and CollisionDistanceField (#155)
    * Copy collision_distance_field package
    * Resurrect chomp
    * remove some old Makefiles and manifests
    * Correct various errors
    * Code formatting, author, description, version, etc
    * Add definitions for c++11. Nested templates problem.
    * Add name to planner plugin.
    * Change getJointModels to getActiveJointModels.
    * Call robot_state::RobotState::update in setRobotStateFromPoint.
    * Create README.md
    * Improve package.xml, CMake config and other changes suggested by jrgnicho.
    * Remove some commented code, add scaling factors to computeTimeStampes
    * Add install targets in moveit_experimental and chomp
    * Add install target for headers in chomp pkgs.
    * Remove unnecessary debugging ROS_INFO.
    * Port collision_distance_field test to indigo.
    * Remove one assertion that makes collision_distance_field test to fail.

---
+++ moveit_planners/chomp/chomp_interface/src/chomp_interface.cpp
@@ -0,0 +1,75 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2012, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: E. Gil Jones */
+
+#include <chomp_interface/chomp_interface.h>
+
+namespace chomp_interface
+{
+CHOMPInterface::CHOMPInterface() : ChompPlanner()
+{
+  loadParams();
+}
+
+void CHOMPInterface::loadParams()
+{
+  nh_.param("planning_time_limit", params_.planning_time_limit_, 10.0);
+  nh_.param("max_iterations", params_.max_iterations_, 200);
+  nh_.param("max_iterations_after_collision_free", params_.max_iterations_after_collision_free_, 5);
+  nh_.param("smoothness_cost_weight", params_.smoothness_cost_weight_, 0.1);
+  nh_.param("obstacle_cost_weight", params_.obstacle_cost_weight_, 1.0);
+  nh_.param("learning_rate", params_.learning_rate_, 0.01);
+  nh_.param("animate_path", params_.animate_path_, true);
+  nh_.param("add_randomness", params_.add_randomness_, false);
+  nh_.param("smoothness_cost_velocity", params_.smoothness_cost_velocity_, 0.0);
+  nh_.param("smoothness_cost_acceleration", params_.smoothness_cost_acceleration_, 1.0);
+  nh_.param("smoothness_cost_jerk", params_.smoothness_cost_jerk_, 0.0);
+  nh_.param("hmc_discretization", params_.hmc_discretization_, 0.01);
+  nh_.param("hmc_stochasticity", params_.hmc_stochasticity_, 0.01);
+  nh_.param("hmc_annealing_factor", params_.hmc_annealing_factor_, 0.99);
+  nh_.param("use_hamiltonian_monte_carlo", params_.use_hamiltonian_monte_carlo_, false);
+  nh_.param("ridge_factor", params_.ridge_factor_, 0.0);
+  nh_.param("use_pseudo_inverse", params_.use_pseudo_inverse_, false);
+  nh_.param("pseudo_inverse_ridge_factor", params_.pseudo_inverse_ridge_factor_, 1e-4);
+  nh_.param("animate_endeffector", params_.animate_endeffector_, false);
+  nh_.param("animate_endeffector_segment", params_.animate_endeffector_segment_, std::string("r_gripper_tool_frame"));
+  nh_.param("joint_update_limit", params_.joint_update_limit_, 0.1);
+  nh_.param("collision_clearence", params_.min_clearence_, 0.2);
+  nh_.param("collision_threshold", params_.collision_threshold_, 0.07);
+  nh_.param("random_jump_amount", params_.random_jump_amount_, 1.0);
+  nh_.param("use_stochastic_descent", params_.use_stochastic_descent_, true);
+  // filter_mode_ = false;
+}
+}
---
+++ moveit_planners/chomp/chomp_interface/src/chomp_plugin.cpp
@@ -0,0 +1,117 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2011, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/planning_scene/planning_scene.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit_msgs/GetMotionPlan.h>
+#include <chomp_interface/chomp_planning_context.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include <pluginlib/class_list_macros.h>
+
+namespace chomp_interface
+{
+class CHOMPPlannerManager : public planning_interface::PlannerManager
+{
+public:
+  CHOMPPlannerManager() : planning_interface::PlannerManager()
+  {
+  }
+
+  bool initialize(const robot_model::RobotModelConstPtr &model, const std::string &ns)
+  {
+    // model->printModelInfo(std::cout);
+    std::vector<std::string> groups = model->getJointModelGroupNames();
+    ROS_INFO_STREAM("Following groups exist:");
+    for (int i = 0; i < groups.size(); i++)
+    {
+      ROS_INFO("%s", groups[i].c_str());
+      planning_contexts_[groups[i]] =
+          CHOMPPlanningContextPtr(new CHOMPPlanningContext("chomp_planning_context", groups[i], model));
+    }
+    return true;
+  }
+
+  planning_interface::PlanningContextPtr getPlanningContext(const planning_scene::PlanningSceneConstPtr &planning_scene,
+                                                            const planning_interface::MotionPlanRequest &req,
+                                                            moveit_msgs::MoveItErrorCodes &error_code) const
+  {
+    error_code.val = moveit_msgs::MoveItErrorCodes::SUCCESS;
+
+    if (req.group_name.empty())
+    {
+      ROS_ERROR("No group specified to plan for");
+      error_code.val = moveit_msgs::MoveItErrorCodes::INVALID_GROUP_NAME;
+      return planning_interface::PlanningContextPtr();
+    }
+
+    if (!planning_scene)
+    {
+      ROS_ERROR("No planning scene supplied as input");
+      error_code.val = moveit_msgs::MoveItErrorCodes::FAILURE;
+      return planning_interface::PlanningContextPtr();
+    }
+
+    planning_contexts_.at(req.group_name)->setMotionPlanRequest(req);
+    error_code.val = moveit_msgs::MoveItErrorCodes::SUCCESS;
+    return planning_contexts_.at(req.group_name);
+  }
+
+  bool canServiceRequest(const planning_interface::MotionPlanRequest &req) const
+  {
+    // TODO: this is a dummy implementation
+    //      capabilities.dummy = false;
+    return true;
+  }
+
+  std::string getDescription() const
+  {
+    return "CHOMP";
+  }
+
+  void getPlanningAlgorithms(std::vector<std::string> &algs) const
+  {
+    algs.resize(1);
+    algs[0] = "CHOMP";
+  }
+
+protected:
+  std::map<std::string, CHOMPPlanningContextPtr> planning_contexts_;
+};
+
+}  // ompl_interface_ros
+
+PLUGINLIB_EXPORT_CLASS(chomp_interface::CHOMPPlannerManager, planning_interface::PlannerManager);
