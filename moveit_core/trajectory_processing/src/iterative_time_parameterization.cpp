941c2251b96ae40b5814a2e4f1de60ef283dd605
Ruben <Rubenburger@gmail.com>
2016-12-14 14:39:03 +0100

    Updated spline.h license to BSDv3.
    Updated spline.h license to BSDv3.
    
    Clang-format.
    
    Fixing license errors in spline.h

--- moveit_core/trajectory_processing/src/iterative_time_parameterization.cpp
+++ moveit_core/trajectory_processing/src/iterative_time_parameterization.cpp
@@ -189,11 +189,7 @@ void updateTrajectory(robot_trajectory::RobotTrajectory &rob_trajectory, const s
   if (time_diff.empty())
     return;
 
-  double time_sum = 0.0;
-
-  robot_state::RobotStatePtr prev_waypoint;
   robot_state::RobotStatePtr curr_waypoint;
-  robot_state::RobotStatePtr next_waypoint;
 
   const robot_model::JointModelGroup *group = rob_trajectory.getGroup();
   const std::vector<std::string> &vars = group->getVariableNames();
@@ -201,7 +197,7 @@ void updateTrajectory(robot_trajectory::RobotTrajectory &rob_trajectory, const s
 
   int num_points = rob_trajectory.getWayPointCount();
 
-  rob_trajectory.setWayPointDurationFromPrevious(0, time_sum);
+  rob_trajectory.setWayPointDurationFromPrevious(0, 0.0);
 
   // Times
   for (int i = 1; i < num_points; ++i)
@@ -212,84 +208,6 @@ void updateTrajectory(robot_trajectory::RobotTrajectory &rob_trajectory, const s
   if (num_points <= 1)
     return;
 
-  // Accelerations
-  for (int i = 0; i < num_points; ++i)
-  {
-    curr_waypoint = rob_trajectory.getWayPointPtr(i);
-
-    if (i > 0)
-      prev_waypoint = rob_trajectory.getWayPointPtr(i - 1);
-
-    if (i < num_points - 1)
-      next_waypoint = rob_trajectory.getWayPointPtr(i + 1);
-
-    for (std::size_t j = 0; j < vars.size(); ++j)
-    {
-      double q1;
-      double q2;
-      double q3;
-      double dt1;
-      double dt2;
-
-      if (i == 0)
-      {
-        // First point
-        q1 = next_waypoint->getVariablePosition(idx[j]);
-        q2 = curr_waypoint->getVariablePosition(idx[j]);
-        q3 = q1;
-
-        dt1 = dt2 = time_diff[i];
-      }
-      else if (i < num_points - 1)
-      {
-        // middle points
-        q1 = prev_waypoint->getVariablePosition(idx[j]);
-        q2 = curr_waypoint->getVariablePosition(idx[j]);
-        q3 = next_waypoint->getVariablePosition(idx[j]);
-
-        dt1 = time_diff[i - 1];
-        dt2 = time_diff[i];
-      }
-      else
-      {
-        // last point
-        q1 = prev_waypoint->getVariablePosition(idx[j]);
-        q2 = curr_waypoint->getVariablePosition(idx[j]);
-        q3 = q1;
-
-        dt1 = dt2 = time_diff[i - 1];
-      }
-
-      double v1, v2, a;
-
-      bool start_velocity = false;
-      if (dt1 == 0.0 || dt2 == 0.0)
-      {
-        v1 = 0.0;
-        v2 = 0.0;
-        a = 0.0;
-      }
-      else
-      {
-        if (i == 0)
-        {
-          if (curr_waypoint->hasVelocities())
-          {
-            start_velocity = true;
-            v1 = curr_waypoint->getVariableVelocity(idx[j]);
-          }
-        }
-        v1 = start_velocity ? v1 : (q2 - q1) / dt1;
-        // v2 = (q3-q2)/dt2;
-        v2 = start_velocity ? v1 : (q3 - q2) / dt2;  // Needed to ensure continuous velocity for first point
-        a = 2.0 * (v2 - v1) / (dt1 + dt2);
-      }
-
-      curr_waypoint->setVariableVelocity(idx[j], (v2 + v1) / 2.0);
-      curr_waypoint->setVariableAcceleration(idx[j], a);
-    }
-  }
-
   // Spline fitting using GPL lib
   for (std::size_t j = 0; j < vars.size(); ++j)
   {
@@ -297,7 +215,6 @@ void updateTrajectory(robot_trajectory::RobotTrajectory &rob_trajectory, const s
     for (int i = 0; i < num_points; ++i)
     {
       curr_waypoint = rob_trajectory.getWayPointPtr(i);
-
       y.push_back(curr_waypoint->getVariablePosition(idx[j]));
       x.push_back(rob_trajectory.getWaypointDurationFromStart(i));
     }
@@ -306,16 +223,11 @@ void updateTrajectory(robot_trajectory::RobotTrajectory &rob_trajectory, const s
     s.set_boundary(tk::spline::first_deriv, 0.0, tk::spline::first_deriv, 0.0, false);
     s.set_points(x, y);
 
-    curr_waypoint = rob_trajectory.getWayPointPtr(0);
-    curr_waypoint->setVariableVelocity(idx[j], s.deriv(1, 0.0));
-    curr_waypoint->setVariableAcceleration(idx[j], s.deriv(2, 0.0));
-
-    for (int i = 1; i < num_points; ++i)
+    for (int i = 0; i < num_points; ++i)
     {
       curr_waypoint = rob_trajectory.getWayPointPtr(i);
-
-        curr_waypoint->setVariableVelocity(idx[j], s.deriv(1, x[i]));
-        curr_waypoint->setVariableAcceleration(idx[j], s.deriv(2, x[i]));
+      curr_waypoint->setVariableVelocity(idx[j], s.deriv(1, x[i]));
+      curr_waypoint->setVariableAcceleration(idx[j], s.deriv(2, x[i]));
     }
   }
 }
