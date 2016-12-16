e56d88afcbdcf6f7513c75f13b2c54bd3227a225
Ruben <Rubenburger@gmail.com>
2016-12-16 11:38:46 +0100

    Updated license information.

--- moveit_core/trajectory_processing/include/moveit/trajectory_processing/spline_parameterization.h
+++ moveit_core/trajectory_processing/include/moveit/trajectory_processing/spline_parameterization.h
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2011, Willow Garage, Inc.
+ *  Copyright (c) 2016, Delft University of Technology
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -14,9 +14,9 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of Willow Garage nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
+ *   * Neither the name of Delft University of Technology nor the names
+ *     of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
  *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -32,7 +32,10 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
-/* Author: Ken Anderson */
+/* Author: Ruben Burger (r.b.burger@tudelft.nl)
+ *
+ * Based on iterative_time_parameterization.h, by Ken Anderson
+ */
 
 #ifndef MOVEIT_TRAJECTORY_PROCESSING_SPLINE_SMOOTHER_
 #define MOVEIT_TRAJECTORY_PROCESSING_SPLINE_SMOOTHER_
