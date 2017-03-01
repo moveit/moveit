a4a55070e02a54b69d471c114334e4ee345b1764
v4hn <me@v4hn.de>
2017-03-02 00:28:34 +0100

    robot state display: fix segfault on topic change
    loadRobotModel should be run in the update thread.
    Calling it in the wrong thread leads to mysterious segfaults over here.

--- moveit_ros/visualization/robot_state_rviz_plugin/src/robot_state_display.cpp
+++ moveit_ros/visualization/robot_state_rviz_plugin/src/robot_state_display.cpp
@@ -288,10 +288,10 @@ void RobotStateDisplay::changedRobotSceneAlpha()
 void RobotStateDisplay::changedRobotStateTopic()
 {
   robot_state_subscriber_.shutdown();
-  robot_state_subscriber_ = root_nh_.subscribe(robot_state_topic_property_->getStdString(), 10,
-                                               &RobotStateDisplay::newRobotStateCallback, this);
   robot_->clear();
-  loadRobotModel();
+  load_robot_model_ = true;
+  robot_state_subscriber_ = root_nh_.subscribe(robot_state_topic_property_->getTopicStd(), 10,
+                                               &RobotStateDisplay::newRobotStateCallback, this);
 }
 
 void RobotStateDisplay::newRobotStateCallback(const moveit_msgs::DisplayRobotStateConstPtr& state_msg)
