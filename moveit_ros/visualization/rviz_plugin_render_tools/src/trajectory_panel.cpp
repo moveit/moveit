93463cb18b4387f49eab7421ad40619ff3b76e1e
Yannick Jonetzko <5jonetzk@informatik.uni-hamburg.de>
2017-05-17 20:07:16 +0200

    New panel with a slider to control the visualized trajectory (#491)
    * New panel with a slider controlling the planned path
    
    This panel is integrated in the trajectory visualization, that means for the Trajectory
    display and the MotionPlanning display appears a new panel.
    
    This is an extension to the displayed planned path, currently the planned path is
    shown once after it is planned, if the loop mode is enabled the animation will
    be repeated. These behaviors will be kept.
    
    This panel includes a slider which controls this animated path, it is possible now
    to display the single waypoints of the trajectory. There is also a play/pause button
    which starts the animation from the current waypoint to the end and to pause it again.
    Pressing the play button after the animation is finished replays the trajectory.
    This can also be used to pause the loop mode.
    
    * Simplified logic of the animated planned path with the slider
    
    the changed logic clarify the seperate cases we have with the loop mode and the slider.

---
+++ moveit_ros/visualization/rviz_plugin_render_tools/src/trajectory_panel.cpp
@@ -0,0 +1,141 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017, Yannick Jonetzko
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Yannick Jonetzko */
+
+#include <moveit/rviz_plugin_render_tools/trajectory_panel.h>
+#include <QHBoxLayout>
+
+namespace moveit_rviz_plugin
+{
+TrajectoryPanel::TrajectoryPanel(QWidget* parent) : Panel(parent)
+{
+}
+
+TrajectoryPanel::~TrajectoryPanel()
+{
+}
+
+void TrajectoryPanel::onInitialize()
+{
+  slider_ = new QSlider(Qt::Horizontal);
+  slider_->setTickInterval(1);
+  slider_->setMinimum(0);
+  slider_->setMaximum(0);
+  slider_->setTickPosition(QSlider::TicksBelow);
+  slider_->setPageStep(1);
+  connect(slider_, SIGNAL(valueChanged(int)), this, SLOT(sliderValueChanged(int)));
+
+  maximum_label_ = new QLabel(QString::number(slider_->maximum()));
+  minimum_label_ = new QLabel(QString::number(slider_->minimum()));
+  minimum_label_->setFixedWidth(20);
+
+  button_ = new QPushButton();
+  button_->setText("Pause");
+  button_->setEnabled(false);
+  connect(button_, SIGNAL(clicked()), this, SLOT(buttonClicked()));
+
+  QHBoxLayout* layout = new QHBoxLayout;
+  layout->addWidget(new QLabel("Waypoint:"));
+  layout->addWidget(minimum_label_);
+  layout->addWidget(slider_);
+  layout->addWidget(maximum_label_);
+  layout->addWidget(button_);
+  setLayout(layout);
+
+  paused_ = false;
+  parentWidget()->setVisible(false);
+}
+
+void TrajectoryPanel::onEnable()
+{
+  show();
+  parentWidget()->show();
+}
+
+void TrajectoryPanel::onDisable()
+{
+  hide();
+  paused_ = false;
+  parentWidget()->hide();
+}
+
+void TrajectoryPanel::update(int way_point_count)
+{
+  if (way_point_count != 0)
+  {
+    last_way_point_ = way_point_count - 1;
+    paused_ = false;
+    button_->setEnabled(true);
+    slider_->setSliderPosition(0);
+    slider_->setMaximum(way_point_count - 1);
+    maximum_label_->setText(QString::number(way_point_count - 1));
+  }
+}
+
+void TrajectoryPanel::pauseButton(bool pause)
+{
+  if (pause)
+  {
+    button_->setText("Play");
+    paused_ = true;
+  }
+  else
+  {
+    button_->setText("Pause");
+    paused_ = false;
+    if (slider_->sliderPosition() == last_way_point_)
+      slider_->setSliderPosition(0);
+  }
+}
+
+void TrajectoryPanel::setSliderPosition(int position)
+{
+  slider_->setSliderPosition(position);
+}
+
+void TrajectoryPanel::sliderValueChanged(int value)
+{
+  minimum_label_->setText(QString::number(value));
+}
+
+void TrajectoryPanel::buttonClicked()
+{
+  if (paused_)
+    pauseButton(false);
+  else
+    pauseButton(true);
+}
+
+}  // namespace moveit_rviz_plugin
