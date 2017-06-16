85a38e1e8791fc7cf228b09ae597b0c67466df10
v4hn <me@v4hn.de>
2017-06-16 19:42:02 +0200

    robot display: Don't reload robot model upon topic change
    Instead just reset it to the default configuration.
    This avoids needless urdf-parsing and retains a valid kstate_.
    For changes to the robot description parameter,
    there is a separate callback function.
    
    On Ubuntu 14.04 indigo this also fixes a segfault during startup,
    that seems to be related to calling loadRobotModel() twice in a row.
    
    Fixes #528

--- moveit_ros/visualization/robot_state_rviz_plugin/src/robot_state_display.cpp
+++ moveit_ros/visualization/robot_state_rviz_plugin/src/robot_state_display.cpp
@@ -292,10 +292,14 @@ void RobotStateDisplay::changedRobotSceneAlpha()
 void RobotStateDisplay::changedRobotStateTopic()
 {
   robot_state_subscriber_.shutdown();
+
+  // reset model to default state, we don't want to show previous messages
+  if (static_cast<bool>(kstate_))
+    kstate_->setToDefaultValues();
+  update_state_ = true;
+
   robot_state_subscriber_ = root_nh_.subscribe(robot_state_topic_property_->getStdString(), 10,
                                                &RobotStateDisplay::newRobotStateCallback, this);
-  robot_->clear();
-  loadRobotModel();
 }
 
 void RobotStateDisplay::newRobotStateCallback(const moveit_msgs::DisplayRobotStateConstPtr& state_msg)
