ec68e71317f4e22255b7dfbd548fb10c1101bc05
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-02-17 17:38:11 +0100

    jmg is also considered for collision checking

--- moveit_setup_assistant/src/widgets/planning_groups_widget.cpp
+++ moveit_setup_assistant/src/widgets/planning_groups_widget.cpp
@@ -105,7 +105,8 @@ PlanningGroupsWidget::PlanningGroupsWidget(QWidget* parent, moveit_setup_assista
   HeaderWidget* header = new HeaderWidget(
       "Planning Groups", "Create and edit planning groups for your robot based on joint collections, "
                          "link collections, kinematic chains or subgroups. "
-                         "A planning group defines the set of (joint, link) pairs considered for planning. "
+                         "A planning group defines the set of (joint, link) pairs considered for planning "
+                         "and collision checking. "
                          "Note: when adding a link to the group, its parent joint is added too and vice versa.\n"
                          "Define individual groups for each subset of the robot you want to plan for.",
       this);
