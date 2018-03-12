6a71b8da1cfa5303f948bcacdfa37a939d4c25ad
Bence Magyar <bence.magyar.robotics@gmail.com>
2018-03-12 15:18:16 +0000

    Update test of C++ MoveGroupInterface through Python wrappers

--- moveit_ros/planning_interface/test/python_move_group.py
+++ moveit_ros/planning_interface/test/python_move_group.py
@@ -7,6 +7,7 @@ import rostest
 import os
 
 from moveit_ros_planning_interface._moveit_move_group_interface import MoveGroupInterface
+from moveit_msgs.msg import MoveItErrorCodes
 
 
 class PythonMoveGroupTest(unittest.TestCase):
@@ -38,13 +39,21 @@ class PythonMoveGroupTest(unittest.TestCase):
 
     def plan(self, target):
         self.group.set_joint_value_target(target)
-        return self.group.compute_plan()
+        return self.group.plan()
 
     def test_validation(self):
         current = np.asarray(self.group.get_current_joint_values())
 
-        plan1 = self.plan(current + 0.2)
-        plan2 = self.plan(current + 0.2)
+        error_code1, plan1, time = self.plan(current + 0.2)
+        error_code2, plan2, time = self.plan(current + 0.2)
+
+        # both plans should have succeeded:
+        error_code = MoveItErrorCodes()
+        error_code.deserialize(error_code1)
+        self.assertEqual(error_code.val, MoveItErrorCodes.SUCCESS)
+        error_code = MoveItErrorCodes()
+        error_code.deserialize(error_code2)
+        self.assertEqual(error_code.val, MoveItErrorCodes.SUCCESS)
 
         # first plan should execute
         self.assertTrue(self.group.execute(plan1))
@@ -53,8 +62,11 @@ class PythonMoveGroupTest(unittest.TestCase):
         self.assertFalse(self.group.execute(plan2))
 
         # newly planned trajectory should execute again
-        plan3 = self.plan(current)
+        error_code3, plan3, time = self.plan(current)
         self.assertTrue(self.group.execute(plan3))
+        error_code = MoveItErrorCodes()
+        error_code.deserialize(error_code3)
+        self.assertEqual(error_code.val, MoveItErrorCodes.SUCCESS)
 
 
 if __name__ == '__main__':
