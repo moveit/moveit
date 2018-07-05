296d2ed7114824627dedbe79eb51464adeff69bf
Raghavender Sahdev <raghavendersahdev@gmail.com>
2018-07-05 13:35:35 -0400

    added STOMP Unit Tests

---
+++ moveit_planners/stomp/test/utest.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file utest.cpp
+ * @brief This executes the gtest code for stomp
+ *
+ * @author Jorge Nicho
+ * @date March 25, 2016
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2016, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+
+/** @brief This executes all tests for the stomp_core package */
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
