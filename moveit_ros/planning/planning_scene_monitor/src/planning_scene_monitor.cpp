b02ef29bee78f31744f7507d9db70d1677116cef
Robert Haschke <rhaschke@users.noreply.github.com>
2018-09-11 06:06:54 +0200

    Cherry-Pick #1033: Fix #868 (#1057)
    * Revert "disable waitForCurrentRobotState() for PlanService capability (#923)"
    
    This reverts commit cf2217c9e0c2f20054ddf272f7cb08a4553c06ef.
    
    * only waitForCurrentRobotState when start_state.is_diff
    
    as discussed in https://github.com/ros-planning/moveit/issues/868#issuecomment-393102371
    
    * reduce code duplication
    
    * use own EventQueue in PlanningSceneMonitor
    
    ... to become independent of any capabilities that might block the global queue
    
    * PlanningSceneMonitor: accept parent NodeHandle
    
    * add MIGRATION note

--- moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp
+++ moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp
@@ -117,28 +117,23 @@ const std::string planning_scene_monitor::PlanningSceneMonitor::MONITORED_PLANNI
 planning_scene_monitor::PlanningSceneMonitor::PlanningSceneMonitor(const std::string& robot_description,
                                                                    const boost::shared_ptr<tf::Transformer>& tf,
                                                                    const std::string& name)
-  : monitor_name_(name), nh_("~"), tf_(tf)
+  : PlanningSceneMonitor(planning_scene::PlanningScenePtr(), robot_description, tf, name)
 {
-  rm_loader_.reset(new robot_model_loader::RobotModelLoader(robot_description));
-  initialize(planning_scene::PlanningScenePtr());
 }
 
 planning_scene_monitor::PlanningSceneMonitor::PlanningSceneMonitor(const planning_scene::PlanningScenePtr& scene,
                                                                    const std::string& robot_description,
                                                                    const boost::shared_ptr<tf::Transformer>& tf,
                                                                    const std::string& name)
-  : monitor_name_(name), nh_("~"), tf_(tf)
+  : PlanningSceneMonitor(scene, std::make_shared<robot_model_loader::RobotModelLoader>(robot_description), tf, name)
 {
-  rm_loader_.reset(new robot_model_loader::RobotModelLoader(robot_description));
-  initialize(scene);
 }
 
 planning_scene_monitor::PlanningSceneMonitor::PlanningSceneMonitor(
     const robot_model_loader::RobotModelLoaderPtr& rm_loader, const boost::shared_ptr<tf::Transformer>& tf,
     const std::string& name)
-  : monitor_name_(name), nh_("~"), tf_(tf), rm_loader_(rm_loader)
+  : PlanningSceneMonitor(planning_scene::PlanningScenePtr(), rm_loader, tf, name)
 {
-  initialize(planning_scene::PlanningScenePtr());
 }
 
 planning_scene_monitor::PlanningSceneMonitor::PlanningSceneMonitor(
@@ -146,6 +141,20 @@ planning_scene_monitor::PlanningSceneMonitor::PlanningSceneMonitor(
     const boost::shared_ptr<tf::Transformer>& tf, const std::string& name)
   : monitor_name_(name), nh_("~"), tf_(tf), rm_loader_(rm_loader)
 {
+  root_nh_.setCallbackQueue(&queue_);
+  nh_.setCallbackQueue(&queue_);
+  spinner_.reset(new ros::AsyncSpinner(1, &queue_));
+  spinner_->start();
+  initialize(scene);
+}
+
+planning_scene_monitor::PlanningSceneMonitor::PlanningSceneMonitor(
+    const planning_scene::PlanningScenePtr& scene, const robot_model_loader::RobotModelLoaderPtr& rm_loader,
+    const ros::NodeHandle& nh, const boost::shared_ptr<tf::Transformer>& tf, const std::string& name)
+  : monitor_name_(name), nh_("~"), root_nh_(nh), tf_(tf), rm_loader_(rm_loader)
+{
+  // use same callback queue as root_nh_
+  nh_.setCallbackQueue(root_nh_.getCallbackQueue());
   initialize(scene);
 }
 
@@ -161,6 +170,7 @@ planning_scene_monitor::PlanningSceneMonitor::~PlanningSceneMonitor()
   stopWorldGeometryMonitor();
   stopSceneMonitor();
 
+  spinner_.reset();
   delete reconfigure_impl_;
   current_state_monitor_.reset();
   scene_const_.reset();
