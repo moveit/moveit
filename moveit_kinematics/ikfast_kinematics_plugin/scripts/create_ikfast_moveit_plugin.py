c60e6062f52f91ff56469094ebe399008f1441bb
Mike L <mike@picknik.ai>
2018-10-09 13:36:09 -0600

    python3 print formatting and flake8 linting

--- moveit_kinematics/ikfast_kinematics_plugin/scripts/create_ikfast_moveit_plugin.py
+++ moveit_kinematics/ikfast_kinematics_plugin/scripts/create_ikfast_moveit_plugin.py
@@ -8,8 +8,7 @@ kinematics service, from within the moveit planning environment, or in
 your own ROS node.
 
 Author: Michael Lautman, PickNik Robotics
-        Dave Coleman PickNik Robotics
-
+        Dave Coleman, PickNik Robotics
         Based heavily on the arm_kinematic_tools package by Jeremy Zoss, SwRI
         and the arm_navigation plugin generator by David Butterworth, KAIST
 
@@ -55,25 +54,41 @@ from lxml import etree
 import shutil
 import argparse
 
-plugin_gen_pkg = 'moveit_kinematics'  # package containing this file
-plugin_sub_dir = 'ikfast_kinematics_plugin' # sub directory which contains the template directory
+# Package containing this file
+plugin_gen_pkg = 'moveit_kinematics'
+# Sub directory which contains the template directory
+plugin_sub_dir = 'ikfast_kinematics_plugin'
 # Allowed search modes, see SEARCH_MODE enum in template file
-search_modes = ['OPTIMIZE_MAX_JOINT', 'OPTIMIZE_FREE_JOINT' ]
+search_modes = ['OPTIMIZE_MAX_JOINT', 'OPTIMIZE_FREE_JOINT']
 
 
 def create_parser():
-  parser = argparse.ArgumentParser(description="Generate an IKFast MoveIt! kinematic plugin")
-  parser.add_argument("robot_name", help="The name of your robot")
-  parser.add_argument("planning_group_name", help="The name of the planning group for which your IKFast solution was generated")
-  parser.add_argument("kinematic_plugin_pkg", help="The name of the MoveIt! IKFast Kinematics Plugin to be updated")
-  parser.add_argument("base_link_name", help="The name of the base link that was used when generating your IKFast solution")
-  parser.add_argument("eef_link_name", help="The name of the end effector link that was used when generating your IKFast solution")
-  parser.add_argument("ikfast_output_path", help="The full path to the analytic IK solution output by IKFast")
-  parser.add_argument("--search_mode", default=search_modes[0], help="The search mode used to solve IK for robots with more than 6DOF")
-  parser.add_argument("--srdf_filename", help="The name of your robot. If your robot's srdf is not <robot_name>.srdf, then set this value with the correct filename. Defaults to <robot_name>.srdf")
-  parser.add_argument("--robot_name_in_srdf", help="The name of your robot as defined in the srdf. Defaults to <robot_name>")
-  parser.add_argument("--moveit_config_pkg", help="The robot moveit_config pagkacge. Defaults to <robot_name>_moveit_config")
+  parser = argparse.ArgumentParser(
+      description="Generate an IKFast MoveIt! kinematic plugin")
+  parser.add_argument("robot_name",
+                      help="The name of your robot")
+  parser.add_argument("planning_group_name",
+                      help="The name of the planning group for which your IKFast solution was generated")
+  parser.add_argument("kinematic_plugin_pkg",
+                      help="The name of the MoveIt! IKFast Kinematics Plugin to be updated")
+  parser.add_argument("base_link_name",
+                      help="The name of the base link that was used when generating your IKFast solution")
+  parser.add_argument("eef_link_name",
+                      help="The name of the end effector link that was used when generating your IKFast solution")
+  parser.add_argument("ikfast_output_path",
+                      help="The full path to the analytic IK solution output by IKFast")
+  parser.add_argument("--search_mode",
+                      default=search_modes[0],
+                      help="The search mode used to solve IK for robots with more than 6DOF")
+  parser.add_argument("--srdf_filename",
+                      help="The name of your robot. If your robot's srdf is not <robot_name>.srdf, then set this value \
+                            with the correct filename. Defaults to <robot_name>.srdf")
+  parser.add_argument("--robot_name_in_srdf",
+                      help="The name of your robot as defined in the srdf. Defaults to <robot_name>")
+  parser.add_argument("--moveit_config_pkg",
+                      help="The robot moveit_config package. Defaults to <robot_name>_moveit_config")
   return parser
+
 
 def populate_optional(args):
   if args.srdf_filename is None:
@@ -83,17 +98,21 @@ def populate_optional(args):
   if args.moveit_config_pkg is None:
     args.moveit_config_pkg = args.robot_name + "_moveit_config"
 
+
 def print_args(args):
-  print "robot_name:", args.robot_name
-  print "base_link_name:", args.base_link_name
-  print "eef_link_name:", args.eef_link_name
-  print "planning_group_name:", args.planning_group_name
-  print "kinematic_plugin_pkg:", args.kinematic_plugin_pkg
-  print "ikfast_output_path:", args.ikfast_output_path
-  print "search_mode:", args.search_mode
-  print "srdf_filename:", args.srdf_filename
-  print "robot_name_in_srdf:", args.robot_name_in_srdf
-  print "moveit_config_pkg:", args.moveit_config_pkg
+  print("Creating IKFastKinematicsPlugin with parameters: ")
+  print(" robot_name:           %s" % args.robot_name)
+  print(" base_link_name:       %s" % args.base_link_name)
+  print(" eef_link_name:        %s" % args.eef_link_name)
+  print(" planning_group_name:  %s" % args.planning_group_name)
+  print(" kinematic_plugin_pkg: %s" % args.kinematic_plugin_pkg)
+  print(" ikfast_output_path:   %s" % args.ikfast_output_path)
+  print(" search_mode:          %s" % args.search_mode)
+  print(" srdf_filename:        %s" % args.srdf_filename)
+  print(" robot_name_in_srdf:   %s" % args.robot_name_in_srdf)
+  print(" moveit_config_pkg:    %s" % args.moveit_config_pkg)
+  print("")
+
 
 def update_deps(reqd_deps, req_type, e_parent):
   curr_deps = [e.text for e in e_parent.findall(req_type)]
@@ -101,35 +120,44 @@ def update_deps(reqd_deps, req_type, e_parent):
   for dep in missing_deps:
      etree.SubElement(e_parent, req_type).text = dep
 
+
 def update_package(args):
   try:
-    moveit_config_pkg_path = roslib.packages.get_pkg_dir(args.moveit_config_pkg)
-  except:
-    print "Failed to find package: %s" % args.moveit_config_pkg
+    moveit_config_pkg_path = roslib.packages.get_pkg_dir(
+        args.moveit_config_pkg)
+  except roslib.packages.InvalidROSPkgException:
+    print("Failed to find package: %s", args.moveit_config_pkg)
     sys.exit(-1)
 
   try:
-    kinematic_plugin_pkg_path = roslib.packages.get_pkg_dir(args.kinematic_plugin_pkg)
-  except:
-    print "Failed to find package: %s" % args.kinematic_plugin_pkg
+    kinematic_plugin_pkg_path = roslib.packages.get_pkg_dir(
+        args.kinematic_plugin_pkg)
+  except roslib.packages.InvalidROSPkgException:
+    print("Failed to find package: %s" % args.kinematic_plugin_pkg)
     sys.exit(-1)
 
   try:
     srdf_file_name = moveit_config_pkg_path + '/config/' + args.srdf_filename
     srdf = etree.parse(srdf_file_name).getroot()
-  except:
-    print "Failed to find srdf file: %s" % srdf_file_name
+    raise IOError
+  except IOError:
+    print("Failed to find SRDF file: %s" % srdf_file_name)
+    sys.exit(-1)
+  except etree.XMLSyntaxError as err:
+    print("Failed to parse xml in file %s" % srdf_file_name)
+    print(err.msg)
     sys.exit(-1)
 
   if args.robot_name_in_srdf != srdf.get('name'):
-    print "\nERROR: non-matching robot name found in '%s'. Expected '%s' but found '%s'" % (srdf_file_name,
-                                                                                            args.robot_name_in_srdf,
-                                                                                            srdf.get('name'))
+    print("\nERROR: non-matching robot name found in '%s'. Expected '%s' "
+          "but found '%s'" % (srdf_file_name,
+                              args.robot_name_in_srdf,
+                              srdf.get('name')))
     sys.exit(-1)
 
   groups = srdf.findall('group')
   if len(groups) < 1:
-    print "Failed to load planning group since no planning groups are defined in the SRDF"
+    print("Failed to load planning group since no planning groups are defined in the SRDF")
     sys.exit(-1)
 
   planning_group = None
@@ -138,9 +166,9 @@ def update_package(args):
       planning_group = group
 
   if planning_group is None:
-    print "Failed to load planning group '%s' since it is not defined in the SRDF. "\
-          "Available groups: \n%s" % (args.planning_group_name,
-                                      "".join([group_name.get('name') for group_name in groups]))
+    print("Failed to load planning group '%s' since it is not defined in the SRDF. Available groups: \n%s" % (
+        args.planning_group_name,
+        "".join([group_name.get('name') for group_name in groups])))
 
   kinematic_plugin_pkg_src_path = kinematic_plugin_pkg_path + '/src/'
   if not os.path.exists(kinematic_plugin_pkg_src_path):
@@ -150,19 +178,23 @@ def update_package(args):
   if not os.path.exists(kinematic_plugin_pkg_include_path):
      os.makedirs(kinematic_plugin_pkg_include_path)
 
-
   if not os.path.exists(args.ikfast_output_path):
-    print "\nERROR: can't find IKFast source code at '%s'\n" % args.ikfast_output_path
+    print("\nERROR: can't find IKFast source code at '%s'\n" % args.ikfast_output_path)
     sys.exit(-1)
 
   # Copy the source code generated by IKFast into our src folder
-  solver_file_path = kinematic_plugin_pkg_src_path + args.robot_name + '_' + args.planning_group_name + '_ikfast_solver.cpp'
+  solver_file_path = \
+      kinematic_plugin_pkg_src_path + \
+      args.robot_name + \
+      '_' + \
+      args.planning_group_name + \
+      '_ikfast_solver.cpp'
 
   # Check if they are the same file - if so, skip
   skip = False
   if os.path.exists(args.ikfast_output_path) and os.path.exists(solver_file_path):
     if os.path.samefile(args.ikfast_output_path, solver_file_path):
-       print 'Skipping copying ' + solver_file_path + ' since it is already in the correct location'
+       print("Skipping copying %s since it is already in the correct location" % solver_file_path)
        skip = True
 
   if not skip:
@@ -170,52 +202,52 @@ def update_package(args):
 
   # Error check
   if not os.path.exists(solver_file_path):
-    print "\nERROR: Unable to copy IKFast source code from '%s' to '%s'" % (args.ikfast_output_path, solver_file_path)
-    print 'Manually copy the source file generated by IKFast to this location and re-run'
+    print("\nERROR: Unable to copy IKFast source code from '%s' to '%s'" % (args.ikfast_output_path, solver_file_path))
+    print('Manually copy the source file generated by IKFast to this location and re-run\n')
     sys.exit(-1)
 
   # Detect version of IKFast used to generate solver code
   solver_version = 0
-  with open(solver_file_path,'r') as src:
+  with open(solver_file_path, 'r') as src:
     for line in src:
        if line.startswith('/// ikfast version'):
           line_search = re.search('ikfast version (.*) generated', line)
           if line_search:
              solver_version = int(line_search.group(1), 0)
           break
-  print 'Found source code generated by IKFast version %s' % str(solver_version)
+  print('Found source code generated by IKFast version %s' % str(solver_version))
 
   # Chose template depending on IKFast version
   if solver_version >= 56:
     template_version = 61
   else:
-    print 'ERROR this converter is not made for IKFast 54 or anything but 61'
+    print('ERROR this converter is not made for IKFast 54 or anything but 61')
     sys.exit(-1)
 
   # Get template folder location
   try:
     plugin_gen_dir = os.path.join(roslib.packages.get_pkg_dir(plugin_gen_pkg), plugin_sub_dir)
-  except:
-    print "ERROR: can't find package " + plugin_gen_pkg
+  except roslib.packages.InvalidROSPkgException:
+    print("ERROR: can't find package %s" % plugin_gen_pkg)
     sys.exit(-1)
 
   # Check if IKFast header file exists
   template_header_path = plugin_gen_dir + '/templates/ikfast.h'
   if not os.path.exists(template_header_path):
-    print "ERROR: can't find ikfast header file at '%s'" % template_header_path
+    print("ERROR: can't find ikfast header file at '%s'" % template_header_path)
     sys.exit(-1)
 
   # Copy the IKFast header file into the include directory
   output_header_path = kinematic_plugin_pkg_include_path + "ikfast.h"
   shutil.copy2(template_header_path, output_header_path)
   if not os.path.exists(output_header_path):
-    print "ERROR: Unable to copy IKFast header file from '%s' to '%s'" % (template_header_path, output_header_path)
+    print("ERROR: Unable to copy IKFast header file from '%s' to '%s'" % (template_header_path, output_header_path))
     sys.exit(-1)
 
   # Check if template exists
   template_src_path = plugin_gen_dir + '/templates/ikfast' + str(template_version) + '_moveit_plugin_template.cpp'
   if not os.path.exists(template_src_path):
-    print "ERROR: can't find template file at '%s' " % template_src_path
+    print("ERROR: can't find template file at '%s' " % template_src_path)
     sys.exit(-1)
 
   # Create plugin source from template
@@ -227,37 +259,43 @@ def update_package(args):
   template_text = re.sub('_EEF_LINK_', args.eef_link_name, template_text)
   template_text = re.sub('_BASE_LINK_', args.base_link_name, template_text)
 
-  output_src_path = kinematic_plugin_pkg_src_path + args.robot_name + '_' + args.planning_group_name + "_ikfast_moveit_plugin.cpp"
+  output_src_path = \
+      kinematic_plugin_pkg_src_path + \
+      args.robot_name + \
+      '_' + \
+      args.planning_group_name + \
+      "_ikfast_moveit_plugin.cpp"
+
   with open(output_src_path, 'w') as f:
     f.write(template_text)
-  print "Created plugin file at '%s'" % output_src_path
+  print("Created plugin file at '%s'" % output_src_path)
 
   # Create plugin definition .xml file
   ik_library_name = args.robot_name + "_" + args.planning_group_name + "_moveit_ikfast_plugin"
   plugin_name = args.robot_name + '_' + args.planning_group_name + '_kinematics/IKFastKinematicsPlugin'
-  plugin_def = etree.Element("library", path="lib/lib"+ik_library_name)
+  plugin_def = etree.Element("library", path="lib/lib" + ik_library_name)
   cl = etree.SubElement(plugin_def, "class")
   cl.set("name", plugin_name)
   cl.set("type", 'ikfast_kinematics_plugin::IKFastKinematicsPlugin')
   cl.set("base_class_type", "kinematics::KinematicsBase")
   desc = etree.SubElement(cl, "description")
-  desc.text = 'IKFast'+str(template_version)+' plugin for closed-form kinematics'
+  desc.text = 'IKFast' + str(template_version) + ' plugin for closed-form kinematics'
 
   # Write plugin definition to file
   plugin_file_name = ik_library_name + "_description.xml"
   plugin_file_path = kinematic_plugin_pkg_path + "/" + plugin_file_name
-  with open(plugin_file_path,'w') as f:
+  with open(plugin_file_path, 'w') as f:
     etree.ElementTree(plugin_def).write(f, xml_declaration=True, pretty_print=True, encoding="UTF-8")
-  print "Created plugin definition at: '%s'" % plugin_file_path
+  print("Created plugin definition at: '%s'" % plugin_file_path)
 
   # Check if CMakeLists file exists
   cmake_template_file = plugin_gen_dir + "/templates/CMakeLists.txt"
   if not os.path.exists(cmake_template_file):
-    print "ERROR: can't find CMakeLists template file at '%s'" % cmake_template_file
+    print("ERROR: can't find CMakeLists template file at '%s'" % cmake_template_file)
     sys.exit(-1)
 
   # Create new CMakeLists file
-  cmake_file = kinematic_plugin_pkg_path+'/CMakeLists.txt'
+  cmake_file = kinematic_plugin_pkg_path + '/CMakeLists.txt'
 
   # Create plugin source from template
   template_file_data = open(cmake_template_file, 'r')
@@ -269,16 +307,16 @@ def update_package(args):
 
   with open(cmake_file, 'w') as f:
     f.write(template_text)
-  print "Overwrote CMakeLists file at '%s'" % cmake_file
+  print("Overwrote CMakeLists file at '%s'" % cmake_file)
 
   # Add plugin export to package manifest
   parser = etree.XMLParser(remove_blank_text=True)
-  package_file_name = kinematic_plugin_pkg_path+"/package.xml"
+  package_file_name = kinematic_plugin_pkg_path + "/package.xml"
   package_xml = etree.parse(package_file_name, parser)
 
   # Make sure at least all required dependencies are in the depends lists
   build_deps = ["liblapack-dev", "moveit_core", "pluginlib", "roscpp", "tf_conversions", "moveit_ros_planning"]
-  run_deps   = ["liblapack-dev", "moveit_core", "pluginlib", "roscpp", "tf_conversions", "moveit_ros_planning"]
+  run_deps = ["liblapack-dev", "moveit_core", "pluginlib", "roscpp", "tf_conversions", "moveit_ros_planning"]
 
   update_deps(build_deps, "build_depend", package_xml.getroot())
   update_deps(run_deps, "exec_depend", package_xml.getroot())
@@ -288,13 +326,14 @@ def update_package(args):
                              plugin="${prefix}/" + plugin_file_name)
 
   export_element = package_xml.getroot().find("export")
-  if export_element == None:
+  if export_element is None:
     export_element = etree.SubElement(package_xml.getroot(), "export")
 
   found = False
   for el in export_element.findall("moveit_core"):
     found = (etree.tostring(new_export) == etree.tostring(el))
-    if found: break
+    if found:
+      break
 
   if not found:
     export_element.append(new_export)
@@ -303,39 +342,43 @@ def update_package(args):
   # proper encodings are used in the future (UTF-8)
   with open(package_file_name, "w") as f:
     package_xml.write(f, xml_declaration=True, pretty_print=True, encoding="UTF-8")
-  print "Modified package.xml at '%s'" % package_file_name
+  print("Modified package.xml at '%s'" % package_file_name)
 
   # Modify kinematics.yaml file
-  kin_yaml_file_name = moveit_config_pkg_path+"/config/kinematics.yaml"
+  kin_yaml_file_name = moveit_config_pkg_path + "/config/kinematics.yaml"
   with open(kin_yaml_file_name, 'r') as f:
     kin_yaml_data = yaml.safe_load(f)
+
   kin_yaml_data[args.planning_group_name]["kinematics_solver"] = plugin_name
   with open(kin_yaml_file_name, 'w') as f:
     yaml.dump(kin_yaml_data, f, default_flow_style=False)
-  print "Modified kinematics.yaml at '%s'" % kin_yaml_file_name
+
+  print("Modified kinematics.yaml at '%s'" % kin_yaml_file_name)
 
   # Create a script for easily updating the plugin in the future in case the plugin needs to be updated
   easy_script_file_name = "update_ikfast_plugin.sh"
   easy_script_file_path = kinematic_plugin_pkg_path + "/" + easy_script_file_name
-  with open(easy_script_file_path,'w') as f:
-    f.write("rosrun moveit_kinematics create_ikfast_moveit_plugin.py"
-            + " --search_mode=" + args.search_mode
-            + " --srdf_filename=" + args.srdf_filename
-            + " --robot_name_in_srdf=" + args.robot_name_in_srdf
-            + " --moveit_config_pkg=" + args.moveit_config_pkg
-            + " " + args.robot_name
-            + " " + args.planning_group_name
-            + " " + args.kinematic_plugin_pkg
-            + " " + args.base_link_name
-            + " " + args.eef_link_name
-            + " " + solver_file_path )
+  with open(easy_script_file_path, 'w') as f:
+    f.write("rosrun moveit_kinematics create_ikfast_moveit_plugin.py" +
+            " --search_mode=" + args.search_mode +
+            " --srdf_filename=" + args.srdf_filename +
+            " --robot_name_in_srdf=" + args.robot_name_in_srdf +
+            " --moveit_config_pkg=" + args.moveit_config_pkg +
+            " " + args.robot_name +
+            " " + args.planning_group_name +
+            " " + args.kinematic_plugin_pkg +
+            " " + args.base_link_name +
+            " " + args.eef_link_name +
+            " " + solver_file_path)
 
-  print "Created update plugin script at '%s'" % easy_script_file_path
+  print("Created update plugin script at '%s'" % easy_script_file_path)
+
 
 def main(args):
   populate_optional(args)
   print_args(args)
   update_package(args)
+
 
 if __name__ == '__main__':
   parser = create_parser()
