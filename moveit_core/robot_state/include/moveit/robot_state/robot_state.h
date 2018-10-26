6136fba52f9060922f23fd105d328f1f57d68901
Michael Lautman <mike@picknik.ai>
2018-10-25 18:03:44 -0600

    separating the cartesian planner

--- moveit_core/robot_state/include/moveit/robot_state/robot_state.h
+++ moveit_core/robot_state/include/moveit/robot_state/robot_state.h
@@ -63,46 +63,6 @@ typedef boost::function<bool(RobotState* robot_state, const JointModelGroup* joi
                              const double* joint_group_variable_values)>
     GroupStateValidityCallbackFn;
 
-/** \brief Struct for containing jump_threshold.
-
-    For the purposes of maintaining API, we support both \e jump_threshold_factor which provides a scaling factor for
-    detecting joint space jumps and \e revolute_jump_threshold and \e prismatic_jump_threshold which provide abolute
-    thresholds for detecting joint space jumps. */
-struct JumpThreshold
-{
-  double factor;
-  double revolute;   // Radians
-  double prismatic;  // Meters
-
-  explicit JumpThreshold() : factor(0.0), revolute(0.0), prismatic(0.0)
-  {
-  }
-
-  explicit JumpThreshold(double jt_factor) : JumpThreshold()
-  {
-    factor = jt_factor;
-  }
-
-  explicit JumpThreshold(double jt_revolute, double jt_prismatic) : JumpThreshold()
-  {
-    revolute = jt_revolute;    // Radians
-    prismatic = jt_prismatic;  // Meters
-  }
-};
-
-/** \brief Struct for containing max_step for computeCartesianPath
-
-    Setting translation to zero will disable checking for translations and the same goes for rotation */
-struct MaxEEFStep
-{
-  MaxEEFStep(double translation = 0.0, double rotation = 0.0) : translation(translation), rotation(rotation)
-  {
-  }
-
-  double translation;  // Meters
-  double rotation;     // Radians
-};
-
 /** \brief Representation of a robot's state. This includes position,
     velocity, acceleration and effort.
 
@@ -1155,113 +1115,48 @@ as the new values that correspond to the group */
 
      For absolute jump thresholds, if any individual joint-space motion delta is larger then \e revolute_jump_threshold
      for revolute joints or \e prismatic_jump_threshold for prismatic joints then this step is considered a failure and
-     the returned path is truncated up to just before the jump.*/
-  double computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
-                              const Eigen::Vector3d& direction, bool global_reference_frame, double distance,
-                              const MaxEEFStep& max_step, const JumpThreshold& jump_threshold,
-                              const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
-                              const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions());
+     the returned path is truncated up to just before the jump.
 
-  double computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
-                              const Eigen::Vector3d& direction, bool global_reference_frame, double distance,
-                              double max_step, double jump_threshold_factor,
-                              const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
-                              const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions())
-  {
-    return computeCartesianPath(group, traj, link, direction, global_reference_frame, distance,
-                                MaxEEFStep(max_step, max_step), JumpThreshold(jump_threshold_factor), validCallback,
-                                options);
-  }
+     NOTE: As of ROS-Melodic these are deprecated and should not be used
+     */
+  MOVEIT_DEPRECATED double
+  computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
+                       const Eigen::Vector3d& direction, bool global_reference_frame, double distance, double max_step,
+                       double jump_threshold_factor,
+                       const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
+                       const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions());
 
   /** \brief Compute the sequence of joint values that correspond to a straight Cartesian path, for a particular group.
 
      In contrast to the previous function, the Cartesian path is specified as a target frame to be reached (\e target)
      for the origin of a robot link (\e link). The target frame is assumed to be either in a global reference frame or
      in the local reference frame of the link. In the latter case (\e global_reference_frame is false) the \e target is
-     rotated accordingly. All other comments from the previous function apply. */
-  double computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
-                              const Eigen::Isometry3d& target, bool global_reference_frame, const MaxEEFStep& max_step,
-                              const JumpThreshold& jump_threshold,
-                              const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
-                              const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions());
+     rotated accordingly. All other comments from the previous function apply.
 
-  double computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
-                              const Eigen::Isometry3d& target, bool global_reference_frame, double max_step,
-                              double jump_threshold_factor,
-                              const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
-                              const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions())
-  {
-    return computeCartesianPath(group, traj, link, target, global_reference_frame, MaxEEFStep(max_step),
-                                JumpThreshold(jump_threshold_factor), validCallback, options);
-  }
+     NOTE: As of ROS-Melodic these are deprecated and should not be used
+     */
+  MOVEIT_DEPRECATED double
+  computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
+                       const Eigen::Isometry3d& target, bool global_reference_frame, double max_step,
+                       double jump_threshold_factor,
+                       const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
+                       const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions());
 
   /** \brief Compute the sequence of joint values that perform a general Cartesian path.
 
      In contrast to the previous functions, the Cartesian path is specified as a set of \e waypoints to be sequentially
      reached for the origin of a robot link (\e link). The waypoints are transforms given either in a global reference
      frame or in the local reference frame of the link at the immediately preceeding waypoint. The link needs to move
-     in a straight line between two consecutive waypoints. All other comments apply. */
-  double computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
-                              const EigenSTL::vector_Isometry3d& waypoints, bool global_reference_frame,
-                              const MaxEEFStep& max_step, const JumpThreshold& jump_threshold,
-                              const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
-                              const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions());
+     in a straight line between two consecutive waypoints. All other comments apply.
 
-  double computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
-                              const EigenSTL::vector_Isometry3d& waypoints, bool global_reference_frame,
-                              double max_step, double jump_threshold_factor,
-                              const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
-                              const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions())
-  {
-    return computeCartesianPath(group, traj, link, waypoints, global_reference_frame, MaxEEFStep(max_step),
-                                JumpThreshold(jump_threshold_factor), validCallback, options);
-  }
-
-  /** \brief Tests joint space jumps of a trajectory.
-
-     If \e jump_threshold_factor is non-zero, we test for relative jumps.
-     If \e revolute_jump_threshold  or \e prismatic_jump_threshold are non-zero, we test for absolute jumps.
-     Both tests can be combined. If all params are zero, jump detection is disabled.
-     For relative jump detection, the average joint-space distance between consecutive points in the trajectory is
-     computed. If any individual joint-space motion delta is larger then this average distance by a factor of
-     \e jump_threshold_factor, this step is considered a failure and the returned path is truncated up to just
-     before the jump.
-
-     @param group The joint model group of the robot state.
-     @param traj The trajectory that should be tested.
-     @param jump_threshold The struct holding jump thresholds to determine if a joint space jump has occurred.
-     @return The fraction of the trajectory that passed.
-  */
-  static double testJointSpaceJump(const JointModelGroup* group, std::vector<RobotStatePtr>& traj,
-                                   const JumpThreshold& jump_threshold);
-
-  /** \brief Tests for relative joint space jumps of the trajectory \e traj.
-
-     First, the average distance between adjacent trajectory points is computed. If two adjacent trajectory points
-     have distance > \e jump_threshold_factor * average, the trajectory is truncated at this point.
-
-     @param group The joint model group of the robot state.
-     @param traj The trajectory that should be tested.
-     @param jump_threshold_factor The threshold to determine if a joint space jump has occurred .
-     @return The fraction of the trajectory that passed.
-  */
-  static double testRelativeJointSpaceJump(const JointModelGroup* group, std::vector<RobotStatePtr>& traj,
-                                           double jump_threshold_factor);
-
-  /** \brief Tests for absolute joint space jumps of the trajectory \e traj.
-
-     The joint-space difference between consecutive waypoints is computed for each active joint and compared to the
-     absolute thresholds \e revolute_jump_threshold for revolute joints and \e prismatic_jump_threshold for prismatic
-     joints. If these thresholds are exceeded, the trajectory is truncated.
-
-     @param group The joint model group of the robot state.
-     @param traj The trajectory that should be tested.
-     @param revolute_jump_threshold Absolute joint-space threshold for revolute joints.
-     @param prismatic_jump_threshold Absolute joint-space threshold for prismatic joints.
-     @return The fraction of the trajectory that passed.
-  */
-  static double testAbsoluteJointSpaceJump(const JointModelGroup* group, std::vector<RobotStatePtr>& traj,
-                                           double revolute_jump_threshold, double prismatic_jump_threshold);
+     NOTE: As of ROS-Melodic these are deprecated and should not be used
+     */
+  MOVEIT_DEPRECATED double
+  computeCartesianPath(const JointModelGroup* group, std::vector<RobotStatePtr>& traj, const LinkModel* link,
+                       const EigenSTL::vector_Isometry3d& waypoints, bool global_reference_frame, double max_step,
+                       double jump_threshold_factor,
+                       const GroupStateValidityCallbackFn& validCallback = GroupStateValidityCallbackFn(),
+                       const kinematics::KinematicsQueryOptions& options = kinematics::KinematicsQueryOptions());
 
   /** \brief Compute the Jacobian with reference to a particular point on a given link, for a specified group.
    * \param group The group to compute the Jacobian for
---
+++ moveit_core/robot_state/src/cartesian_interpolator.cpp
@@ -0,0 +1,319 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2013, Ioan A. Sucan
+*  Copyright (c) 2013, Willow Garage, Inc.
+*  Copyright (c) 2019, PickNik LLC.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Ioan Sucan, Sachin Chitta, Acorn Pooley, Mario Prats, Dave Coleman */
+
+#include <moveit/robot_state/cartesian_interpolator.h>
+
+namespace moveit
+{
+namespace core
+{
+/** \brief It is recommended that there are at least 10 steps per trajectory
+ * for testing jump thresholds with computeCartesianPath. With less than 10 steps
+ * it is difficult to choose a jump_threshold parameter that effectively separates
+ * valid paths from paths with large joint space jumps. */
+static const std::size_t MIN_STEPS_FOR_JUMP_THRESH = 10;
+
+const std::string LOGNAME = "cartesian_interpolator";
+
+double CartesianInterpolator::computeCartesianPath(RobotState* start_state, const JointModelGroup* group,
+                                                   std::vector<RobotStatePtr>& traj, const LinkModel* link,
+                                                   const Eigen::Vector3d& direction, bool global_reference_frame,
+                                                   double distance, const MaxEEFStep& max_step,
+                                                   const JumpThreshold& jump_threshold,
+                                                   const GroupStateValidityCallbackFn& validCallback,
+                                                   const kinematics::KinematicsQueryOptions& options)
+{
+  // this is the Cartesian pose we start from, and have to move in the direction indicated
+  const Eigen::Isometry3d& start_pose = start_state->getGlobalLinkTransform(link);
+
+  // the direction can be in the local reference frame (in which case we rotate it)
+  const Eigen::Vector3d rotated_direction = global_reference_frame ? direction : start_pose.rotation() * direction;
+
+  // The target pose is built by applying a translation to the start pose for the desired direction and distance
+  Eigen::Isometry3d target_pose = start_pose;
+  target_pose.translation() += rotated_direction * distance;
+
+  // call computeCartesianPath for the computed target pose in the global reference frame
+  return (distance * computeCartesianPath(start_state, group, traj, link, target_pose, true, max_step, jump_threshold,
+                                          validCallback, options));
+}
+
+double CartesianInterpolator::computeCartesianPath(RobotState* start_state, const JointModelGroup* group,
+                                                   std::vector<RobotStatePtr>& traj, const LinkModel* link,
+                                                   const Eigen::Isometry3d& target, bool global_reference_frame,
+                                                   const MaxEEFStep& max_step, const JumpThreshold& jump_threshold,
+                                                   const GroupStateValidityCallbackFn& validCallback,
+                                                   const kinematics::KinematicsQueryOptions& options)
+{
+  const std::vector<const JointModel*>& cjnt = group->getContinuousJointModels();
+  // make sure that continuous joints wrap
+  for (std::size_t i = 0; i < cjnt.size(); ++i)
+    start_state->enforceBounds(cjnt[i]);
+
+  // this is the Cartesian pose we start from, and we move in the direction indicated
+  Eigen::Isometry3d start_pose = start_state->getGlobalLinkTransform(link);
+
+  // the target can be in the local reference frame (in which case we rotate it)
+  Eigen::Isometry3d rotated_target = global_reference_frame ? target : start_pose * target;
+
+  Eigen::Quaterniond start_quaternion(start_pose.rotation());
+  Eigen::Quaterniond target_quaternion(rotated_target.rotation());
+
+  if (max_step.translation <= 0.0 && max_step.rotation <= 0.0)
+  {
+    ROS_ERROR_NAMED(LOGNAME,
+                    "Invalid MaxEEFStep passed into computeCartesianPath. Both the MaxEEFStep.rotation and "
+                    "MaxEEFStep.translation components must be non-negative and at least one component must be "
+                    "greater than zero");
+    return 0.0;
+  }
+
+  double rotation_distance = start_quaternion.angularDistance(target_quaternion);
+  double translation_distance = (rotated_target.translation() - start_pose.translation()).norm();
+
+  // decide how many steps we will need for this trajectory
+  std::size_t translation_steps = 0;
+  if (max_step.translation > 0.0)
+    translation_steps = floor(translation_distance / max_step.translation);
+
+  std::size_t rotation_steps = 0;
+  if (max_step.rotation > 0.0)
+    rotation_steps = floor(rotation_distance / max_step.rotation);
+
+  // If we are testing for relative jumps, we always want at least MIN_STEPS_FOR_JUMP_THRESH steps
+  std::size_t steps = std::max(translation_steps, rotation_steps) + 1;
+  if (jump_threshold.factor > 0 && steps < MIN_STEPS_FOR_JUMP_THRESH)
+    steps = MIN_STEPS_FOR_JUMP_THRESH;
+
+  // To limit absolute joint-space jumps, we pass consistency limits to the IK solver
+  std::vector<double> consistency_limits;
+  if (jump_threshold.prismatic > 0 || jump_threshold.revolute > 0)
+    for (const JointModel* jm : group->getActiveJointModels())
+    {
+      double limit;
+      switch (jm->getType())
+      {
+        case JointModel::REVOLUTE:
+          limit = jump_threshold.revolute;
+          break;
+        case JointModel::PRISMATIC:
+          limit = jump_threshold.prismatic;
+          break;
+        default:
+          limit = 0.0;
+      }
+      if (limit == 0.0)
+        limit = jm->getMaximumExtent();
+      consistency_limits.push_back(limit);
+    }
+
+  traj.clear();
+  traj.push_back(RobotStatePtr(new robot_state::RobotState(*start_state)));
+
+  double last_valid_percentage = 0.0;
+  for (std::size_t i = 1; i <= steps; ++i)
+  {
+    double percentage = (double)i / (double)steps;
+
+    Eigen::Isometry3d pose(start_quaternion.slerp(percentage, target_quaternion));
+    pose.translation() = percentage * rotated_target.translation() + (1 - percentage) * start_pose.translation();
+
+    // Explicitly use a single IK attempt only: We want a smooth trajectory.
+    // Random seeding (of additional attempts) would probably create IK jumps.
+    if (start_state->setFromIK(group, pose, link->getName(), consistency_limits, 0.0, validCallback, options))
+      traj.push_back(RobotStatePtr(new robot_state::RobotState(*start_state)));
+    else
+      break;
+
+    last_valid_percentage = percentage;
+  }
+
+  last_valid_percentage *= checkJointSpaceJump(group, traj, jump_threshold);
+
+  return last_valid_percentage;
+}
+
+double CartesianInterpolator::computeCartesianPath(RobotState* start_state, const JointModelGroup* group,
+                                                   std::vector<RobotStatePtr>& traj, const LinkModel* link,
+                                                   const EigenSTL::vector_Isometry3d& waypoints,
+                                                   bool global_reference_frame, const MaxEEFStep& max_step,
+                                                   const JumpThreshold& jump_threshold,
+                                                   const GroupStateValidityCallbackFn& validCallback,
+                                                   const kinematics::KinematicsQueryOptions& options)
+{
+  double percentage_solved = 0.0;
+  for (std::size_t i = 0; i < waypoints.size(); ++i)
+  {
+    // Don't test joint space jumps for every waypoint, test them later on the whole trajectory.
+    static const JumpThreshold NO_JOINT_SPACE_JUMP_TEST;
+    std::vector<RobotStatePtr> waypoint_traj;
+    double wp_percentage_solved =
+        computeCartesianPath(start_state, group, waypoint_traj, link, waypoints[i], global_reference_frame, max_step,
+                             NO_JOINT_SPACE_JUMP_TEST, validCallback, options);
+    if (fabs(wp_percentage_solved - 1.0) < std::numeric_limits<double>::epsilon())
+    {
+      percentage_solved = (double)(i + 1) / (double)waypoints.size();
+      std::vector<RobotStatePtr>::iterator start = waypoint_traj.begin();
+      if (i > 0 && !waypoint_traj.empty())
+        std::advance(start, 1);
+      traj.insert(traj.end(), start, waypoint_traj.end());
+    }
+    else
+    {
+      percentage_solved += wp_percentage_solved / (double)waypoints.size();
+      std::vector<RobotStatePtr>::iterator start = waypoint_traj.begin();
+      if (i > 0 && !waypoint_traj.empty())
+        std::advance(start, 1);
+      traj.insert(traj.end(), start, waypoint_traj.end());
+      break;
+    }
+  }
+
+  percentage_solved *= checkJointSpaceJump(group, traj, jump_threshold);
+
+  return percentage_solved;
+}
+
+double CartesianInterpolator::checkJointSpaceJump(const JointModelGroup* group, std::vector<RobotStatePtr>& traj,
+                                                  const JumpThreshold& jump_threshold)
+{
+  double percentage_solved = 1.0;
+  if (traj.size() <= 1)
+    return percentage_solved;
+
+  if (jump_threshold.factor > 0.0)
+    percentage_solved *= checkRelativeJointSpaceJump(group, traj, jump_threshold.factor);
+
+  if (jump_threshold.revolute > 0.0 || jump_threshold.prismatic > 0.0)
+    percentage_solved *= checkAbsoluteJointSpaceJump(group, traj, jump_threshold.revolute, jump_threshold.prismatic);
+
+  return percentage_solved;
+}
+
+double CartesianInterpolator::checkRelativeJointSpaceJump(const JointModelGroup* group,
+                                                          std::vector<RobotStatePtr>& traj,
+                                                          double jump_threshold_factor)
+{
+  if (traj.size() < MIN_STEPS_FOR_JUMP_THRESH)
+  {
+    ROS_WARN_NAMED(LOGNAME, "The computed trajectory is too short to detect jumps in joint-space "
+                            "Need at least %zu steps, only got %zu. Try a lower max_step.",
+                   MIN_STEPS_FOR_JUMP_THRESH, traj.size());
+  }
+
+  std::vector<double> dist_vector;
+  dist_vector.reserve(traj.size() - 1);
+  double total_dist = 0.0;
+  for (std::size_t i = 1; i < traj.size(); ++i)
+  {
+    double dist_prev_point = traj[i]->distance(*traj[i - 1], group);
+    dist_vector.push_back(dist_prev_point);
+    total_dist += dist_prev_point;
+  }
+
+  double percentage = 1.0;
+  // compute the average distance between the states we looked at
+  double thres = jump_threshold_factor * (total_dist / (double)dist_vector.size());
+  for (std::size_t i = 0; i < dist_vector.size(); ++i)
+    if (dist_vector[i] > thres)
+    {
+      ROS_DEBUG_NAMED(LOGNAME, "Truncating Cartesian path due to detected jump in joint-space distance");
+      percentage = (double)(i + 1) / (double)(traj.size());
+      traj.resize(i + 1);
+      break;
+    }
+
+  return percentage;
+}
+
+double CartesianInterpolator::checkAbsoluteJointSpaceJump(const JointModelGroup* group,
+                                                          std::vector<RobotStatePtr>& traj, double revolute_threshold,
+                                                          double prismatic_threshold)
+{
+  bool check_revolute = revolute_threshold > 0.0;
+  bool check_prismatic = prismatic_threshold > 0.0;
+
+  double joint_threshold;
+  bool check_joint;
+  bool still_valid = true;
+  const std::vector<const JointModel*>& joints = group->getActiveJointModels();
+  for (std::size_t traj_ix = 0, ix_end = traj.size() - 1; traj_ix != ix_end; ++traj_ix)
+  {
+    for (auto& joint : joints)
+    {
+      switch (joint->getType())
+      {
+        case JointModel::REVOLUTE:
+          check_joint = check_revolute;
+          joint_threshold = revolute_threshold;
+          break;
+        case JointModel::PRISMATIC:
+          check_joint = check_prismatic;
+          joint_threshold = prismatic_threshold;
+          break;
+        default:
+          ROS_WARN_NAMED(LOGNAME, "Joint %s has not supported type %s. \n"
+                                  "checkAbsoluteJointSpaceJump only supports prismatic and revolute joints.",
+                         joint->getName().c_str(), joint->getTypeName().c_str());
+          continue;
+      }
+      if (check_joint)
+      {
+        double distance = traj[traj_ix]->distance(*traj[traj_ix + 1], joint);
+        if (distance > joint_threshold)
+        {
+          ROS_DEBUG_NAMED(LOGNAME, "Truncating Cartesian path due to detected jump of %.4f > %.4f in joint %s",
+                          distance, joint_threshold, joint->getName().c_str());
+          still_valid = false;
+          break;
+        }
+      }
+    }
+
+    if (!still_valid)
+    {
+      double percent_valid = (double)(traj_ix + 1) / (double)(traj.size());
+      traj.resize(traj_ix + 1);
+      return percent_valid;
+    }
+  }
+  return 1.0;
+}
+
+}  // end of namespace core
+}  // end of namespace moveit
