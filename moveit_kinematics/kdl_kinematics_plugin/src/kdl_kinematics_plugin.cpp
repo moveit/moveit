6cda52babe508546f8d28b9abc08f33648d27ce1
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2019-01-04 11:35:16 +0100

    decrease step size when running into singularities

--- moveit_kinematics/kdl_kinematics_plugin/src/kdl_kinematics_plugin.cpp
+++ moveit_kinematics/kdl_kinematics_plugin/src/kdl_kinematics_plugin.cpp
@@ -370,32 +370,58 @@ bool KDLKinematicsPlugin::searchPositionIK(const geometry_msgs::Pose& ik_pose, c
 int KDLKinematicsPlugin::CartToJnt(KDL::ChainIkSolverVel& ik_solver, const KDL::JntArray& q_init,
                                    const KDL::Frame& p_in, KDL::JntArray& q_out, const unsigned int max_iter) const
 {
+  double last_delta_twist_norm = DBL_MAX;
+  double step_size = 1.0;
   KDL::Frame f;
   KDL::Twist delta_twist;
-  KDL::JntArray delta_q(q_out.rows());
+  KDL::JntArray delta_q(q_out.rows()), q_backup(q_out.rows());
 
   q_out = q_init;
   ROS_DEBUG_STREAM_NAMED("kdl", "Input: " << q_init);
 
   unsigned int i;
+  bool success = false;
   for (i = 0; i < max_iter; ++i)
   {
     fk_solver_->JntToCart(q_out, f);
     delta_twist = diff(f, p_in);
     ROS_DEBUG_STREAM_NAMED("kdl", "[" << std::setw(3) << i << "] delta_twist: " << delta_twist);
 
-    if (position_ik_)
+    // check norms of position and orientation errors
+    const double position_error = delta_twist.vel.Norm();
+    const double orientation_error = position_ik_ ? 0 : delta_twist.rot.Norm();
+    const double delta_twist_norm = std::max(position_error, orientation_error);
+    if (delta_twist_norm <= epsilon_)
     {
-      if (fabs(delta_twist(0)) < epsilon_ && fabs(delta_twist(1)) < epsilon_ && fabs(delta_twist(2)) < epsilon_)
-        break;
+      success = true;
+      break;
+    }
+
+    if (delta_twist_norm >= last_delta_twist_norm)
+    {
+      // if the error increased, we are close to a singularity -> reduce step size
+      double old_step_size = step_size;
+      step_size *= std::min(0.5, last_delta_twist_norm / delta_twist_norm);  // reduce scale;
+      KDL::Multiply(delta_q, step_size / old_step_size, delta_q);
+      ROS_WARN_NAMED("kdl", "      error increased: %f -> %f, scale: %f", last_delta_twist_norm, delta_twist_norm,
+                     step_size);
+      q_out = q_backup;  // restore previous unclipped joint values
     }
     else
     {
-      if (KDL::Equal(delta_twist, KDL::Twist::Zero(), epsilon_))
-        break;
-    }
+      q_backup = q_out;  // remember joint values of last successful step
+      step_size = 1.0;   // reset step size
+      last_delta_twist_norm = delta_twist_norm;
 
-    ik_solver.CartToJnt(q_out, delta_twist, delta_q);
+      ik_solver.CartToJnt(q_out, delta_twist, delta_q);
+    }
+    const double delta_q_norm = delta_q.data.lpNorm<1>();
+    ROS_INFO_NAMED("kdl", "[%3d] pos err: %f  rot err: %f  delta_q: %f", i, position_error, orientation_error,
+                   delta_q_norm);
+
+    if (delta_q_norm < epsilon_)  // stuck in singularity
+      break;
+
     KDL::Add(q_out, delta_q, q_out);
 
     ROS_DEBUG_STREAM_NAMED("kdl", "      delta_q: " << delta_q);
@@ -413,12 +439,10 @@ int KDLKinematicsPlugin::CartToJnt(KDL::ChainIkSolverVel& ik_solver, const KDL::
     }
   }
 
-  ROS_DEBUG_STREAM_NAMED("kdl", "[" << std::setw(3) << i << "] Solution : " << q_out);
+  int result = (i == max_iter) ? -3 : (success ? 0 : -2);
+  ROS_INFO_STREAM_NAMED("kdl", "Result " << result << " after " << i << " iterations: " << q_out);
 
-  if (i != max_iter)
-    return 0;
-  else
-    return -3;
+  return result;
 }
 
 bool KDLKinematicsPlugin::getPositionFK(const std::vector<std::string>& link_names,
