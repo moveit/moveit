3574d4639f1616b4d8a7f58ee90381957cfd6a2c
AndyZe <andyz@utexas.edu>
2019-02-21 10:54:04 -0600

    Use the 'velocities' field of JointJog msg.

--- moveit_experimental/jog_arm/src/jog_arm/jog_ros_interface.cpp
+++ moveit_experimental/jog_arm/src/jog_arm/jog_ros_interface.cpp
@@ -207,7 +207,7 @@ void JogROSInterface::deltaJointCmdCB(const control_msgs::JointJogConstPtr& msg)
 
   // Check if joint inputs is all zeros. Flag it if so to skip calculations/publication
   bool all_zeros = true;
-  for (double delta : shared_variables_.joint_command_deltas.displacements)
+  for (double delta : shared_variables_.joint_command_deltas.velocities)
   {
     all_zeros &= (delta == 0.0);
   };
