22602b4a80c5cd66d07681b79e42ff3f36be084c
Jens Petit <petit.jens@gmail.com>
2019-04-02 12:54:40 +0200

    Remove more debugging commands

--- moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp
+++ moveit_ros/planning/planning_scene_monitor/src/planning_scene_monitor.cpp
@@ -469,6 +469,7 @@ bool PlanningSceneMonitor::requestPlanningSceneState(const std::string& service_
   // use global namespace for service
   ros::ServiceClient client = ros::NodeHandle().serviceClient<moveit_msgs::GetPlanningScene>(service_name);
   moveit_msgs::GetPlanningScene srv;
+  srv.request.components.components = 0;
 
   // Make sure client is connected to server
   if (!client.exists())
@@ -480,7 +481,6 @@ bool PlanningSceneMonitor::requestPlanningSceneState(const std::string& service_
   if (client.call(srv))
   {
     newPlanningSceneMessage(srv.response.scene);
-    ROS_INFO_STREAM(srv.response.scene);
   }
   else
   {
