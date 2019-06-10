3e86cabc524b3c083c9213bde8e85481e8c7f888
Jens Petit <petit.jens@gmail.com>
2019-06-10 22:11:10 +0200

    Empty collision detector template for tesseract and bullet

---
+++ moveit_core/collision_detection_bullet/include/moveit/collision_detection_bullet/collision_world_bt.h
@@ -0,0 +1,86 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Open Source Robotics Foundation, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Jens Petit */
+
+#ifndef MOVEIT_COLLISION_DETECTION_BT_COLLISION_WORLD_BT_
+#define MOVEIT_COLLISION_DETECTION_BT_COLLISION_WORLD_BT_
+
+#include <moveit/collision_detection_bullet/collision_robot_bt.h>
+
+namespace collision_detection
+{
+class CollisionWorldBt : public CollisionWorld
+{
+public:
+  CollisionWorldBt();
+  explicit CollisionWorldBt(const WorldPtr& world);
+  CollisionWorldBt(const CollisionWorldBt& other, const WorldPtr& world);
+  ~CollisionWorldBt() override;
+
+  void checkRobotCollision(const CollisionRequest& req, CollisionResult& res, const CollisionRobot& robot,
+                           const robot_state::RobotState& state) const override;
+  void checkRobotCollision(const CollisionRequest& req, CollisionResult& res, const CollisionRobot& robot,
+                           const robot_state::RobotState& state, const AllowedCollisionMatrix& acm) const override;
+  void checkRobotCollision(const CollisionRequest& req, CollisionResult& res, const CollisionRobot& robot,
+                           const robot_state::RobotState& state1, const robot_state::RobotState& state2) const override;
+  void checkRobotCollision(const CollisionRequest& req, CollisionResult& res, const CollisionRobot& robot,
+                           const robot_state::RobotState& state1, const robot_state::RobotState& state2,
+                           const AllowedCollisionMatrix& acm) const override;
+  void checkWorldCollision(const CollisionRequest& req, CollisionResult& res,
+                           const CollisionWorld& other_world) const override;
+  void checkWorldCollision(const CollisionRequest& req, CollisionResult& res, const CollisionWorld& other_world,
+                           const AllowedCollisionMatrix& acm) const override;
+
+  void distanceRobot(const DistanceRequest& req, DistanceResult& res, const CollisionRobot& robot,
+                     const robot_state::RobotState& state) const override;
+
+  void distanceWorld(const DistanceRequest& req, DistanceResult& res, const CollisionWorld& world) const override;
+
+  void setWorld(const WorldPtr& world) override;
+
+protected:
+  void checkWorldCollisionHelper(const CollisionRequest& req, CollisionResult& res, const CollisionWorld& other_world,
+                                 const AllowedCollisionMatrix* acm) const;
+  void checkRobotCollisionHelper(const CollisionRequest& req, CollisionResult& res, const CollisionRobot& robot,
+                                 const robot_state::RobotState& state, const AllowedCollisionMatrix* acm) const;
+
+private:
+  void initialize();
+  void notifyObjectChange(const ObjectConstPtr& obj, World::Action action);
+  World::ObserverHandle observer_handle_;
+};
+}
+
+#endif
---
+++ moveit_core/collision_detection_bullet/src/collision_robot_bt.cpp
@@ -0,0 +1,142 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Open Source Robotics Foundation, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Jens Petit */
+
+#include <moveit/collision_detection_bullet/collision_robot_bt.h>
+
+namespace collision_detection
+{
+CollisionRobotBt::CollisionRobotBt(const robot_model::RobotModelConstPtr& model, double padding, double scale)
+  : CollisionRobot(model, padding, scale)
+{
+}
+
+CollisionRobotBt::CollisionRobotBt(const CollisionRobotBt& other) : CollisionRobot(other)
+{
+}
+
+void CollisionRobotBt::checkSelfCollision(const CollisionRequest& req, CollisionResult& res,
+                                          const robot_state::RobotState& state) const
+{
+  checkSelfCollisionHelper(req, res, state, nullptr);
+}
+
+void CollisionRobotBt::checkSelfCollision(const CollisionRequest& req, CollisionResult& res,
+                                          const robot_state::RobotState& state, const AllowedCollisionMatrix& acm) const
+{
+  checkSelfCollisionHelper(req, res, state, &acm);
+}
+
+void CollisionRobotBt::checkSelfCollision(const CollisionRequest& req, CollisionResult& res,
+                                          const robot_state::RobotState& state1,
+                                          const robot_state::RobotState& state2) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Bullet continuous collision checking not yet implemented");
+}
+
+void CollisionRobotBt::checkSelfCollision(const CollisionRequest& req, CollisionResult& res,
+                                          const robot_state::RobotState& state1, const robot_state::RobotState& state2,
+                                          const AllowedCollisionMatrix& acm) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Bullet continuous collision checking not yet implemented");
+}
+
+void CollisionRobotBt::checkSelfCollisionHelper(const CollisionRequest& req, CollisionResult& res,
+                                                const robot_state::RobotState& state,
+                                                const AllowedCollisionMatrix* acm) const
+{
+}
+
+void CollisionRobotBt::checkOtherCollision(const CollisionRequest& req, CollisionResult& res,
+                                           const robot_state::RobotState& state, const CollisionRobot& other_robot,
+                                           const robot_state::RobotState& other_state) const
+{
+  checkOtherCollisionHelper(req, res, state, other_robot, other_state, nullptr);
+}
+
+void CollisionRobotBt::checkOtherCollision(const CollisionRequest& req, CollisionResult& res,
+                                           const robot_state::RobotState& state, const CollisionRobot& other_robot,
+                                           const robot_state::RobotState& other_state,
+                                           const AllowedCollisionMatrix& acm) const
+{
+  checkOtherCollisionHelper(req, res, state, other_robot, other_state, &acm);
+}
+
+void CollisionRobotBt::checkOtherCollision(const CollisionRequest& req, CollisionResult& res,
+                                           const robot_state::RobotState& state1, const robot_state::RobotState& state2,
+                                           const CollisionRobot& other_robot,
+                                           const robot_state::RobotState& other_state1,
+                                           const robot_state::RobotState& other_state2) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Bullet continuous collision checking not yet implemented");
+}
+
+void CollisionRobotBt::checkOtherCollision(const CollisionRequest& req, CollisionResult& res,
+                                           const robot_state::RobotState& state1, const robot_state::RobotState& state2,
+                                           const CollisionRobot& other_robot,
+                                           const robot_state::RobotState& other_state1,
+                                           const robot_state::RobotState& other_state2,
+                                           const AllowedCollisionMatrix& acm) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Bullet continuous collision checking not yet implemented");
+}
+
+void CollisionRobotBt::checkOtherCollisionHelper(const CollisionRequest& req, CollisionResult& res,
+                                                 const robot_state::RobotState& state,
+                                                 const CollisionRobot& other_robot,
+                                                 const robot_state::RobotState& other_state,
+                                                 const AllowedCollisionMatrix* acm) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Other collision not implemented yet.");
+}
+
+void CollisionRobotBt::updatedPaddingOrScaling(const std::vector<std::string>& links)
+{
+}
+
+void CollisionRobotBt::distanceSelf(const DistanceRequest& req, DistanceResult& res,
+                                    const robot_state::RobotState& state) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Collision distance to self not implemented yet.");
+}
+
+void CollisionRobotBt::distanceOther(const DistanceRequest& req, DistanceResult& res,
+                                     const robot_state::RobotState& state, const CollisionRobot& other_robot,
+                                     const robot_state::RobotState& other_state) const
+{
+  ROS_ERROR_NAMED("collision_detection.bullet", "Collision distance to other not implemented yet.");
+}
+
+}  // end of namespace collision_detection
