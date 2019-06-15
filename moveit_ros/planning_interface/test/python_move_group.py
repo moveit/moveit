72f1f2095d57fb3d60a1d39e6020c6f2c5bba46d
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2019-06-15 22:46:00 +0900

    Test to compare the jacobian by known value

--- moveit_ros/planning_interface/test/python_move_group.py
+++ moveit_ros/planning_interface/test/python_move_group.py
@@ -58,9 +58,20 @@ class PythonMoveGroupTest(unittest.TestCase):
 
     def test_get_jacobian_matrix(self):
         current = self.group.get_current_joint_values()
-        matrix = np.array(self.group.get_jacobian_matrix(current))
+        matrix = self.group.get_jacobian_matrix(current)
+        # Size check of the jacoban matrix
         self.assertEqual(matrix.shape[0], 6)
         self.assertEqual(matrix.shape[1], 6)
+        # Value check by known value at the initial pose 
+        answer = np.array([[ 0.  ,  0.8 , -0.2 ,  0.  ,  0.  ,  0.  ],
+                           [ 0.89,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ],
+                           [ 0.  , -0.74,  0.74,  0.  ,  0.1 ,  0.  ],
+                           [ 0.  ,  0.  ,  0.  , -1.  ,  0.  , -1.  ],
+                           [ 0.  ,  1.  , -1.  ,  0.  , -1.  ,  0.  ],
+                           [ 1.  ,  0.  ,  0.  ,  0.  ,  0.  ,  0.  ]])
+        for row in range(matrix.shape[0]):
+            for col in range(matrix.shape[1]):
+                self.assertAlmostEqual(matrix[row, col], answer[row, col])
 
 if __name__ == '__main__':
     PKGNAME = 'moveit_ros_planning_interface'
