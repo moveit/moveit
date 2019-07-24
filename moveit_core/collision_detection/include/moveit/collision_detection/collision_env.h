9065ea7ab79f4390d5be98800f1b426668b87215
Jens Petit <petit.jens@gmail.com>
2019-07-24 16:26:02 +0200

    Unified collision environment

---
+++ moveit_core/collision_detection/include/moveit/collision_detection/collision_env.h
@@ -0,0 +1,332 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Jens Petit
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Jens Petit */
+
+#ifndef MOVEIT_COLLISION_DETECTION_COLLISION_ENV_
+#define MOVEIT_COLLISION_DETECTION_COLLISION_ENV_
+
+#include <moveit/collision_detection/collision_matrix.h>
+#include <moveit/macros/class_forward.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit_msgs/LinkPadding.h>
+#include <moveit_msgs/LinkScale.h>
+#include <moveit/collision_detection/world.h>
+
+namespace collision_detection
+{
+MOVEIT_CLASS_FORWARD(CollisionEnv);
+
+/** \brief  */
+class CollisionEnv
+{
+public:
+  CollisionEnv() = delete;
+
+  /** @brief Constructor
+   *  @param model A robot model to construct the collision robot from
+   *  @param padding The padding to use for all objects/links on the robot
+   *  @scale scale A common scaling to use for all objects/links on the robot
+   */
+  CollisionEnv(const robot_model::RobotModelConstPtr& model, double padding = 0.0, double scale = 1.0);
+
+  /** \brief Copy constructor */
+  CollisionEnv(const CollisionEnv& other, const WorldPtr& world);
+
+  virtual ~CollisionEnv()
+  {
+  }
+
+  /** @brief Check for robot self collision. Any collision between any pair of links is checked for, NO collisions are
+   *   ignored.
+   *
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @param state The kinematic state for which checks are being made */
+  virtual void checkSelfCollision(const CollisionRequest& req, CollisionResult& res,
+                                  const robot_state::RobotState& state) = 0;
+
+  /** \brief Check for self collision. Allowed collisions specified by the allowed collision matrix are
+   *   taken into account.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @param state The kinematic state for which checks are being made
+   *  @param acm The allowed collision matrix. */
+  virtual void checkSelfCollision(const CollisionRequest& req, CollisionResult& res,
+                                  const robot_state::RobotState& state, const AllowedCollisionMatrix& acm) = 0;
+
+  /** \brief Check whether the robot model is in collision with itself or the world at a particular state.
+   *  Any collision between any pair of links is checked for, NO collisions are ignored.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @param state The kinematic state for which checks are being made         */
+  virtual void checkCollision(const CollisionRequest& req, CollisionResult& res, const robot_state::RobotState& state);
+
+  /** \brief Check whether the robot model is in collision with itself or the world at a particular state.
+   *  Allowed collisions specified by the allowed collision matrix are taken into account.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @param state The kinematic state for which checks are being made
+   *  @param acm The allowed collision matrix. */
+  virtual void checkCollision(const CollisionRequest& req, CollisionResult& res, const robot_state::RobotState& state, const AllowedCollisionMatrix& acm);
+
+  /** \brief Check whether the robot model is in collision with the world. Any collisions between a robot link
+   *  and the world are considered. Self collisions are not checked.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @robot robot The collision model for the robot
+   *  @param state The kinematic state for which checks are being made
+   */
+  virtual void checkRobotCollision(const CollisionRequest& req, CollisionResult& res,
+                                   const robot_state::RobotState& state) = 0;
+
+  /** \brief Check whether the robot model is in collision with the world.
+   *  Allowed collisions are ignored. Self collisions are not checked.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @robot robot The collision model for the robot
+   *  @param state The kinematic state for which checks are being made
+   *  @param acm The allowed collision matrix.*/
+  virtual void checkRobotCollision(const CollisionRequest& req, CollisionResult& res,
+                                   const robot_state::RobotState& state, const AllowedCollisionMatrix& acm) = 0;
+
+  /** \brief Check whether the robot model is in collision with the world in a continuous manner (between two robot
+   * states).
+   *  Allowed collisions are ignored. Self collisions are not checked.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @robot robot The collision model for the robot
+   *  @param state1 The kinematic state at the start of the segment for which checks are being made
+   *  @param state2 The kinematic state at the end of the segment for which checks are being made
+   *  @param acm The allowed collision matrix.*/
+  virtual void checkRobotCollision(const CollisionRequest& req, CollisionResult& res,
+                                   const robot_state::RobotState& state1, const robot_state::RobotState& state2,
+                                   const AllowedCollisionMatrix& acm) = 0;
+
+  /** \brief Check whether the robot model is in collision with the world in a continuous manner (between two robot
+   * states).
+   *  Allowed collisions are ignored. Self collisions are not checked.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @robot robot The collision model for the robot
+   *  @param state1 The kinematic state at the start of the segment for which checks are being made
+   *  @param state2 The kinematic state at the end of the segment for which checks are being made
+   *  @param acm The allowed collision matrix.*/
+  virtual void checkRobotCollision(const CollisionRequest& req, CollisionResult& res,
+                                   const robot_state::RobotState& state1, const robot_state::RobotState& state2) = 0;
+
+  /** \brief Check whether a given set of objects is in collision with objects from another world.
+   *  Any contacts are considered.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   */
+  virtual void checkWorldCollision(const CollisionRequest& req, CollisionResult& res) = 0;
+
+  /** \brief Check whether a given set of objects is in collision with objects from another world.
+   *  Allowed collisions are ignored. Any contacts are considered.
+   *  @param req A CollisionRequest object that encapsulates the collision request
+   *  @param res A CollisionResult object that encapsulates the collision result
+   *  @param acm The allowed collision matrix.*/
+  virtual void checkWorldCollision(const CollisionRequest& req, CollisionResult& res,
+                                   const AllowedCollisionMatrix& acm) = 0;
+
+  /** \brief The distance to self-collision given the robot is at state \e state.
+      @param req A DistanceRequest object that encapsulates the distance request
+      @param res A DistanceResult object that encapsulates the distance result
+      @param state The state of this robot to consider */
+  virtual void distanceSelf(const DistanceRequest& req, DistanceResult& res,
+                            const robot_state::RobotState& state) = 0;
+
+  /** \brief The distance to self-collision given the robot is at state \e state. */
+  inline double distanceSelf(const robot_state::RobotState& state)
+  {
+    DistanceRequest req;
+    DistanceResult res;
+
+    req.enableGroup(getRobotModel());
+    distanceSelf(req, res, state);
+    return res.minimum_distance.distance;
+  }
+
+  /** \brief The distance to self-collision given the robot is at state \e state, ignoring
+      the distances between links that are allowed to always collide (as specified by \e acm) */
+  inline double distanceSelf(const robot_state::RobotState& state, const AllowedCollisionMatrix& acm)
+  {
+    DistanceRequest req;
+    DistanceResult res;
+
+    req.enableGroup(getRobotModel());
+    req.acm = &acm;
+    distanceSelf(req, res, state);
+    return res.minimum_distance.distance;
+  }
+
+  /** \brief Compute the distance between a robot and the world
+   *  @param req A DistanceRequest object that encapsulates the distance request
+   *  @param res A DistanceResult object that encapsulates the distance result
+   *  @param robot The robot to check distance for
+   *  @param state The state for the robot to check distances from */
+  virtual void distanceRobot(const DistanceRequest& req, DistanceResult& res, const robot_state::RobotState& state) = 0;
+
+  /** \brief Compute the shortest distance between a robot and the world
+   *  @param robot The robot to check distance for
+   *  @param state The state for the robot to check distances from
+   *  @param verbose Output debug information about distance checks */
+  inline double distanceRobot(const robot_state::RobotState& state, bool verbose = false)
+  {
+    DistanceRequest req;
+    DistanceResult res;
+
+    req.verbose = verbose;
+    req.enableGroup(getRobotModel());
+
+    distanceRobot(req, res, state);
+    return res.minimum_distance.distance;
+  }
+
+  /** \brief Compute the shortest distance between a robot and the world
+   *  @param robot The robot to check distance for
+   *  @param state The state for the robot to check distances from
+   *  @param acm Using an allowed collision matrix has the effect of ignoring distances from links that are always
+   * allowed to be in collision.
+   *  @param verbose Output debug information about distance checks */
+  inline double distanceRobot(const robot_state::RobotState& state, const AllowedCollisionMatrix& acm, bool verbose = false)
+  {
+    DistanceRequest req;
+    DistanceResult res;
+
+    req.acm = &acm;
+    req.verbose = verbose;
+    req.enableGroup(getRobotModel());
+
+    distanceRobot(req, res, state);
+    return res.minimum_distance.distance;
+  }
+
+  /** set the world to use.
+   * This can be expensive unless the new and old world are empty.
+   * Passing NULL will result in a new empty world being created. */
+  virtual void setWorld(const WorldPtr& world);
+
+  /** access the world geometry */
+  const WorldPtr& getWorld()
+  {
+    return world_;
+  }
+
+  /** access the world geometry */
+  const WorldConstPtr& getWorld() const
+  {
+    return world_const_;
+  }
+
+  typedef World::ObjectPtr ObjectPtr;
+  typedef World::ObjectConstPtr ObjectConstPtr;
+
+  /** @brief The kinematic model corresponding to this collision model*/
+  const robot_model::RobotModelConstPtr& getRobotModel() const
+  {
+    return robot_model_;
+  }
+
+  /** @brief Set the link padding for a particular link.
+   *  @param link_name The link name to set padding for
+   *  @param padding The padding to set (in meters)
+   */
+  void setLinkPadding(const std::string& link_name, double padding);
+
+  /** @brief Get the link padding for a particular link*/
+  double getLinkPadding(const std::string& link_name) const;
+
+  /** @brief Set the link paddings using a map (from link names to padding value) */
+  void setLinkPadding(const std::map<std::string, double>& padding);
+
+  /** @brief Get the link paddings as a map (from link names to padding value) */
+  const std::map<std::string, double>& getLinkPadding() const;
+
+  /** @brief Set the scaling for a particular link*/
+  void setLinkScale(const std::string& link_name, double scale);
+
+  /** @brief Set the scaling for a particular link*/
+  double getLinkScale(const std::string& link_name) const;
+
+  /** @brief Set the link scaling using a map (from link names to scale value) */
+  void setLinkScale(const std::map<std::string, double>& scale);
+
+  /** @brief Get the link scaling as a map (from link names to scale value)*/
+  const std::map<std::string, double>& getLinkScale() const;
+
+  /** @brief Set the link padding (for every link)*/
+  void setPadding(double padding);
+
+  /** @brief Set the link scaling (for every link)*/
+  void setScale(double scale);
+
+  /** @brief Set the link padding from a vector of messages*/
+  void setPadding(const std::vector<moveit_msgs::LinkPadding>& padding);
+
+  /** @brief Get the link padding as a vector of messages*/
+  void getPadding(std::vector<moveit_msgs::LinkPadding>& padding) const;
+
+  /** @brief Set the link scaling from a vector of messages*/
+  void setScale(const std::vector<moveit_msgs::LinkScale>& scale);
+
+  /** @brief Get the link scaling as a vector of messages*/
+  void getScale(std::vector<moveit_msgs::LinkScale>& scale) const;
+
+protected:
+  /** @brief When the scale or padding is changed for a set of links by any of the functions in this class,
+     updatedPaddingOrScaling() function is called.
+      This function has an empty default implementation. The intention is to override this function in a derived class
+     to allow for updating
+      additional structures that may need such updating when link scale or padding changes.
+      @param links the names of the links whose padding or scaling were updated */
+  virtual void updatedPaddingOrScaling(const std::vector<std::string>& links);
+
+  /** @brief The kinematic model corresponding to this collision model*/
+  robot_model::RobotModelConstPtr robot_model_;
+
+  /** @brief The internally maintained map (from link names to padding)*/
+  std::map<std::string, double> link_padding_;
+
+  /** @brief The internally maintained map (from link names to scaling)*/
+  std::map<std::string, double> link_scale_;
+
+private:
+  WorldPtr world_;             // The world always valid, never nullptr.
+  WorldConstPtr world_const_;  // always same as world_
+};
+}
+
+#endif  // MOVEIT_COLLISION_DETECTION_COLLISION_ENV_
---
+++ moveit_core/collision_detection/src/collision_env.cpp
@@ -0,0 +1,282 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2019, Jens Petit
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Jens Petit */
+
+#include <moveit/collision_detection/collision_env.h>
+#include <limits>
+
+static inline bool validateScale(double scale)
+{
+  if (scale < std::numeric_limits<double>::epsilon())
+  {
+    ROS_ERROR_NAMED("collision_detection", "Scale must be positive");
+    return false;
+  }
+  if (scale > std::numeric_limits<double>::max())
+  {
+    ROS_ERROR_NAMED("collision_detection", "Scale must be finite");
+    return false;
+  }
+  return true;
+}
+
+static inline bool validatePadding(double padding)
+{
+  if (padding < 0.0)
+  {
+    ROS_ERROR_NAMED("collision_detection", "Padding cannot be negative");
+    return false;
+  }
+  if (padding > std::numeric_limits<double>::max())
+  {
+    ROS_ERROR_NAMED("collision_detection", "Padding must be finite");
+    return false;
+  }
+  return true;
+}
+
+namespace collision_detection
+{
+CollisionEnv::CollisionEnv(const robot_model::RobotModelConstPtr& model, double padding, double scale)
+  : robot_model_(model), world_(new World()), world_const_(world_)
+{
+  if (!validateScale(scale))
+    scale = 1.0;
+  if (!validatePadding(padding))
+    padding = 0.0;
+
+  const std::vector<const robot_model::LinkModel*>& links = robot_model_->getLinkModelsWithCollisionGeometry();
+  for (auto link : links)
+  {
+    link_padding_[link->getName()] = padding;
+    link_scale_[link->getName()] = scale;
+  }
+}
+
+CollisionEnv::CollisionEnv(const CollisionEnv& other, const WorldPtr& world) : robot_model_(other.robot_model_), world_(world), world_const_(world)
+{
+  link_padding_ = other.link_padding_;
+  link_scale_ = other.link_scale_;
+}
+void CollisionEnv::setPadding(double padding)
+{
+  if (!validatePadding(padding))
+    return;
+  std::vector<std::string> u;
+  const std::vector<const robot_model::LinkModel*>& links = robot_model_->getLinkModelsWithCollisionGeometry();
+  for (auto link : links)
+  {
+    if (getLinkPadding(link->getName()) != padding)
+      u.push_back(link->getName());
+    link_padding_[link->getName()] = padding;
+  }
+  if (!u.empty())
+    updatedPaddingOrScaling(u);
+}
+
+void CollisionEnv::setScale(double scale)
+{
+  if (!validateScale(scale))
+    return;
+  std::vector<std::string> u;
+  const std::vector<const robot_model::LinkModel*>& links = robot_model_->getLinkModelsWithCollisionGeometry();
+  for (auto link : links)
+  {
+    if (getLinkScale(link->getName()) != scale)
+      u.push_back(link->getName());
+    link_scale_[link->getName()] = scale;
+  }
+  if (!u.empty())
+    updatedPaddingOrScaling(u);
+}
+
+void CollisionEnv::setLinkPadding(const std::string& link_name, double padding)
+{
+  bool update = getLinkPadding(link_name) != padding;
+  link_padding_[link_name] = padding;
+  if (update)
+  {
+    std::vector<std::string> u(1, link_name);
+    updatedPaddingOrScaling(u);
+  }
+}
+
+double CollisionEnv::getLinkPadding(const std::string& link_name) const
+{
+  auto it = link_padding_.find(link_name);
+  if (it != link_padding_.end())
+    return it->second;
+  else
+    return 0.0;
+}
+
+void CollisionEnv::setLinkPadding(const std::map<std::string, double>& padding)
+{
+  std::vector<std::string> u;
+  for (const auto& link_pad_pair : padding)
+  {
+    bool update = getLinkPadding(link_pad_pair.first) != link_pad_pair.second;
+    link_padding_[link_pad_pair.first] = link_pad_pair.second;
+    if (update)
+      u.push_back(link_pad_pair.first);
+  }
+  if (!u.empty())
+    updatedPaddingOrScaling(u);
+}
+
+const std::map<std::string, double>& CollisionEnv::getLinkPadding() const
+{
+  return link_padding_;
+}
+
+void CollisionEnv::setLinkScale(const std::string& link_name, double scale)
+{
+  bool update = getLinkScale(link_name) != scale;
+  link_scale_[link_name] = scale;
+  if (update)
+  {
+    std::vector<std::string> u(1, link_name);
+    updatedPaddingOrScaling(u);
+  }
+}
+
+double CollisionEnv::getLinkScale(const std::string& link_name) const
+{
+  auto it = link_scale_.find(link_name);
+  if (it != link_scale_.end())
+    return it->second;
+  else
+    return 1.0;
+}
+
+void CollisionEnv::setLinkScale(const std::map<std::string, double>& scale)
+{
+  std::vector<std::string> u;
+  for (const auto& link_scale_pair : scale)
+  {
+    bool update = getLinkScale(link_scale_pair.first) != link_scale_pair.second;
+    link_scale_[link_scale_pair.first] = link_scale_pair.second;
+    if (update)
+      u.push_back(link_scale_pair.first);
+  }
+  if (!u.empty())
+    updatedPaddingOrScaling(u);
+}
+
+const std::map<std::string, double>& CollisionEnv::getLinkScale() const
+{
+  return link_scale_;
+}
+
+void CollisionEnv::setPadding(const std::vector<moveit_msgs::LinkPadding>& padding)
+{
+  std::vector<std::string> u;
+  for (const auto& p : padding)
+  {
+    bool update = getLinkPadding(p.link_name) != p.padding;
+    link_padding_[p.link_name] = p.padding;
+    if (update)
+      u.push_back(p.link_name);
+  }
+  if (!u.empty())
+    updatedPaddingOrScaling(u);
+}
+
+void CollisionEnv::setScale(const std::vector<moveit_msgs::LinkScale>& scale)
+{
+  std::vector<std::string> u;
+  for (const auto& s : scale)
+  {
+    bool update = getLinkScale(s.link_name) != s.scale;
+    link_scale_[s.link_name] = s.scale;
+    if (update)
+      u.push_back(s.link_name);
+  }
+  if (!u.empty())
+    updatedPaddingOrScaling(u);
+}
+
+void CollisionEnv::getPadding(std::vector<moveit_msgs::LinkPadding>& padding) const
+{
+  padding.clear();
+  for (const auto& lp : link_padding_)
+  {
+    moveit_msgs::LinkPadding lp_msg;
+    lp_msg.link_name = lp.first;
+    lp_msg.padding = lp.second;
+    padding.push_back(lp_msg);
+  }
+}
+
+void CollisionEnv::getScale(std::vector<moveit_msgs::LinkScale>& scale) const
+{
+  scale.clear();
+  for (const auto& ls : link_scale_)
+  {
+    moveit_msgs::LinkScale ls_msg;
+    ls_msg.link_name = ls.first;
+    ls_msg.scale = ls.second;
+    scale.push_back(ls_msg);
+  }
+}
+
+void CollisionEnv::updatedPaddingOrScaling(const std::vector<std::string>& links)
+{
+}
+
+void CollisionEnv::setWorld(const WorldPtr& world)
+{
+  world_ = world;
+  if (!world_)
+    world_.reset(new World());
+
+  world_const_ = world;
+}
+
+void CollisionEnv::checkCollision(const CollisionRequest& req, CollisionResult& res, const robot_state::RobotState& state)
+{
+  checkSelfCollision(req, res, state);
+  if (!res.collision || (req.contacts && res.contacts.size() < req.max_contacts))
+    checkRobotCollision(req, res, state);
+}
+
+void CollisionEnv::checkCollision(const CollisionRequest& req, CollisionResult& res, const robot_state::RobotState& state, const AllowedCollisionMatrix& acm)
+{
+  checkSelfCollision(req, res, state, acm);
+  if (!res.collision || (req.contacts && res.contacts.size() < req.max_contacts))
+    checkRobotCollision(req, res, state, acm);
+}
+
+}  // end of namespace collision_detection
