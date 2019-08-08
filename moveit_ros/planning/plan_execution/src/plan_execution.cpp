9ac87756fa174eaf9996b95c6aef370d4aa121a1
Robert Haschke <rhaschke@users.noreply.github.com>
2019-08-08 21:53:07 +0200

    TrajectoryMonitor: disable recording with zero sampling frequency disables recording (#1542)

--- moveit_ros/planning/plan_execution/src/plan_execution.cpp
+++ moveit_ros/planning/plan_execution/src/plan_execution.cpp
@@ -391,8 +391,13 @@ moveit_msgs::MoveItErrorCodes plan_execution::PlanExecution::executeAndMonitor(E
   }
 
   if (!trajectory_monitor_ && planning_scene_monitor_->getStateMonitor())
-    trajectory_monitor_.reset(
-        new planning_scene_monitor::TrajectoryMonitor(planning_scene_monitor_->getStateMonitor()));
+  {
+    // Pass current value of reconfigurable parameter plan_execution/record_trajectory_state_frequency
+    double sampling_frequency = 0.0;
+    node_handle_.getParam("plan_execution/record_trajectory_state_frequency", sampling_frequency);
+    trajectory_monitor_ = std::make_shared<planning_scene_monitor::TrajectoryMonitor>(
+        planning_scene_monitor_->getStateMonitor(), sampling_frequency);
+  }
 
   // start recording trajectory states
   if (trajectory_monitor_)
