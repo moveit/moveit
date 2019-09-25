587a8c57a314b641e06b7aaeafa31973e2e8afd5
Henning Kayser <henningkayser@picknik.ai>
2019-09-25 15:57:36 +0200

    Always copy dynamics if enabled in CurrentStateMonitor

--- moveit_ros/planning/planning_scene_monitor/src/current_state_monitor.cpp
+++ moveit_ros/planning/planning_scene_monitor/src/current_state_monitor.cpp
@@ -364,22 +364,6 @@ void planning_scene_monitor::CurrentStateMonitor::jointStateCallback(const senso
         update = true;
         robot_state_.setJointPositions(jm, &(joint_state->position[i]));
 
-        // optionally copy velocities and effort
-        if (copy_dynamics_)
-        {
-          // check if velocities exist
-          if (joint_state->name.size() == joint_state->velocity.size())
-          {
-            robot_state_.setJointVelocities(jm, &(joint_state->velocity[i]));
-
-            // check if effort exist. assume they are not useful if no velocities were passed in
-            if (joint_state->name.size() == joint_state->effort.size())
-            {
-              robot_state_.setJointEfforts(jm, &(joint_state->effort[i]));
-            }
-          }
-        }
-
         // continuous joints wrap, so we don't modify them (even if they are outside bounds!)
         if (jm->getType() == moveit::core::JointModel::REVOLUTE)
           if (static_cast<const moveit::core::RevoluteJointModel*>(jm)->isContinuous())
@@ -394,6 +378,24 @@ void planning_scene_monitor::CurrentStateMonitor::jointStateCallback(const senso
           robot_state_.setJointPositions(jm, &b.min_position_);
         else if (joint_state->position[i] > b.max_position_ && joint_state->position[i] <= b.max_position_ + error_)
           robot_state_.setJointPositions(jm, &b.max_position_);
+      }
+
+      // optionally copy velocities and effort
+      if (copy_dynamics_)
+      {
+        // check if velocities exist
+        if (joint_state->name.size() == joint_state->velocity.size() &&
+            robot_state_.getJointVelocities(jm)[0] != joint_state->velocity[i])
+        {
+          update = true;
+          robot_state_.setJointVelocities(jm, &(joint_state->velocity[i]));
+
+          // check if effort exist. assume they are not useful if no velocities were passed in
+          if (joint_state->name.size() == joint_state->effort.size())
+          {
+            robot_state_.setJointEfforts(jm, &(joint_state->effort[i]));
+          }
+        }
       }
     }
   }
