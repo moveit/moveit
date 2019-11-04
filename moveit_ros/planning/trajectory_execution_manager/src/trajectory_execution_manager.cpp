c6d0077e1bbe77ad633d388be185a92bd2b18b18
Lovro Ivanov <lovro.ivanov@gideonbros.ai>
2019-11-04 15:01:46 +0100

    Add execution thread mutex.

--- moveit_ros/planning/trajectory_execution_manager/src/trajectory_execution_manager.cpp
+++ moveit_ros/planning/trajectory_execution_manager/src/trajectory_execution_manager.cpp
@@ -1170,8 +1170,13 @@ void TrajectoryExecutionManager::stopExecution(bool auto_clear)
       ROS_INFO_NAMED(name_, "Stopped trajectory execution.");
 
       // wait for the execution thread to finish
-      execution_thread_->join();
-      execution_thread_.reset();
+      execution_thread_mutex_.lock();
+      if (execution_thread_ != nullptr)
+      {
+        execution_thread_->join();
+        execution_thread_.reset();
+      }
+      execution_thread_mutex_.unlock();
 
       if (auto_clear)
         clear();
@@ -1182,8 +1187,13 @@ void TrajectoryExecutionManager::stopExecution(bool auto_clear)
   else if (execution_thread_)  // just in case we have some thread waiting to be joined from some point in the past, we
                                // join it now
   {
-    execution_thread_->join();
-    execution_thread_.reset();
+    execution_thread_mutex_.lock();
+    if (execution_thread_ != nullptr)
+    {
+      execution_thread_->join();
+      execution_thread_.reset();
+    }
+    execution_thread_mutex_.unlock();
   }
 }
 
