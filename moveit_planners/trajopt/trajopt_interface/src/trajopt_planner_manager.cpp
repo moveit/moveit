7a77c3f09519ef4ddb97e11547f05b7aaf4d8847
omid <omid@picknik.ai>
2019-11-10 17:02:20 -0700

    fixed license

--- moveit_planners/trajopt/trajopt_interface/src/trajopt_planner_manager.cpp
+++ moveit_planners/trajopt/trajopt_interface/src/trajopt_planner_manager.cpp
@@ -1,7 +1,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2019, PickNik LLC
+ *  Copyright (c) 2019, PickNik, LLC
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
