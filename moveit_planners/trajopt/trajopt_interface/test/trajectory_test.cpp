50b9b7b21993d0ca6ca348bd243d402b2fa24a38
omid <omid@picknik.ai>
2019-11-20 13:34:14 -0700

    When using motion planning display in rviz, the reference trajectory is not set. Check this and warn the user if ref traj is empty trajectory_test not working yet

--- moveit_planners/trajopt/trajopt_interface/test/trajectory_test.cpp
+++ moveit_planners/trajopt/trajopt_interface/test/trajectory_test.cpp
@@ -21,22 +21,27 @@
 // Testing
 #include <gtest/gtest.h>
 
-#include <trajopt/common.hpp>
-
 #include <moveit/planning_interface/planning_interface.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_pipeline/planning_pipeline.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/utils/robot_model_test_utils.h>
 
 #include <trajopt/problem_description.h>
 #include <trajopt/kinematic_terms.h>
+#include <trajopt/common.hpp>
 
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_scene_monitor/planning_scene_monitor.h>
 #include <moveit_msgs/MotionPlanResponse.h>
+
+#include <tf2/utils.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf2_ros/transform_listener.h>
 
 class TrajectoryTest : public ::testing::Test
 {
@@ -90,40 +95,83 @@ TEST_F(TrajectoryTest, goalTolerance)
 {
   const std::string NODE_NAME = "trajectory_test";
 
-  // Create a RobotState and JointModelGroup to keep track of the current robot pose and planning group
-  moveit::core::RobotStatePtr current_state(new moveit::core::RobotState(robot_model_));
-  current_state->setToDefaultValues();
+  // Create a planing scene monitor
+  planning_scene_monitor::PlanningSceneMonitorPtr psm(
+      new planning_scene_monitor::PlanningSceneMonitor("robot_description"));
 
-  const moveit::core::JointModelGroup* joint_model_group = current_state->getJointModelGroup(PLANNING_GROUP);
-  EXPECT_NE(joint_model_group, nullptr);
+  psm->startSceneMonitor();
+  psm->startWorldGeometryMonitor();
+  psm->startStateMonitor();
+
+  // Create a RobotState to keep track of the current robot pose and planning group
+  robot_state::RobotStatePtr robot_state(
+      new robot_state::RobotState(planning_scene_monitor::LockedPlanningSceneRO(psm)->getCurrentState()));
+  robot_state->setToDefaultValues();
+  robot_state->update();
+
+  // Create JointModelGroup
+  const robot_state::JointModelGroup* joint_model_group = robot_state->getJointModelGroup(PLANNING_GROUP);
   const std::vector<std::string>& joint_names = joint_model_group->getActiveJointModelNames();
-  EXPECT_EQ(joint_names.size(), 7);
+  const std::vector<std::string>& link_model_names = joint_model_group->getLinkModelNames();
+  ROS_INFO_NAMED(NODE_NAME, "end effector name %s\n", link_model_names.back().c_str());
 
   planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model_));
+
+  // Set the planner
+  std::string planner_plugin_name = "trajopt_interface/TrajOptPlanner";
+  node_handle_.setParam("planning_plugin", planner_plugin_name);
+
+  // Create pipeline
+  planning_pipeline::PlanningPipelinePtr planning_pipeline(
+      new planning_pipeline::PlanningPipeline(robot_model_, node_handle_, "planning_plugin", "request_adapters"));
+
+  // Current state
+  std::vector<double> current_joint_values = { 0, 0, 0, -1.5, 0, 0.6, 0.9 };
+  robot_state->setJointGroupPositions(joint_model_group, current_joint_values);
+  geometry_msgs::Pose pose_msg_current;
+  const Eigen::Isometry3d& end_effector_transform_current =
+      robot_state->getGlobalLinkTransform(link_model_names.back());
+  pose_msg_current = tf2::toMsg(end_effector_transform_current);
 
   // Create response and request
   planning_interface::MotionPlanRequest req;
   planning_interface::MotionPlanResponse res;
 
+  std::vector<std::string> keys;
+  node_handle_.getParamNames(keys);
+  for (std::string key : keys)
+  {
+    std::cerr << key << std::endl;
+  }
+
   // Set start state
-  // ======================================================================================
+  // ========================================================================================
   std::vector<double> start_joint_values = { 0.4, 0.3, 0.5, -0.55, 0.88, 1.0, -0.075 };
+<<<<<<< HEAD
   moveit::core::RobotStatePtr start_state(new moveit::core::RobotState(robot_model_));
   start_state->setJointGroupPositions(joint_model_group, start_joint_values);
   start_state->update();
+=======
+  robot_state->setJointGroupPositions(joint_model_group, start_joint_values);
+  robot_state->update();
+>>>>>>> When using motion planning display in rviz, the reference trajectory is not set. Check this and warn the user if ref traj is empty
 
   req.start_state.joint_state.name = joint_names;
   req.start_state.joint_state.position = start_joint_values;
   req.goal_constraints.clear();
   req.group_name = PLANNING_GROUP;
 
-  // Set the goal state and joints tolerance
+  geometry_msgs::Pose pose_msg_start;
+  const Eigen::Isometry3d& end_effector_transform_start = robot_state->getGlobalLinkTransform(link_model_names.back());
+  pose_msg_start = tf2::toMsg(end_effector_transform_start);
+
+  // Set the goal state
   // ========================================================================================
-  moveit::core::RobotStatePtr goal_state(new moveit::core::RobotState(robot_model_));
   std::vector<double> goal_joint_values = { 0.8, 0.7, 1, -1.3, 1.9, 2.2, -0.1 };
-  goal_state->setJointGroupPositions(joint_model_group, goal_joint_values);
-  goal_state->update();
-  moveit_msgs::Constraints joint_goal = kinematic_constraints::constructGoalConstraints(*goal_state, joint_model_group);
+  robot_state->setJointGroupPositions(joint_model_group, goal_joint_values);
+  robot_state->update();
+  moveit_msgs::Constraints joint_goal =
+      kinematic_constraints::constructGoalConstraints(*robot_state, joint_model_group);
   req.goal_constraints.push_back(joint_goal);
   req.goal_constraints[0].name = "goal_pos";
   // Set joint tolerance
@@ -134,59 +182,39 @@ TEST_F(TrajectoryTest, goalTolerance)
     req.goal_constraints[0].joint_constraints[x].tolerance_below = 0.001;
   }
 
-  // Load planner
-  // ======================================================================================
-  // We will now construct a loader to load a planner, by name.
-  // Note that we are using the ROS pluginlib library here.
-  boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
-  planning_interface::PlannerManagerPtr planner_instance;
+  geometry_msgs::Pose pose_msg_goal;
+  const Eigen::Isometry3d& end_effector_transform_goal = robot_state->getGlobalLinkTransform(link_model_names.back());
+  pose_msg_goal = tf2::toMsg(end_effector_transform_goal);
 
-  std::string planner_plugin_name = "trajopt_interface/TrajOptPlanner";
-  node_handle_.setParam("planning_plugin", planner_plugin_name);
-
-  // Make sure the planner plugin is loaded
-  EXPECT_TRUE(node_handle_.getParam("planning_plugin", planner_plugin_name));
-  try
-  {
-    planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>(
-        "moveit_core", "planning_interface::PlannerManager"));
-  }
-  catch (pluginlib::PluginlibException& ex)
-  {
-    ROS_FATAL_STREAM_NAMED(NODE_NAME, "Exception while creating planning plugin loader " << ex.what());
-  }
-  try
-  {
-    planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
-    if (!planner_instance->initialize(robot_model_, node_handle_.getNamespace()))
-      ROS_FATAL_STREAM_NAMED(NODE_NAME, "Could not initialize planner instance");
-    ROS_INFO_STREAM_NAMED(NODE_NAME, "Using planning interface '" << planner_instance->getDescription() << "'");
-  }
-  catch (pluginlib::PluginlibException& ex)
-  {
-    const std::vector<std::string>& classes = planner_plugin_loader->getDeclaredClasses();
-    std::stringstream ss;
-    for (std::size_t i = 0; i < classes.size(); ++i)
-      ss << classes[i] << " ";
-    ROS_ERROR_STREAM_NAMED(NODE_NAME, "Exception while loading planner '" << planner_plugin_name << "': " << ex.what()
-                                                                          << std::endl
-                                                                          << "Available plugins: " << ss.str());
-  }
-
-  // Creat planning context
+  // Reference Trajectory. The type should be defined in the yaml file.
   // ========================================================================================
-  planning_interface::PlanningContextPtr context =
-      planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
+  // type: JOINT_INTERPOLATED
+  req.reference_trajectories.resize(1);
+  req.reference_trajectories[0].joint_trajectory.resize(1);
+  req.reference_trajectories[0].joint_trajectory[0].joint_names = joint_names;
+  req.reference_trajectories[0].joint_trajectory[0].points.resize(1);
+  req.reference_trajectories[0].joint_trajectory[0].points[0].positions = goal_joint_values;
 
-  context->solve(res);
-  EXPECT_EQ(res.error_code_.val, res.error_code_.SUCCESS);
+  // Solve the problem
+  // ========================================================================================
+  {
+    planning_scene_monitor::LockedPlanningSceneRO lscene(psm);
+    // Now, call the pipeline and check whether planning was successful.
+    planning_pipeline->generatePlan(lscene, req, res);
+  }
 
-  moveit_msgs::MotionPlanResponse response;
-  res.getMessage(response);
+  // Check that the planning was successful
+  if (res.error_code_.val != res.error_code_.SUCCESS)
+  {
+    ROS_ERROR_STREAM_NAMED(NODE_NAME, "Could not compute plan successfully");
+    return;
+  }
 
   // Check the difference between the last step in the solution and the goal
   // ========================================================================================
-  std::vector<double> joints_values_last_step = response.trajectory.joint_trajectory.points.back().positions;
+  moveit_msgs::RobotTrajectory solution_trajectory;
+  res.trajectory_->getRobotTrajectoryMsg(solution_trajectory);
+  std::vector<double> joints_values_last_step = solution_trajectory.joint_trajectory.points.back().positions;
 
   for (std::size_t joint_index = 0; joint_index < joints_values_last_step.size(); ++joint_index)
   {
