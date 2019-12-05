fad6b41aa66e1ea6b5c8b281a78baff54a95766c
AndyZe <andyz@utexas.edu>
2019-12-05 10:32:55 -0600

    Clang format

--- moveit_experimental/moveit_jog_arm/include/moveit_jog_arm/collision_check_thread.h
+++ moveit_experimental/moveit_jog_arm/include/moveit_jog_arm/collision_check_thread.h
@@ -3,26 +3,26 @@
  * Project   : moveit_jog_arm
  * Created   : 1/11/2019
  * Author    : Brian O'Neil, Andy Zelenak, Blake Anderson
- * 
+ *
  * BSD 3-Clause License
- * 
+ *
  * Copyright (c) 2019, Los Alamos National Security, LLC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of the copyright holder nor the names of its
  *   contributors may be used to endorse or promote products derived from
  *   this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
