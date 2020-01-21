ec0b0c4f165350d35968dbd6e13e60b7fdc246a1
AndyZe <andyz@utexas.edu>
2020-01-21 09:38:26 -0600

    Move traj size check to top of function.

--- moveit_experimental/moveit_jog_arm/src/jog_calcs.cpp
+++ moveit_experimental/moveit_jog_arm/src/jog_calcs.cpp
@@ -561,24 +561,26 @@ bool JogCalcs::checkIfJointsWithinURDFBounds(trajectory_msgs::JointTrajectory& n
   {
     if (!kinematic_state_->satisfiesVelocityBounds(joint))
     {
-      // TODO(andyz): Why would the points member or points.velocities[i] not be initialized for some joints?
-      // It seems to do with joints that are included in the MoveGroup but don't actually contribute to robot motion.
       if (new_joint_traj.points.size() > 0)
       {
-        ROS_WARN_STREAM_THROTTLE_NAMED(2, LOGNAME, ros::this_node::getName() << " " << joint->getName() << " "
+        ROS_WARN_STREAM_THROTTLE_NAMED(2, LOGNAME, "Empty trajectory passed into checkIfJointsWithinURDFBounds().");
+        return true;  // technically an empty trajectory is still within bounds
+      }
+
+      ROS_WARN_STREAM_THROTTLE_NAMED(2, LOGNAME, ros::this_node::getName() << " " << joint->getName() << " "
                                                                              << " close to a "
                                                                                 " velocity limit. Enforcing limit.");
-
-        kinematic_state_->enforceVelocityBounds(joint);
-        for (std::size_t c = 0; c < new_joint_traj.joint_names.size(); ++c)
+      kinematic_state_->enforceVelocityBounds(joint);
+      for (std::size_t c = 0; c < new_joint_traj.joint_names.size(); ++c)
+      {
+        if (new_joint_traj.joint_names[c] == joint->getName())
         {
-          if (new_joint_traj.joint_names[c] == joint->getName())
+          // TODO(andyz): This is caused by publishing in position mode -- which does not initialize the velocity members.
+          // TODO(andyz): Also need to adjust the joint positions that would be published.
+          if (new_joint_traj.points[0].velocities.size() > c + 1)
           {
-            if (new_joint_traj.points[0].velocities.size() > c + 1)
-            {
-              new_joint_traj.points[0].velocities[c] = *(kinematic_state_->getJointVelocities(joint));
-              break;
-            }
+            new_joint_traj.points[0].velocities[c] = *(kinematic_state_->getJointVelocities(joint));
+            break;
           }
         }
       }
