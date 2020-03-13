c201c142bb85e2828764b273df93a31f1e54cba5
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-03-13 11:31:21 +0100

    fix error message

--- moveit_experimental/moveit_jog_arm/test/python_tests/util.py
+++ moveit_experimental/moveit_jog_arm/test/python_tests/util.py
@@ -3,8 +3,8 @@ import rospy
 def wait_for_jogger_initialization(service_name):
     try:
       rospy.wait_for_service(service_name, timeout=15)
-    except rospy.ServiceException as exc:
-      rospy.logerr("The jogger never finished initialization, expected service is not available: " + str(exc))
+    except rospy.ROSException as exc:
+      rospy.logerr("The jogger service " + service_name + " is not available: " + str(exc))
       return False
 
     return True
