7af9bc7b491b2ff683b4623c5d716881faccfc58
Martin Pecka <peckama2@fel.cvut.cz>
2020-04-28 01:34:33 +0200

    Utilize setScaleDirty() and setPaddingDirty() to improve performance

--- moveit_ros/perception/point_containment_filter/src/shape_mask.cpp
+++ moveit_ros/perception/point_containment_filter/src/shape_mask.cpp
@@ -72,8 +72,9 @@ point_containment_filter::ShapeHandle point_containment_filter::ShapeMask::addSh
   ss.body = bodies::createBodyFromShape(shape.get());
   if (ss.body)
   {
-    ss.body->setScale(scale);
-    ss.body->setPadding(padding);
+    ss.body->setScaleDirty(scale);
+    ss.body->setPaddingDirty(padding);
+    ss.body->updateInternalData();
     ss.volume = ss.body->computeVolume();
     ss.handle = next_handle_;
     std::pair<std::set<SeeShape, SortBodies>::iterator, bool> insert_op = bodies_.insert(ss);
