b6680d518f139172b87c71ce47ea5d0f7536ce28
Simon Schmeisser <simon.schmeisser@isys-vision.de>
2020-05-04 16:45:24 +0200

    Don't rebuild the ogre scene if only the robot state changed

--- moveit_ros/visualization/rviz_plugin_render_tools/include/moveit/rviz_plugin_render_tools/planning_scene_render.h
+++ moveit_ros/visualization/rviz_plugin_render_tools/include/moveit/rviz_plugin_render_tools/planning_scene_render.h
@@ -76,6 +76,8 @@ public:
     return scene_robot_;
   }
 
+  void updateRobotPosition(const planning_scene::PlanningSceneConstPtr& scene);
+
   void renderPlanningScene(const planning_scene::PlanningSceneConstPtr& scene, const rviz::Color& default_scene_color,
                            const rviz::Color& default_attached_color, OctreeVoxelRenderMode voxel_render_mode,
                            OctreeVoxelColorMode voxel_color_mode, float default_scene_alpha);
