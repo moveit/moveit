00f1ed9330a2a1dd93d2545c451ffba1bf23e4f2
Michael Görner <me@v4hn.de>
2020-05-06 18:22:02 +0200

    move NOLINT instructions to intended positions (#2058)

--- moveit_kinematics/kdl_kinematics_plugin/include/moveit/kdl_kinematics_plugin/kdl_kinematics_plugin.h
+++ moveit_kinematics/kdl_kinematics_plugin/include/moveit/kdl_kinematics_plugin/kdl_kinematics_plugin.h
@@ -124,8 +124,8 @@ public:
 protected:
   typedef Eigen::Matrix<double, 6, 1> Twist;
 
-  // NOLINTNEXTLINE(readability-identifier-naming)
   /// Solve position IK given initial joint values
+  // NOLINTNEXTLINE(readability-identifier-naming)
   int CartToJnt(KDL::ChainIkSolverVelMimicSVD& ik_solver, const KDL::JntArray& q_init, const KDL::Frame& p_in,
                 KDL::JntArray& q_out, const unsigned int max_iter, const Eigen::VectorXd& joint_weights,
                 const Twist& cartesian_weights) const;
