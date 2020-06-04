b272539201beb505ac39f03b16ed3606e1655433
Benjamin Scholz <ben.scholz@posteo.net>
2020-06-04 20:09:13 +0200

    add method to set cartesian eef speed
    add method to set the cartesian end effector speed via the move group
    interface.
    The value is then set in the MotionPlanRequest, which is then used in
    the planning request adapter for setting the cartesian end effector
    speed.

--- moveit_ros/planning_interface/move_group_interface/src/move_group_interface.cpp
+++ moveit_ros/planning_interface/move_group_interface/src/move_group_interface.cpp
@@ -130,6 +130,7 @@ public:
     goal_orientation_tolerance_ = 1e-3;  // ~0.1 deg
     allowed_planning_time_ = 5.0;
     num_planning_attempts_ = 1;
+    max_cartesian_speed_ = 0.0;
     node_handle_.param<double>("robot_description_planning/default_velocity_scaling_factor",
                                max_velocity_scaling_factor_, 0.1);
     node_handle_.param<double>("robot_description_planning/default_acceleration_scaling_factor",
@@ -415,6 +416,18 @@ public:
     {
       variable = target_value;
     }
+  }
+
+  void setMaxCartesianEndEffectorSpeed(const double speed, const std::string& end_effector_link)
+  {
+    cartesian_end_effector_link_ = end_effector_link;
+    max_cartesian_speed_ = speed;
+  }
+
+  void clearMaxCartesianEndEffectorSpeed()
+  {
+    cartesian_end_effector_link_ = "";
+    max_cartesian_speed_ = 0.0;
   }
 
   moveit::core::RobotState& getTargetRobotState()
@@ -1062,6 +1075,8 @@ public:
     request.num_planning_attempts = num_planning_attempts_;
     request.max_velocity_scaling_factor = max_velocity_scaling_factor_;
     request.max_acceleration_scaling_factor = max_acceleration_scaling_factor_;
+    request.cartesian_speed_end_effector_link = cartesian_end_effector_link_;
+    request.max_cartesian_speed = max_cartesian_speed_;
     request.allowed_planning_time = allowed_planning_time_;
     request.pipeline_id = planning_pipeline_id_;
     request.planner_id = planner_id_;
@@ -1301,6 +1316,8 @@ private:
   unsigned int num_planning_attempts_;
   double max_velocity_scaling_factor_;
   double max_acceleration_scaling_factor_;
+  std::string cartesian_end_effector_link_;
+  double max_cartesian_speed_;
   double goal_joint_tolerance_;
   double goal_position_tolerance_;
   double goal_orientation_tolerance_;
@@ -1477,6 +1494,16 @@ void MoveGroupInterface::setMaxVelocityScalingFactor(double max_velocity_scaling
 void MoveGroupInterface::setMaxAccelerationScalingFactor(double max_acceleration_scaling_factor)
 {
   impl_->setMaxAccelerationScalingFactor(max_acceleration_scaling_factor);
+}
+
+void MoveGroupInterface::setMaxCartesianEndEffectorSpeed(const double speed, const std::string& end_effector_link)
+{
+  impl_->setMaxCartesianEndEffectorSpeed(speed, end_effector_link);
+}
+
+void MoveGroupInterface::clearMaxCartesianEndEffectorSpeed()
+{
+  impl_->clearMaxCartesianEndEffectorSpeed();
 }
 
 MoveItErrorCode MoveGroupInterface::asyncMove()
