b16660b23d67613415467caa4a0a8e7dc6b6474e
jeroendm <jeroendemaeyer@live.be>
2020-07-22 15:47:17 +0200

    add OMPL position and orientation constraints to integrate OMPL's constrained state space

---
+++ moveit_planners/ompl/ompl_interface/include/moveit/ompl_interface/detail/ompl_constraints.h
@@ -0,0 +1,264 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, Jeroen De Maeyer
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Jeroen De Maeyer */
+
+#pragma once
+
+#include <ompl/base/Constraint.h>
+
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit_msgs/Constraints.h>
+
+namespace ompl_interface
+{
+namespace ob = ompl::base;
+
+/** \brief Represents upper and lower bound on a scalar value (double).
+ *
+ * Equality constraints can be represented by setting
+ * the upper bound and lower bound almost equal.
+ * I (jeroendm) assume that it is better to not have them exactly equal
+ * for numerical reasons. Not sure.
+ * **/
+struct Bounds
+{
+  double lower, upper;
+
+  /** \brief Distance to region inside bounds
+   *
+   * Distance of a given value outside the bounds,
+   * zero inside the bounds.
+   *
+   * Creates a penalty function that looks like this:
+   *
+   *  \         /
+   *   \       /
+   *    \_____/
+   * (how does ascii art work??)
+   * */
+  double distance(double value) const;
+};
+
+/** \brief Abstract base class for differen types of constraints, implementations of ompl::base::Constraint
+ *
+ * To create a constrained state space in OMPL, we need a model of the constraints.
+ * Any generic model that can be written in the form of equality constraints F(joint_positions) = 0
+ * will work. In this code we use bounds on scalar values:
+ *    lower_bound < scalar value < upper bound
+ * and convert them to equality constraints using the distance function explained in the class Bounds above.
+ *
+ * The 'scalar value' can be any error in general.
+ * For planning in MoveIt, this can be error on the position or orientation of a link relative to a
+ * desired reference position.
+ * */
+class BaseConstraint : public ob::Constraint
+{
+public:
+  BaseConstraint(robot_model::RobotModelConstPtr robot_model, const std::string& group, const unsigned int num_dofs,
+                 const unsigned int num_cons_ = 3);
+
+  /** \brief initialize constraint based on message content.
+   *
+   * This is necessary because we cannot call the pure virtual
+   * parseConstraintsMsg method from the constructor of this class.
+   * */
+  void init(const moveit_msgs::Constraints& constraints);
+
+  /** OMPL's main constraint evaluation function.
+   *
+   *  OMPL requires you to override at least "function" which represents the constraint F(q) = 0
+   * */
+  virtual void function(const Eigen::Ref<const Eigen::VectorXd>& x, Eigen::Ref<Eigen::VectorXd> out) const;
+
+  /** Optionally you can also provide dF(q)/dq, the Jacobian of  the constriants.
+   *
+   * */
+  virtual void jacobian(const Eigen::Ref<const Eigen::VectorXd>& x, Eigen::Ref<Eigen::MatrixXd> out) const;
+
+  /** \brief Wrapper for forward kinematics calculated by MoveIt's Robot State.
+   *
+   * TODO(jeroendm) Are these actually const, as the robot state is modified? How come it works?
+   * Also, output arguments could be significantly more performant,
+   * but MoveIt's robot state does not support passing Eigen::Ref objects at the moment.
+   * */
+  Eigen::Isometry3d forwardKinematics(const Eigen::Ref<const Eigen::VectorXd>& joint_values) const;
+
+  /** \brief Calculate the geometric Jacobian using MoveIt's Robot State.
+   *
+   * Ideally I would pass the output agrument from OMPL's jacobian function directly,
+   * but I cannot pass an object of type , Eigen::Ref<Eigen::MatrixXd> to MoveIt's
+   * Jacobian method.
+   * */
+  Eigen::MatrixXd geometricJacobian(const Eigen::Ref<const Eigen::VectorXd>& joint_values) const;
+
+  /** \brief Parse bounds on position and orientation parameters from MoveIt's constraint message.
+   *
+   * This can be non-trivial given the often complex structure of these messages.
+   * For the current example with equality constraints it could be to simple
+   * to have this separate function instead of using the init function directly.
+   * */
+  virtual void parseConstraintMsg(const moveit_msgs::Constraints& constraints) = 0;
+
+  /** \brief For inequality constraints: calculate the value of the parameter that is being constraint by the bounds.
+   *
+   * In this Position constraints case, it calculates the x, y and z position
+   * of the end-effector. This error is then converted in generic equality constraints in the implementation of
+   * BaseConstraint::function.
+   * */
+  virtual Eigen::VectorXd calcError(const Eigen::Ref<const Eigen::VectorXd>& x) const
+  {
+    ROS_ERROR_STREAM("Constraint method calcError was not overridded, so it should not be used.");
+    return Eigen::VectorXd::Zero(getCoDimension());
+  }
+
+  /** \brief For inequality constraints: calculate the Jacobian for the current parameters that are being constraints.
+   *
+   * TODO(jeroendm), maybe also provide output agruments similar to the jacobian function
+   * so we can default to ob::Constraint::jacobian(x, out) when needed.
+   *
+   * This error jacobian, as the name suggests, is only the jacobian of the position / orientation / ... error.
+   * It does not take into acount the derivative of the distance functions defined in the Bounds class.
+   * This correction is added in the implementation of of BaseConstraint::jacobian.
+   * */
+  virtual Eigen::MatrixXd calcErrorJacobian(const Eigen::Ref<const Eigen::VectorXd>& x) const
+  {
+    ROS_ERROR_STREAM("Constraint method calcErrorJacobian was not overridded, so it should not be used.");
+    return Eigen::MatrixXd::Zero(getCoDimension(), n_);
+  }
+
+protected:
+  // MoveIt's robot representation for kinematic calculations
+  robot_model::RobotModelConstPtr robot_model_;
+  robot_state::RobotStatePtr robot_state_;
+  const robot_state::JointModelGroup* joint_model_group_;
+
+  /** \brief Robot link the constraints are applied to. */
+  std::string link_name_;
+
+  /** \brief Upper and lower bounds on constrained variables. */
+  std::vector<Bounds> bounds_;
+
+  /** \brief target for equality constraints, nominal value for inequality constraints. */
+  Eigen::Vector3d target_position_;
+
+  /** \brief target for equality constraints, nominal value for inequality constraints. */
+  Eigen::Quaterniond target_orientation_;
+};
+
+/** \brief Box shaped position constraints
+ *
+ * Reads bounds on x, y and z position from a position constraint
+ * at constraint_region.primitives[0].dimensions.
+ * Where the primitive has to be of type BOX.
+ *
+ * These bounds are applied around the nominal position and orientation
+ * of the box.
+ *
+ * */
+class PositionConstraint : public BaseConstraint
+{
+public:
+  PositionConstraint(robot_model::RobotModelConstPtr robot_model, const std::string& group, const unsigned int num_dofs)
+    : BaseConstraint(robot_model, group, num_dofs)
+  {
+  }
+  virtual void parseConstraintMsg(const moveit_msgs::Constraints& constraints) override;
+  virtual Eigen::VectorXd calcError(const Eigen::Ref<const Eigen::VectorXd>& x) const override;
+  virtual Eigen::MatrixXd calcErrorJacobian(const Eigen::Ref<const Eigen::VectorXd>& x) const override;
+};
+
+/** \brief orientation constraints based on angle-axis error.
+ *
+ * (aka exponential coordinates)
+ * This is analog to how orientation Error is calculated in the TrajOpt motion planner.
+ * It is NOT how orientation error is handled in the default MoveIt constraint samplers.
+ * (There, XYZ intrinsic euler angles are used.)
+ *
+ * */
+class AngleAxisConstraint : public BaseConstraint
+{
+public:
+  AngleAxisConstraint(robot_model::RobotModelConstPtr robot_model, const std::string& group,
+                      const unsigned int num_dofs)
+    : BaseConstraint(robot_model, group, num_dofs)
+  {
+  }
+
+  virtual void parseConstraintMsg(const moveit_msgs::Constraints& constraints) override;
+  virtual Eigen::VectorXd calcError(const Eigen::Ref<const Eigen::VectorXd>& x) const override;
+  virtual Eigen::MatrixXd calcErrorJacobian(const Eigen::Ref<const Eigen::VectorXd>& x) const override;
+};
+
+/** \brief Extract position constraints from the MoveIt message.
+ *
+ * Assumes there is a single primitive of type box.
+ * Only the dimensions of this box are used here.
+ * These are bounds on the deviation of the end-effector from
+ * the desired position given as the position of the box in the field
+ * constraint_regions.primitive_poses[0].position.
+ *
+ * @todo: also use the link name in future?
+ * Now we assume the constraints are for the end-effector link.
+ * */
+std::vector<Bounds> positionConstraintMsgToBoundVector(const moveit_msgs::PositionConstraint& pos_con);
+
+/** \brief Extract orientation constraints from the MoveIt message
+ *
+ * These bounds are assumed to be centered around the nominal orientation / desired orientation
+ * given in the field "orientation" in the message.
+ * These bounds are therefore bounds on the orientation error between the desired orientation
+ * and the current orientation of the end-effector.
+ *
+ * The three bounds x, y, and z, can be applied to different parameterizations of the rotation error.
+ * (Roll, pithc, and yaw or exponential coordinates or something else.)
+ *
+ * */
+std::vector<Bounds> orientationConstraintMsgToBoundVector(const moveit_msgs::OrientationConstraint& ori_con);
+
+/** \brief Factory to create constraints based on what is in the MoveIt constraint message. **/
+std::shared_ptr<BaseConstraint> createConstraint(robot_model::RobotModelConstPtr robot_model, const std::string& group,
+                                                 const moveit_msgs::Constraints& constraints);
+
+/** \brief Conversion matrix to go from angular velocity in the world frame to
+ * angle axis equivalent.
+ *
+ * Based on:
+ * https://ethz.ch/content/dam/ethz/special-interest/mavt/robotics-n-intelligent-systems/rsl-dam/documents/RobotDynamics2016/RD2016script.pdf
+ *
+ * */
+Eigen::Matrix3d angularVelocityToAngleAxis(double angle, const Eigen::Vector3d& axis);
+
+}  // namespace ompl_interface
---
+++ moveit_planners/ompl/ompl_interface/test/test_ompl_constraints.cpp
@@ -0,0 +1,401 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2020, Jeroen De Maeyer
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Jeroen De Maeyer */
+
+/** This file tests the implementation of constriants inheriting from
+ * the ompl::base::Constraint class in the file /detail/ompl_constraint.h/cpp.
+ * These are used to create an ompl::base::ConstrainedStateSpace to plan with path constraints.
+ **/
+
+#include <moveit/ompl_interface/detail/ompl_constraints.h>
+
+#include <memory>
+#include <string>
+#include <iostream>
+
+#include <gtest/gtest.h>
+#include <Eigen/Dense>
+
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit_msgs/Constraints.h>
+#include <moveit/robot_state/conversions.h>
+#include <moveit/utils/robot_model_test_utils.h>
+
+#include <ompl/util/Exception.h>
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+#include <ompl/base/spaces/constraint/ProjectedStateSpace.h>
+#include <ompl/base/ConstrainedSpaceInformation.h>
+
+/** \brief Number of times to run a test that uses randomly generated input. **/
+constexpr int NUM_RANDOM_TESTS{ 10 };
+
+/** \brief For failing tests, some extra print statements are useful. **/
+constexpr bool VERBOSE{ false };
+
+/** \brief Allowed error when comparing Jacobian matrix error.
+ *
+ * High tolerance because of high finite difference error.
+ * (And it is the L1-norm over the whole matrix difference.)
+ **/
+constexpr double JAC_ERROR_TOLERANCE{ 1e-4 };
+
+/** \brief Helper function to create a specific position constraint. **/
+moveit_msgs::PositionConstraint createPositionConstraint(std::string& base_link, std::string& ee_link_name)
+{
+  shape_msgs::SolidPrimitive box_constraint;
+  box_constraint.type = shape_msgs::SolidPrimitive::BOX;
+  box_constraint.dimensions = { 0.05, 0.4, 0.05 }; /* use -1 to indicate no constraints. */
+
+  geometry_msgs::Pose box_pose;
+  box_pose.position.x = 0.9;
+  box_pose.position.y = 0.0;
+  box_pose.position.z = 0.2;
+  box_pose.orientation.w = 1.0;
+
+  moveit_msgs::PositionConstraint position_constraint;
+  position_constraint.header.frame_id = base_link;
+  position_constraint.link_name = ee_link_name;
+  position_constraint.constraint_region.primitives.push_back(box_constraint);
+  position_constraint.constraint_region.primitive_poses.push_back(box_pose);
+
+  return position_constraint;
+}
+
+moveit_msgs::OrientationConstraint createOrientationConstraint(std::string& base_link, std::string& ee_link_name)
+{
+  moveit_msgs::OrientationConstraint orientation_constraint;
+  orientation_constraint.header.frame_id = base_link;
+  orientation_constraint.link_name = ee_link_name;
+  orientation_constraint.orientation.w = 1.0;
+  orientation_constraint.absolute_x_axis_tolerance = 0.1;
+  orientation_constraint.absolute_y_axis_tolerance = 0.1;
+  orientation_constraint.absolute_z_axis_tolerance = -1.0;
+  return orientation_constraint;
+}
+
+/** \brief Robot indepentent test class implementing all tests
+ *
+ * All tests are implemented in a generic test fixture, so it is
+ * easy to run them on different robots.
+ *
+ * based on
+ * https://stackoverflow.com/questions/38207346/specify-constructor-arguments-for-a-google-test-fixture/38218657
+ * (answer by PiotrNycz)
+ *
+ * It is implemented this way to avoid the ros specific test framework
+ * outside moveit_ros.
+ *
+ * (This is an (uglier) alternative to using the rostest framework
+ * and reading the robot settings from the parameter server.
+ * Then we have several rostest launch files that load the parameters
+ * for a specific robot and run the same compiled tests for all robots.)
+ * */
+class ConstraintTestBaseClass : public testing::Test
+{
+protected:
+  ConstraintTestBaseClass(const std::string& robot_name, const std::string& group_name)
+    : robot_name_(robot_name), group_name_(group_name)
+  {
+  }
+
+  void SetUp() override
+  {
+    // load robot
+    robot_model_ = moveit::core::loadTestingRobotModel(robot_name_);
+    robot_state_ = std::make_shared<robot_state::RobotState>(robot_model_);
+    joint_model_group_ = robot_state_->getJointModelGroup(group_name_);
+
+    // extract useful parameters for tests
+    num_dofs_ = joint_model_group_->getVariableCount();
+    ee_link_name_ = joint_model_group_->getLinkModelNames().back();
+    base_link_name_ = robot_model_->getRootLinkName();
+  };
+
+  void TearDown() override
+  {
+  }
+
+  const Eigen::Isometry3d fk(const Eigen::VectorXd q) const
+  {
+    robot_state_->setJointGroupPositions(joint_model_group_, q);
+    return robot_state_->getGlobalLinkTransform(ee_link_name_);
+  }
+
+  Eigen::VectorXd getRandomState()
+  {
+    robot_state_->setToRandomPositions(joint_model_group_);
+    Eigen::VectorXd joint_positions;
+    robot_state_->copyJointGroupPositions(joint_model_group_, joint_positions);
+    return joint_positions;
+  }
+
+  Eigen::MatrixXd numericalJacobianPosition(const Eigen::VectorXd q)
+  {
+    const double h{ 1e-6 }; /* step size for numerical derivation */
+
+    Eigen::MatrixXd jacobian = Eigen::MatrixXd::Zero(3, num_dofs_);
+
+    // helper matrix for differentiation.
+    Eigen::MatrixXd m_helper = h * Eigen::MatrixXd::Identity(num_dofs_, num_dofs_);
+
+    for (std::size_t dim{ 0 }; dim < num_dofs_; ++dim)
+    {
+      Eigen::Vector3d pos = fk(q).translation();
+      Eigen::Vector3d pos_plus_h = fk(q + m_helper.col(dim)).translation();
+      Eigen::Vector3d col = (pos_plus_h - pos) / h;
+      jacobian.col(dim) = col;
+    }
+    return jacobian;
+  }
+
+  void setPositionConstraints()
+  {
+    moveit_msgs::Constraints constraint_msgs;
+    constraint_msgs.position_constraints.push_back(createPositionConstraint(base_link_name_, ee_link_name_));
+
+    constraint_ = std::make_shared<ompl_interface::PositionConstraint>(robot_model_, group_name_, num_dofs_);
+    constraint_->init(constraint_msgs);
+  }
+
+  void setOrientationConstraints()
+  {
+    moveit_msgs::Constraints constraint_msgs;
+    constraint_msgs.orientation_constraints.push_back(createOrientationConstraint(base_link_name_, ee_link_name_));
+
+    constraint_ = std::make_shared<ompl_interface::AngleAxisConstraint>(robot_model_, group_name_, num_dofs_);
+    constraint_->init(constraint_msgs);
+  }
+
+  void testJacobian()
+  {
+    double total_error{ 999.9 };
+
+    for (int i{ 0 }; i < NUM_RANDOM_TESTS; ++i)
+    {
+      auto q = getRandomState();
+      auto jac_exact = constraint_->calcErrorJacobian(q);
+      auto jac_approx = numericalJacobianPosition(q);
+
+      if (VERBOSE)
+      {
+        std::cout << "Analytical jacobian: \n";
+        std::cout << jac_exact << std::endl;
+        std::cout << "Finite difference jacobian: \n";
+        std::cout << jac_approx << std::endl;
+      }
+
+      total_error = (jac_exact - jac_approx).lpNorm<1>();
+      EXPECT_LT(total_error, JAC_ERROR_TOLERANCE);
+    }
+  }
+
+  void testOMPLProjectedStateSpaceConstruction()
+  {
+    auto state_space = std::make_shared<ompl::base::RealVectorStateSpace>(num_dofs_);
+    ompl::base::RealVectorBounds bounds(num_dofs_);
+
+    // get joint limits from the joint model group
+    auto joint_limits = joint_model_group_->getActiveJointModelsBounds();
+    EXPECT_EQ(joint_limits.size(), num_dofs_);
+    for (std::size_t i{ 0 }; i < num_dofs_; ++i)
+    {
+      EXPECT_EQ(joint_limits[i]->size(), 1);
+      bounds.setLow(i, joint_limits[i]->at(0).min_position_);
+      bounds.setHigh(i, joint_limits[i]->at(0).max_position_);
+    }
+
+    state_space->setBounds(bounds);
+
+    auto constrained_state_space = std::make_shared<ompl::base::ProjectedStateSpace>(state_space, constraint_);
+
+    auto constrained_state_space_info =
+        std::make_shared<ompl::base::ConstrainedSpaceInformation>(constrained_state_space);
+
+    // TODO(jeroendm) Fix issues with sanity checks.
+    // The jacobian test is expected to fail because of the discontinuous constraint derivative.
+    // The issue with the state sampler is unresolved.
+    // int flags = 1 & ompl::base::ConstrainedStateSpace::CONSTRAINED_STATESPACE_JACOBIAN;
+    // flags = flags & ompl::base::ConstrainedStateSpace::CONSTRAINED_STATESPACE_SAMPLERS;
+    try
+    {
+      constrained_state_space->sanityChecks();
+    }
+    catch (ompl::Exception& ex)
+    {
+      ROS_ERROR("Sanity checks did not pass: %s", ex.what());
+    }
+  }
+
+protected:
+  const std::string group_name_;
+  const std::string robot_name_;
+
+  moveit::core::RobotModelPtr robot_model_;
+  robot_state::RobotStatePtr robot_state_;
+  const robot_state::JointModelGroup* joint_model_group_;
+
+  std::shared_ptr<ompl_interface::BaseConstraint> constraint_;
+
+  std::size_t num_dofs_;
+  std::string base_link_name_;
+  std::string ee_link_name_;
+};
+
+/***************************************************************************
+ * Run all tests on the Panda robot
+ * ************************************************************************/
+class PandaConstraintTest : public ConstraintTestBaseClass
+{
+protected:
+  PandaConstraintTest() : ConstraintTestBaseClass("panda", "panda_arm")
+  {
+  }
+};
+
+TEST_F(PandaConstraintTest, InitPositionConstraint)
+{
+  setPositionConstraints();
+}
+
+TEST_F(PandaConstraintTest, PositionConstraintJacobian)
+{
+  setPositionConstraints();
+  testJacobian();
+}
+
+TEST_F(PandaConstraintTest, PositionConstraintOMPLCheck)
+{
+  setPositionConstraints();
+  testOMPLProjectedStateSpaceConstruction();
+}
+
+TEST_F(PandaConstraintTest, InitAngleAxisConstraint)
+{
+  setOrientationConstraints();
+}
+
+TEST_F(PandaConstraintTest, AngleAxisConstraintOMPLCheck)
+{
+  setOrientationConstraints();
+  testOMPLProjectedStateSpaceConstruction();
+}
+
+/***************************************************************************
+ * Run all tests on the Fanuc robot
+ * ************************************************************************/
+class FanucConstraintTest : public ConstraintTestBaseClass
+{
+protected:
+  FanucConstraintTest() : ConstraintTestBaseClass("fanuc", "manipulator")
+  {
+  }
+};
+
+TEST_F(FanucConstraintTest, InitPositionConstraint)
+{
+  setPositionConstraints();
+}
+
+TEST_F(FanucConstraintTest, PositionConstraintJacobian)
+{
+  setPositionConstraints();
+  testJacobian();
+}
+
+TEST_F(FanucConstraintTest, PositionConstraintOMPLCheck)
+{
+  setPositionConstraints();
+  testOMPLProjectedStateSpaceConstruction();
+}
+
+TEST_F(FanucConstraintTest, InitAngleAxisConstraint)
+{
+  setOrientationConstraints();
+}
+
+TEST_F(FanucConstraintTest, AngleAxisConstraintOMPLCheck)
+{
+  setOrientationConstraints();
+  testOMPLProjectedStateSpaceConstruction();
+}
+
+/***************************************************************************
+ * Run all tests on the PR2's left arm
+ * ************************************************************************/
+class PR2LeftArmConstraintTest : public ConstraintTestBaseClass
+{
+protected:
+  PR2LeftArmConstraintTest() : ConstraintTestBaseClass("pr2", "left_arm")
+  {
+  }
+};
+
+TEST_F(PR2LeftArmConstraintTest, InitPositionConstraint)
+{
+  setPositionConstraints();
+}
+
+TEST_F(PR2LeftArmConstraintTest, PositionConstraintJacobian)
+{
+  setPositionConstraints();
+  testJacobian();
+}
+
+TEST_F(PR2LeftArmConstraintTest, PositionConstraintOMPLCheck)
+{
+  setPositionConstraints();
+  testOMPLProjectedStateSpaceConstruction();
+}
+
+TEST_F(PR2LeftArmConstraintTest, InitAngleAxisConstraint)
+{
+  setOrientationConstraints();
+}
+
+TEST_F(PR2LeftArmConstraintTest, AngleAxisConstraintOMPLCheck)
+{
+  setOrientationConstraints();
+  testOMPLProjectedStateSpaceConstruction();
+}
+
+/***************************************************************************
+ * MAIN
+ * ************************************************************************/
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
