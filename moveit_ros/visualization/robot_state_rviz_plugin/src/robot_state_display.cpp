bf5dfea2cec60bbbd15101df18bc3235f3adb6f3
v4hn <me@v4hn.de>
2020-08-05 21:29:04 +0200

    RViz RS & Traj display: ensure robot_description is available onEnable
    This is a (in my opinion) rather dirty workaround because RViz
    enables Displays systematically *before* loading their properties.
    
    So the patch loads the required property a second time *before* everything else.
    
    Assuming this ever changes we can remove this code again.
    
    A Display's "enabled" state is encoded in a boolean property the Display inherits from.
    The way loading works (recursively running through the properties)
    this special property is loaded first and triggers onEnable because
    of the Property's changed() signal.
    
    A second "Enabled" property exists in RViz's Displays but does not change behavior at all.

--- moveit_ros/visualization/robot_state_rviz_plugin/src/robot_state_display.cpp
+++ moveit_ros/visualization/robot_state_rviz_plugin/src/robot_state_display.cpp
@@ -399,6 +399,14 @@ void RobotStateDisplay::loadRobotModel()
   highlights_.clear();
 }
 
+void RobotStateDisplay::load(const rviz::Config& config)
+{
+  // This property needs to be loaded in onEnable() below, which is triggered
+  // in the beginning of Display::load() before the other property would be available
+  robot_description_property_->load(config.mapGetChild("Robot Description"));
+  Display::load(config);
+}
+
 void RobotStateDisplay::onEnable()
 {
   Display::onEnable();
