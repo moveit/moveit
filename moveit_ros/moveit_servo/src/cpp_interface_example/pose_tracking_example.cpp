2a0ed2104e45738ab923834ed9bf7c024d0493c7
AdamPettinger <adam.pettinger@utexas.edu>
2020-09-04 12:27:36 -0500

    First attempt at using pose tracking

---
+++ moveit_ros/moveit_servo/src/cpp_interface_example/pose_tracking_example.cpp
@@ -0,0 +1,143 @@
+/*******************************************************************************
+ *      Title     : pose_tracking_example.cpp
+ *      Project   : moveit_servo
+ *      Created   : 09/04/2020
+ *      Author    : Adam Pettinger
+ *
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2019, Los Alamos National Security, LLC
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+#include <std_msgs/Int8.h>
+
+#include <moveit_servo/servo.h>
+#include <moveit_servo/pose_tracking.h>
+#include <moveit_servo/status_codes.h>
+#include <moveit_servo/make_shared_from_pool.h>
+#include <thread>
+
+static const std::string LOGNAME = "cpp_interface_example";
+
+// Class for monitoring status of moveit_servo
+class StatusMonitor
+{
+public:
+  StatusMonitor(ros::NodeHandle& nh, const std::string& topic)
+  {
+    sub_ = nh.subscribe(topic, 1, &StatusMonitor::statusCB, this);
+  }
+
+private:
+  void statusCB(const std_msgs::Int8ConstPtr& msg)
+  {
+    moveit_servo::StatusCode latest_status = static_cast<moveit_servo::StatusCode>(msg->data);
+    if (latest_status != status_)
+    {
+      status_ = latest_status;
+      const auto& status_str = moveit_servo::SERVO_STATUS_CODE_MAP.at(status_);
+      ROS_INFO_STREAM_NAMED(LOGNAME, "Servo status: " << status_str);
+    }
+  }
+  moveit_servo::StatusCode status_ = moveit_servo::StatusCode::INVALID;
+  ros::Subscriber sub_;
+};
+
+/**
+ * Instantiate the C++ servo node interface.
+ * Send some Cartesian commands, then some joint commands.
+ */
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, LOGNAME);
+  ros::NodeHandle nh;
+  ros::AsyncSpinner spinner(8);
+  spinner.start();
+
+  // Load the planning scene monitor
+  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor;
+  planning_scene_monitor = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
+  if (!planning_scene_monitor->getPlanningScene())
+  {
+    ROS_ERROR_STREAM_NAMED(LOGNAME, "Error in setting up the PlanningSceneMonitor.");
+    exit(EXIT_FAILURE);
+  }
+
+  planning_scene_monitor->startSceneMonitor();
+  planning_scene_monitor->startWorldGeometryMonitor(
+      planning_scene_monitor::PlanningSceneMonitor::DEFAULT_COLLISION_OBJECT_TOPIC,
+      planning_scene_monitor::PlanningSceneMonitor::DEFAULT_PLANNING_SCENE_WORLD_TOPIC,
+      false /* skip octomap monitor */);
+  planning_scene_monitor->startStateMonitor();
+
+  // Run the servo node C++ interface in a new timer to ensure a constant outgoing message rate.
+  moveit_servo::PoseTracking tracker(planning_scene_monitor, "servo_server");
+
+
+  ros::Publisher target_pose_pub =
+        nh.advertise<geometry_msgs::PoseStamped>("/servo_server/target_pose", 1 /* queue */, true /* latch */);
+
+  // Subscribe to servo status (and log it when it changes)
+  StatusMonitor status_monitor(nh, "servo_server/status");
+
+  Eigen::Vector3d lin_tol{0.01, 0.01, 0.01};
+  double rot_tol = 0.1; 
+
+  geometry_msgs::PoseStamped target_pose;
+  target_pose.header.frame_id = "base_link";
+  target_pose.header.stamp = ros::Time::now();
+  target_pose.pose.position.x = 0.3;
+  target_pose.pose.position.y = 0;
+  target_pose.pose.position.z = 0.3;
+  target_pose.pose.orientation.x = 0;
+  target_pose.pose.orientation.y = 0;
+  target_pose.pose.orientation.z = 0;
+  target_pose.pose.orientation.w = 1.0;
+
+
+  target_pose_pub.publish(target_pose);
+
+  std::thread move_to_pose_thread([&tracker, &lin_tol, &rot_tol]{tracker.moveToPose(lin_tol, rot_tol);});
+
+  ros::Rate loop_rate(50);
+
+  for (size_t i=0; i<1000; ++i)
+  {
+    // target_pose_pub.publish(target_pose);
+
+    loop_rate.sleep();
+  }
+
+  tracker.stopMotion();
+  move_to_pose_thread.join();
+
+
+  return 0;
+}
