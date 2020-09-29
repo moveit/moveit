26c2bf6a9e3a6d5fd30a09157fcf6d2eede7006b
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2020-09-29 22:01:56 +0200

    Fix some more issues with create_ikfast_moveit_plugin.sh
    - require python-lxml and python-yaml to be installed
    - use str() to print Exception

--- moveit_kinematics/test/test_ikfast_plugins.sh
+++ moveit_kinematics/test/test_ikfast_plugins.sh
@@ -7,6 +7,17 @@
 # We will create ikfast plugins for fanuc and panda from moveit_resources
 # using the script auto_create_ikfast_moveit_plugin.sh
 
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+
+if [ "$ROS_DISTRO" == "noetic" ]; then
+	sudo update-alternatives --set python /usr/bin/python3
+	travis_run sudo apt-get -qq install python3-lxml python3-yaml
+else
+	sudo update-alternatives --set python /usr/bin/python2
+	travis_run sudo apt-get -qq install python-lxml python-yaml
+fi
+
 # Clone moveit_resources for URDFs. They are not available before running docker.
 travis_run git clone -q --depth=1 https://github.com/ros-planning/moveit_resources /tmp/resources
 fanuc=/tmp/resources/fanuc_description/urdf/fanuc.urdf
