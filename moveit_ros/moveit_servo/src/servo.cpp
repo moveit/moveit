4791e462494a24d76ab6a049d04d734f68594984
picknik-jliukkonen <jere@picknik.ai>
2020-10-07 16:16:08 -0600

    Set relative joint state topic names be relative to the parent namespace of the servo node.

--- moveit_ros/moveit_servo/src/servo.cpp
+++ moveit_ros/moveit_servo/src/servo.cpp
@@ -53,7 +53,16 @@ Servo::Servo(ros::NodeHandle& nh, const planning_scene_monitor::PlanningSceneMon
   if (!readParameters())
     exit(EXIT_FAILURE);
 
-  joint_state_subscriber_ = std::make_shared<JointStateSubscriber>(nh_, parameters_.joint_topic);
+  // By default joint topic name is relative to the node parent namespace. E.g. if the joint topic specified in the
+  // configuration files is "joint_states" and the node is launched in namespace "bar", which exists within
+  // namespace "foo", then the subscribed topic would be "/foo/bar/joint_states".
+  //
+  // Fully custom joint name topics can be used by using absolute topic names in config files. For example,
+  // joint topic parameter can be explicitly set to "/baz/my_joint_state_topic".
+  // **Note!** Make sure your planning scene monitor subscribes to correct joint states topic when using absolute
+  // joint state topic name *or* custom relative joint state topic name (for example, "my_joint_states").
+  ros::NodeHandle nh_parent_ns = ros::NodeHandle("");
+  joint_state_subscriber_ = std::make_shared<JointStateSubscriber>(nh_parent_ns, parameters_.joint_topic);
 
   servo_calcs_ =
       std::make_unique<ServoCalcs>(nh_, parameters_, planning_scene_monitor_, joint_state_subscriber_, parameter_ns_);
