62224be6423afeded44e0b894a26403da5aecbe4
picknik-jliukkonen <jere@picknik.ai>
2020-10-07 16:14:11 -0600

    Publish services correctly in the namespace of the node.

--- moveit_ros/moveit_servo/src/servo_calcs.cpp
+++ moveit_ros/moveit_servo/src/servo_calcs.cpp
@@ -114,15 +114,18 @@ ServoCalcs::ServoCalcs(ros::NodeHandle& nh, ServoParameters& parameters,
 
   // ROS Server for allowing drift in some dimensions
   drift_dimensions_server_ =
-      nh_.advertiseService(ros_namespace + "/change_drift_dimensions", &ServoCalcs::changeDriftDimensions, this);
+      nh_.advertiseService(ros::names::append(nh_.getNamespace(), "change_drift_dimensions"),
+                           &ServoCalcs::changeDriftDimensions, this);
 
   // ROS Server for changing the control dimensions
   control_dimensions_server_ =
-      nh_.advertiseService(ros_namespace + "/change_control_dimensions", &ServoCalcs::changeControlDimensions, this);
+      nh_.advertiseService(ros::names::append(nh_.getNamespace(), "change_control_dimensions"),
+                           &ServoCalcs::changeControlDimensions, this);
 
   // ROS Server to reset the status, e.g. so the arm can move again after a collision
   reset_servo_status_ =
-      nh_.advertiseService(ros_namespace + "/reset_servo_status", &ServoCalcs::resetServoStatus, this);
+      nh_.advertiseService(ros::names::append(nh_.getNamespace(), "reset_servo_status"),
+                           &ServoCalcs::resetServoStatus, this);
 
   // Publish and Subscribe to internal namespace topics
   ros::NodeHandle internal_nh("~internal");
