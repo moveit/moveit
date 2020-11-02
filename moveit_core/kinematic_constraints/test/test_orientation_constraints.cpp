08b4e68ec5b0a9a8ca4af062b692a63933906c0c
JeroenDM <jeroendemaeyer@live.be>
2020-11-02 08:49:30 +0100

    use floating joint model for orientation constraint test

--- moveit_core/kinematic_constraints/test/test_orientation_constraints.cpp
+++ moveit_core/kinematic_constraints/test/test_orientation_constraints.cpp
@@ -46,31 +46,11 @@ class LoadPlanningModelsPr2 : public testing::Test
 protected:
   void SetUp() override
   {
-    //robot_model_ = moveit::core::loadTestingRobotModel("pr2");
-
-    tf2::Quaternion q1, q2, q3;
-    q1.setRPY(M_PI_2, -M_PI_2, 0.0);
-    // q1 = tf2::Quaternion::getIdentity();
-
-    q2.setRPY(0.0, M_PI_2, 0.0);
-    // q2.setRPY(0.0, 0.0, M_PI_2);
-    // q2 = tf2::Quaternion::getIdentity();
-
-    // q3.setRPY(0.0, M_PI_2, 0.0);
-    q3.setRPY(0.0, 0.0, -M_PI_2);
-    // q3 = tf2::Quaternion::getIdentity();
-
-    geometry_msgs::Pose p1, p2, p3;
-    tf2::convert(q1, p1.orientation);
-    tf2::convert(q2, p2.orientation);
-    tf2::convert(q3, p3.orientation);
-
-    moveit::core::RobotModelBuilder robot("robot_wrist", "base_link");
-    robot.addChain("base_link->a->b->c", "revolute", {p1, p2, p3});
-    // robot.addChain("c->tool", "fixed", {p1});
-    robot.addGroupChain("base_link", "c", "wrist");
+    moveit::core::RobotModelBuilder robot("floating_robot", "base_link");
+    robot.addChain("base_link->ee", "floating");
+    robot.addGroupChain("base_link", "ee", "group1");
     ASSERT_TRUE(robot.isValid());
-    robot_wrist_model_ = robot.build();
+    robot_model_ = robot.build();
   }
 
   void TearDown() override
@@ -78,9 +58,22 @@ protected:
   }
 
 protected:
-  //moveit::core::RobotModelPtr robot_model_;
-  moveit::core::RobotModelPtr robot_wrist_model_;
+  moveit::core::RobotModelPtr robot_model_;
+  // moveit::core::RobotModelPtr robot_wrist_model_;
 };
+
+inline Eigen::Quaterniond xyz_intrinsix_to_quat(double rx, double ry, double rz)
+{
+  return Eigen::AngleAxisd(rx, Eigen::Vector3d::UnitX()) * Eigen::AngleAxisd(ry, Eigen::Vector3d::UnitY()) *
+         Eigen::AngleAxisd(rz, Eigen::Vector3d::UnitZ());
+}
+
+inline Eigen::Quaterniond rotation_vector_to_quat(double rx, double ry, double rz)
+{
+  Eigen::Vector3d v{ rx, ry, rz };
+  Eigen::Matrix3d m{ Eigen::AngleAxisd(v.norm(), v.normalized()) };
+  return Eigen::Quaterniond{ m };
+}
 
 TEST_F(SphericalRobot, Test1)
 {
@@ -284,49 +277,46 @@ TEST_F(SphericalRobot, Test5)
 
 TEST_F(LoadPlanningModelsPr2, OrientationConstraintsParameterization)
 {
-   moveit::core::RobotState robot_state(robot_wrist_model_);
+  moveit::core::RobotState robot_state(robot_model_);
   robot_state.setToDefaultValues();
   robot_state.update();
-  moveit::core::Transforms tf(robot_wrist_model_->getModelFrame());
+  moveit::core::Transforms tf(robot_model_->getModelFrame());
 
-  kinematic_constraints::OrientationConstraint oc(robot_wrist_model_);
+  kinematic_constraints::OrientationConstraint oc(robot_model_);
 
   moveit_msgs::OrientationConstraint ocm;
 
   // center the orientation constraints around the current orientation of the link
-  geometry_msgs::Pose p = tf2::toMsg(robot_state.getGlobalLinkTransform("c"));
+  geometry_msgs::Pose p = tf2::toMsg(robot_state.getGlobalLinkTransform("ee"));
   ocm.orientation = p.orientation;
 
-  ocm.link_name = "c";
-  ocm.header.frame_id = robot_wrist_model_->getModelFrame();
-  // ocm.orientation.x = 0.0;
-  // ocm.orientation.y = 0.0;
-  // ocm.orientation.z = 0.0;
-  // ocm.orientation.w = 1.0;
+  ocm.link_name = "ee";
+  ocm.header.frame_id = robot_model_->getModelFrame();
   ocm.absolute_x_axis_tolerance = 0.5;
   ocm.absolute_y_axis_tolerance = 0.5;
   ocm.absolute_z_axis_tolerance = 0.5;
   // ocm.parameterization = moveit_msgs::OrientationConstraint::XYZ_EULER_ANGLES;
-  ocm.parameterization = 3;
+  ocm.parameterization = moveit_msgs::OrientationConstraint::ROTATION_VECTOR;
+  // ocm.parameterization = 3;
   ocm.weight = 1.0;
 
   EXPECT_TRUE(oc.configure(ocm, tf));
 
-  // Constraints should be satisfied based on how we created them
+  // Constraints should be satisfied because we created them around the current orientation
   EXPECT_TRUE(oc.decide(robot_state, true).satisfied);
 
-  // move a joint and check the constraints again
-  std::map<std::string, double> jvals;
-  jvals["base_link-a-joint"] = 0.0;
-  jvals["a-b-joint"] = 0.5;
-  jvals["b-c-joint"] = 0.5;
-  robot_state.setVariablePositions(jvals);
+  // change the orientation of the end-effector to some fixed values
+  // and check the constraints again
+  // Eigen::Quaterniond quat = xyz_intrinsix_to_quat(0.1, 0.2, 0.3);
+  Eigen::Quaterniond quat = rotation_vector_to_quat(0.1, 0.2, 0.3);
+  Eigen::VectorXd joint_values(7);
+  joint_values << 0.0, 0.0, 0.0, quat.x(), quat.y(), quat.z(), quat.w();
+  robot_state.setJointGroupPositions("group1", joint_values);
   robot_state.update();
 
   EXPECT_FALSE(oc.decide(robot_state, true).satisfied);
 
-  //oc.decide()
-
+  // oc.decide()
 }
 
 // TEST_F(LoadPlanningModelsPr2, OrientationConstraintsParameterization)
