5de84ac4a8a183eb54d6f7c28fe73af52778af84
Kevin Chang <kevinshchang@gmail.com>
2020-11-16 10:19:00 -0800

    fix trajectory constraints for moveit commander
    remove unimplemented ability to load trajectory constraints from str
    
    Fixup by v4hn <me@v4hn.de>

--- moveit_commander/src/moveit_commander/move_group.py
+++ moveit_commander/src/moveit_commander/move_group.py
@@ -497,14 +497,14 @@ class MoveGroupCommander(object):
         self._g.clear_path_constraints()
 
     def get_trajectory_constraints(self):
-        """ Get the actual trajectory constraints in form of a moveit_msgs.msgs.Constraints """
-        c = Constraints()
+        """ Get the actual trajectory constraints in form of a moveit_msgs.msgs.TrajectoryConstraints """
+        c = TrajectoryConstraints()
         c_str = self._g.get_trajectory_constraints()
         conversions.msg_from_string(c, c_str)
         return c
 
     def set_trajectory_constraints(self, value):
-        """ Specify the trajectory constraints to be used """
+        """ Specify the trajectory constraints to be used (setting from database is not implemented yet)"""
         if value is None:
             self.clear_trajectory_constraints()
         else:
@@ -512,7 +512,7 @@ class MoveGroupCommander(object):
                 self._g.set_trajectory_constraints_from_msg(
                     conversions.msg_to_string(value)
                 )
-            elif not self._g.set_trajectory_constraints(value):
+            else:
                 raise MoveItCommanderException(
                     "Unable to set trajectory constraints " + value
                 )
