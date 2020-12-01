0cea46e2c0719d53ba33746e26599964ceb0c70f
Tyler Weaver <tylerjw@gmail.com>
2020-12-01 07:27:55 -0700

    [fix] servo_server base node namespace

--- moveit_ros/moveit_servo/src/servo_server.cpp
+++ moveit_ros/moveit_servo/src/servo_server.cpp
@@ -52,7 +52,7 @@ int main(int argc, char** argv)
   ros::AsyncSpinner spinner(ROS_THREADS);
   spinner.start();
 
-  ros::NodeHandle nh("~");
+  ros::NodeHandle nh(ros::this_node::getName());
 
   // Load the planning scene monitor
   auto planning_scene_monitor = std::make_shared<planning_scene_monitor::PlanningSceneMonitor>("robot_description");
