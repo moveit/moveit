c9637e11a4e1235eb5845e6717acbd4bf4e8e5a0
Peter Mitrano <mitranopeter@gmail.com>
2021-03-10 23:30:16 -0500

    reorder bindings, make bindings not required

--- moveit_core/collision_detection/src/pycollision_detection.cpp
+++ moveit_core/collision_detection/src/pycollision_detection.cpp
@@ -14,8 +14,29 @@ auto SET_ENTRY1 = py::overload_cast<const std::string&, const std::string&, bool
 
 void def_collision_detect_bindings(py::module& m)
 {
-  m.doc() = "World represents the state of the collision objects, robot, and other environment information";
-  py::class_<World, WorldPtr>(m, "World").def(py::init<>());
+  m.doc() = "contains collision detection, the world, and allowed collision matrices";
+  py::enum_<BodyType>(m, "BodyType")
+      .value("ROBOT_ATTACHED", BodyType::ROBOT_ATTACHED)
+      .value("ROBOT_LINK", BodyType::ROBOT_LINK)
+      .value("WORLD_OBJECT", BodyType::WORLD_OBJECT)
+      .export_values();
+  py::class_<Contact>(m, "Contact")
+      .def(py::init<>())
+      .def_readwrite("body_name_1", &Contact::body_name_1)
+      .def_readwrite("body_name_2", &Contact::body_name_2)
+      .def_readwrite("body_type_1", &Contact::body_type_1)
+      .def_readwrite("body_type_2", &Contact::body_type_2)
+      .def_readwrite("depth", &Contact::depth)
+      .def_property_readonly("nearest_points",
+                             [](const Contact & contact) {
+                               std::vector<Eigen::Vector3d> v{ contact.nearest_points[0], contact.nearest_points[1] };
+                               return v;
+                             })
+      .def_readwrite("normal", &Contact::normal)
+      .def_readwrite("percent_interpolation", &Contact::percent_interpolation)
+      .def_readwrite("pos", &Contact::pos)
+    //
+      ;
   py::class_<CollisionRequest>(m, "CollisionRequest")
       .def(py::init<>())
       .def_readwrite("contacts", &CollisionRequest::contacts)
@@ -42,26 +63,5 @@ void def_collision_detect_bindings(py::module& m)
   py::class_<AllowedCollisionMatrix>(m, "AllowedCollisionMatrix").def(py::init<>()).def("setEntry", SET_ENTRY1)
       //
       ;
-  py::class_<Contact>(m, "Contact")
-      .def(py::init<>())
-      .def_readwrite("body_name_1", &Contact::body_name_1)
-      .def_readwrite("body_name_2", &Contact::body_name_2)
-      .def_readwrite("body_type_1", &Contact::body_type_1)
-      .def_readwrite("body_type_2", &Contact::body_type_2)
-      .def_readwrite("depth", &Contact::depth)
-      .def_property_readonly("nearest_points",
-                             [](const Contact & contact) {
-                               std::vector<Eigen::Vector3d> v{ contact.nearest_points[0], contact.nearest_points[1] };
-                               return v;
-                             })
-      .def_readwrite("normal", &Contact::normal)
-      .def_readwrite("percent_interpolation", &Contact::percent_interpolation)
-      .def_readwrite("pos", &Contact::pos)
-      //
-      ;
-  py::enum_<BodyType>(m, "BodyType")
-      .value("ROBOT_ATTACHED", BodyType::ROBOT_ATTACHED)
-      .value("ROBOT_LINK", BodyType::ROBOT_LINK)
-      .value("WORLD_OBJECT", BodyType::WORLD_OBJECT)
-      .export_values();
+  py::class_<World, WorldPtr>(m, "World").def(py::init<>());
 }
