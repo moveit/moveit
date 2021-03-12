de404322c7e2c4edd3b30d5239f06b1df46fb43f
Peter Mitrano <mitranopeter@gmail.com>
2021-03-11 23:51:58 -0500

    implement full transforms API

--- moveit_core/transforms/src/pytransforms.cpp
+++ moveit_core/transforms/src/pytransforms.cpp
@@ -12,7 +12,21 @@ void def_transforms_bindings(py::module& m)
   m.doc() = "Provides an implementation of a snapshot of a transform tree that can be easily queried for transforming "
             "different quantities. Transforms are maintained as a list of transforms to a particular frame. All stored "
             "transforms are considered fixed.";
-  py::class_<Transforms, TransformsPtr>(m, "Transforms").def(py::init<const std::string&>())
+  py::class_<Transforms, TransformsPtr>(m, "Transforms")
+      .def(py::init<const std::string&>())
+      .def("canTransform", &Transforms::canTransform)
+      .def("getTargetFrame", &Transforms::getTargetFrame)
+      .def("getTransform", &Transforms::getTransform)
+      .def("isFixedFrame", &Transforms::isFixedFrame)
+      .def("getAllTransforms", &Transforms::getAllTransforms)
+      .def("setTransform", py::overload_cast<const Eigen::Isometry3d&, const std::string&>(&Transforms::setTransform))
+      .def("setTransform", py::overload_cast<const geometry_msgs::TransformStamped&>(&Transforms::setTransform))
+      .def("setTransforms", &Transforms::setTransforms)
+      .def("setAllTransforms", &Transforms::setAllTransforms)
+      .def("transformVector3", &Transforms::transformVector3)
+      .def("transformQuaternion", &Transforms::transformQuaternion)
+      .def("transformRotationMatrix", &Transforms::transformRotationMatrix)
+      .def("transformPose", &Transforms::transformPose)
       //
       ;
 }
