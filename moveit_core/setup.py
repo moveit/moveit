bc0709356f1469b597776e44cdba52e419f1d088
Peter Mitrano <mitranopeter@gmail.com>
2021-03-17 20:53:34 -0400

    setup python install correctly

--- moveit_core/setup.py
+++ moveit_core/setup.py
@@ -1,10 +1,10 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages, setup
 
 d = generate_distutils_setup(
-    packages=['moveit_core'],
+    packages=find_packages('src'),
     package_dir={'': 'src'}
 )
 
