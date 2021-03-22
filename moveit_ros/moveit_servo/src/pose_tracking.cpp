f310c58e2fa821347e917b37c95db322a02201b6
AdamPettinger <adam.pettinger@utexas.edu>
2021-03-22 08:45:47 -0500

    [Servo] Halt Servo command on Pose Tracking stop (#2501)
    * Publish Servo stop in Pose Tracking cleanup
    
    * Move publish halt to stopMotion()

--- moveit_ros/moveit_servo/src/pose_tracking.cpp
+++ moveit_ros/moveit_servo/src/pose_tracking.cpp
@@ -309,8 +309,23 @@ geometry_msgs::TwistStampedConstPtr PoseTracking::calculateTwistCommand()
   return msg;
 }
 
+void PoseTracking::stopMotion()
+{
+  stop_requested_ = true;
+
+  // Send a 0 command to Servo to halt arm motion
+  auto msg = moveit::util::make_shared_from_pool<geometry_msgs::TwistStamped>();
+  {
+    std::lock_guard<std::mutex> lock(target_pose_mtx_);
+    msg->header.frame_id = target_pose_.header.frame_id;
+  }
+  msg->header.stamp = ros::Time::now();
+  twist_stamped_pub_.publish(msg);
+}
+
 void PoseTracking::doPostMotionReset()
 {
+  stopMotion();
   stop_requested_ = false;
   angular_error_ = boost::none;
 
