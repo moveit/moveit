55070f8475f83c45b4ce8e095ea0e020d39c6c5b
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-03-30 09:17:21 +0200

    Correctly configure moveit as a namespace package

--- moveit_core/python/src/moveit/__init__.py
+++ moveit_core/python/src/moveit/__init__.py
@@ -1,1 +1,2 @@
-from . import core
+# https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
