27c8a1375b97e7ea40788c9caa9ac8cbc55c451a
Felix von Drigalski <FvDrigalski@gmail.com>
2021-04-29 16:58:00 +0900

    Add planning_pipeline_id setting to Python MGI (#2622)

--- moveit_commander/src/moveit_commander/move_group.py
+++ moveit_commander/src/moveit_commander/move_group.py
@@ -532,12 +532,20 @@ class MoveGroupCommander(object):
         """ Specify the amount of time to be used for motion planning. """
         return self._g.get_planning_time()
 
+    def set_planning_pipeline_id(self, planning_pipeline):
+        """ Specify which planning pipeline to use when motion planning (e.g. ompl, pilz_industrial_motion_planner) """
+        self._g.set_planning_pipeline_id(planning_pipeline)
+
+    def get_planning_pipeline_id(self, planning_pipeline):
+        """ Get the current planning_pipeline_id (e.g. ompl, pilz_industrial_motion_planner) """
+        self._g.get_planning_pipeline_id(planning_pipeline)
+
     def set_planner_id(self, planner_id):
-        """ Specify which planner to use when motion planning """
+        """ Specify which planner of the currently selected pipeline to use when motion planning (e.g. RRTConnect, LIN) """
         self._g.set_planner_id(planner_id)
 
     def get_planner_id(self):
-        """ Get the current planner_id """
+        """ Get the current planner_id (e.g. RRTConnect, LIN) of the currently selected pipeline """
         return self._g.get_planner_id()
 
     def set_num_planning_attempts(self, num_planning_attempts):
