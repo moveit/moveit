ea4f4ab374986a2970507542bb535fb4d92d1d10
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-05-06 16:59:17 +0200

    Fix ikfast testing

--- moveit_kinematics/test/test_ikfast_plugins.sh
+++ moveit_kinematics/test/test_ikfast_plugins.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Script to test ikfast plugin creation and functionality
-# This script is intended to run as a BEFORE_DOCKER_SCRIPT from Travis.
-# Particularly we assume that the travis_* utility functions are available.
 
 # We will create ikfast plugins for fanuc and panda from moveit_resources
 # using the script auto_create_ikfast_moveit_plugin.sh
+
+set -e # fail script on error
 
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 
 if [ "$ROS_DISTRO" == "noetic" ]; then
 	sudo update-alternatives --set python /usr/bin/python3
-	travis_run sudo apt-get -qq install python3-lxml python3-yaml
+	sudo apt-get -qq install python3-lxml python3-yaml
 else
 	sudo update-alternatives --set python /usr/bin/python2
-	travis_run sudo apt-get -qq install python-lxml python-yaml
+	sudo apt-get -qq install python-lxml python-yaml
 fi
 
 # Clone moveit_resources for URDFs. They are not available before running docker.
-travis_run git clone -q --depth=1 https://github.com/ros-planning/moveit_resources /tmp/resources
+git clone -q --depth=1 https://github.com/ros-planning/moveit_resources /tmp/resources
 fanuc=/tmp/resources/fanuc_description/urdf/fanuc.urdf
 panda=/tmp/resources/panda_description/urdf/panda.urdf
 
@@ -27,10 +27,10 @@ panda=/tmp/resources/panda_description/urdf/panda.urdf
 test ${QUIET:-1} -eq 0 && QUIET="" || QUIET="--quiet"
 
 # Create ikfast plugins for Fanuc and Panda
-travis_run moveit_kinematics/ikfast_kinematics_plugin/scripts/auto_create_ikfast_moveit_plugin.sh \
+moveit_kinematics/ikfast_kinematics_plugin/scripts/auto_create_ikfast_moveit_plugin.sh \
 	$QUIET --name fanuc --pkg $PWD/fanuc_ikfast_plugin $fanuc manipulator base_link tool0
 
-travis_run moveit_kinematics/ikfast_kinematics_plugin/scripts/auto_create_ikfast_moveit_plugin.sh \
+moveit_kinematics/ikfast_kinematics_plugin/scripts/auto_create_ikfast_moveit_plugin.sh \
 	$QUIET --name panda --pkg $PWD/panda_ikfast_plugin $panda panda_arm panda_link0 panda_link8
 
 echo "Done."
