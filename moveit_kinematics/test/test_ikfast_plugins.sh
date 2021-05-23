8770a18f752e5e7c21fd0e029af3fd2990b8ab67
Robert Haschke <rhaschke@users.noreply.github.com>
2021-05-23 15:40:37 +0200

    Improve ikfast QUIET handling (#2685)
    Write stdout+stderr to intermediate log file which is shown on failure.

--- moveit_kinematics/test/test_ikfast_plugins.sh
+++ moveit_kinematics/test/test_ikfast_plugins.sh
@@ -23,14 +23,13 @@ git clone -q --depth=1 https://github.com/ros-planning/moveit_resources /tmp/res
 fanuc=/tmp/resources/fanuc_description/urdf/fanuc.urdf
 panda=/tmp/resources/panda_description/urdf/panda.urdf
 
-# Translate environment variable QUIET=[0 | 1] into actual option
-test ${QUIET:-1} -eq 0 && QUIET="" || QUIET="--quiet"
+export QUIET=${QUIET:=1}
 
 # Create ikfast plugins for Fanuc and Panda
 moveit_kinematics/ikfast_kinematics_plugin/scripts/auto_create_ikfast_moveit_plugin.sh \
-	$QUIET --name fanuc --pkg $PWD/fanuc_ikfast_plugin $fanuc manipulator base_link tool0
+	--name fanuc --pkg $PWD/fanuc_ikfast_plugin $fanuc manipulator base_link tool0
 
 moveit_kinematics/ikfast_kinematics_plugin/scripts/auto_create_ikfast_moveit_plugin.sh \
-	$QUIET --name panda --pkg $PWD/panda_ikfast_plugin $panda panda_arm panda_link0 panda_link8
+	--name panda --pkg $PWD/panda_ikfast_plugin $panda panda_arm panda_link0 panda_link8
 
 echo "Done."
