11c7bd7dc37035e090f0b3f31c5896b0eba09509
Felix von Drigalski <FvDrigalski@gmail.com>
2021-08-07 22:05:43 +0900

    Add planning_pipeline_id to MotionSequence service (#2755)
    * Add planning_pipeline_id to MotionSequence action and service
    
    * check for empty request
    
    Co-authored-by: Joachim Schleicher <j.schleicher@pilz.de>

--- moveit_planners/pilz_industrial_motion_planner/src/move_group_sequence_action.cpp
+++ moveit_planners/pilz_industrial_motion_planner/src/move_group_sequence_action.cpp
@@ -194,7 +194,18 @@ void MoveGroupSequenceAction::executeMoveCallbackPlanOnly(const moveit_msgs::Mov
   RobotTrajCont traj_vec;
   try
   {
-    traj_vec = command_list_manager_->solve(the_scene, context_->planning_pipeline_, goal->request);
+    // Select planning_pipeline to handle request
+    // All motions in the SequenceRequest need to use the same planning pipeline (but can use different planners)
+    const planning_pipeline::PlanningPipelinePtr planning_pipeline =
+        resolvePlanningPipeline(goal->request.items[0].req.pipeline_id);
+    if (!planning_pipeline)
+    {
+      ROS_ERROR_STREAM("Could not load planning pipeline " << goal->request.items[0].req.pipeline_id);
+      res.response.error_code.val = moveit_msgs::MoveItErrorCodes::FAILURE;
+      return;
+    }
+
+    traj_vec = command_list_manager_->solve(the_scene, planning_pipeline, goal->request);
   }
   catch (const MoveItErrorCodeException& ex)
   {
