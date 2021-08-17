1c2d3493b30a5ae951409c15af4b87532e81f7ca
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-08-17 21:49:04 +0200

    Don't use std::pair of string refs as it triggers a stack-use-after-scope error
    This optimization was originally introduced in #2698 to avoid string copying.

--- moveit_core/collision_detection_fcl/src/collision_common.cpp
+++ moveit_core/collision_detection_fcl/src/collision_common.cpp
@@ -495,9 +495,9 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
 
   double dist_threshold = cdata->req->distance_threshold;
 
-  const std::pair<const std::string&, const std::string&> pc = cd1->getID() < cd2->getID() ?
-                                                                   std::make_pair(cd1->getID(), cd2->getID()) :
-                                                                   std::make_pair(cd2->getID(), cd1->getID());
+  const std::pair<const std::string, const std::string> pc = cd1->getID() < cd2->getID() ?
+                                                                 std::make_pair(cd1->getID(), cd2->getID()) :
+                                                                 std::make_pair(cd2->getID(), cd1->getID());
 
   DistanceMap::iterator it = cdata->res->distances.find(pc);
 

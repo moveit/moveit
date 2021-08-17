moveit/moveit

commit 1eb2af5462608c1ec414e6788cebd726aa7e9f7c

Use thread_local var's in FCL distanceCallback() (#2698)
* Use thread_local var's in FCL distanceCallback()

* Use thread_local on std::pair, too

* Add temporary benchmarking code

* Revert "Add temporary benchmarking code"

This reverts commit f2ed42bcca7dd8ce23ff5d6ea3ffa058bd83ad15.

* Format

* Clear thread-local vars between iterations

* Do not use thread_local for POD data types

* One include is not needed

* dsitance_result.clear() is not necessary

The .clear() function is documented here:
https://github.com/ros-planning/moveit/blob/master/moveit_core/collision_detection/include/moveit/collision_detection/collision_common.h
Everything that the .clear() call does is already covered by the following lines.

* Short-circuit GLOBAL distance_threshold initialization

* Went a little overboard with thread-local

* Slightly more readable conversion to Eigen vector.

* Use std::pair of string references

* Avoid extra copy for Eigen::Vector3d

* Add comments about clearing thread_local variables

Co-authored-by: Robert Haschke <rhaschke@techfak.uni-bielefeld.de>

moveit_core/collision_detection_fcl/src/collision_common.cpp
@@ -443,7 +443,6 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
   if (cdata->req->acm)
   {
     AllowedCollision::Type type;
-
     bool found = cdata->req->acm->getAllowedCollision(cd1->getID(), cd2->getID(), type);
     if (found)
     {
@@ -495,13 +494,19 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
 
   double dist_threshold = cdata->req->distance_threshold;
 
-  const std::pair<std::string, std::string>& pc = cd1->getID() < cd2->getID() ?
-                                                      std::make_pair(cd1->getID(), cd2->getID()) :
-                                                      std::make_pair(cd2->getID(), cd1->getID());
+  const std::pair<const std::string&, const std::string&> pc = cd1->getID() < cd2->getID() ?
+                                                                   std::make_pair(cd1->getID(), cd2->getID()) :
+                                                                   std::make_pair(cd2->getID(), cd1->getID());
 
   DistanceMap::iterator it = cdata->res->distances.find(pc);
 
-  if (it != cdata->res->distances.end())
+  // GLOBAL search: for efficiency, distance_threshold starts at the smallest distance between any pairs found so far
+  if (cdata->req->type == DistanceRequestType::GLOBAL)
+  {
+    dist_threshold = cdata->res->minimum_distance.distance;
+  }
+  // Check if a distance between this pair has been found yet. Decrease threshold_distance if so, to narrow the search
+  else if (it != cdata->res->distances.end())
   {
     if (cdata->req->type == DistanceRequestType::LIMITED)
     {
@@ -510,10 +515,6 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
       {
         return cdata->done;
       }
-    }
-    else if (cdata->req->type == DistanceRequestType::GLOBAL)
-    {
-      dist_threshold = cdata->res->minimum_distance.distance;
     }
     else if (cdata->req->type == DistanceRequestType::SINGLE)
     {
@@ -531,14 +532,16 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
   {
     return false;
   }
-  double d = fcl::distance(o1, o2, fcl::DistanceRequestd(cdata->req->enable_nearest_points), fcl_result);
+  double distance = fcl::distance(o1, o2, fcl::DistanceRequestd(cdata->req->enable_nearest_points), fcl_result);
 
   // Check if either object is already in the map. If not add it or if present
   // check to see if the new distance is closer. If closer remove the existing
   // one and add the new distance information.
-  if (d < dist_threshold)
+  if (distance < dist_threshold)
   {
-    DistanceResultsData dist_result;
+    // thread_local storage makes this variable persistent. We do not clear it at every iteration because all members
+    // get overwritten.
+    thread_local DistanceResultsData dist_result;
     dist_result.distance = fcl_result.min_distance;
 
     // Careful here: Get the collision geometry data again, since FCL might
@@ -550,8 +553,8 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
     dist_result.nearest_points[0] = fcl_result.nearest_points[0];
     dist_result.nearest_points[1] = fcl_result.nearest_points[1];
 #else
-    dist_result.nearest_points[0] = Eigen::Vector3d(fcl_result.nearest_points[0].data.vs);
-    dist_result.nearest_points[1] = Eigen::Vector3d(fcl_result.nearest_points[1].data.vs);
+    dist_result.nearest_points[0] = Eigen::Map<const Eigen::Vector3d>(fcl_result.nearest_points[0].data.vs);
+    dist_result.nearest_points[1] = Eigen::Map<const Eigen::Vector3d>(fcl_result.nearest_points[1].data.vs);
 #endif
     dist_result.link_names[0] = res_cd1->getID();
     dist_result.link_names[1] = res_cd2->getID();
@@ -562,14 +565,15 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
       dist_result.normal = (dist_result.nearest_points[1] - dist_result.nearest_points[0]).normalized();
     }
 
-    if (d <= 0 && cdata->req->enable_signed_distance)
+    if (distance <= 0 && cdata->req->enable_signed_distance)
     {
       dist_result.nearest_points[0].setZero();
       dist_result.nearest_points[1].setZero();
       dist_result.normal.setZero();
 
       fcl::CollisionRequestd coll_req;
-      fcl::CollisionResultd coll_res;
+      thread_local fcl::CollisionResultd coll_res;
+      coll_res.clear();  // thread_local storage makes the variable persistent. Ensure that it is cleared!
       coll_req.enable_contact = true;
       coll_req.num_max_contacts = 200;
       std::size_t contacts = fcl::collide(o1, o2, coll_req, coll_res);
@@ -589,13 +593,13 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
 
         const fcl::Contactd& contact = coll_res.getContact(max_index);
         dist_result.distance = -contact.penetration_depth;
+
 #if (MOVEIT_FCL_VERSION >= FCL_VERSION_CHECK(0, 6, 0))
         dist_result.nearest_points[0] = contact.pos;
         dist_result.nearest_points[1] = contact.pos;
 #else
-        dist_result.nearest_points[0] = Eigen::Vector3d(contact.pos.data.vs);
-        dist_result.nearest_points[1] = Eigen::Vector3d(contact.pos.data.vs);
-        Eigen::Vector3d normal(contact.normal.data.vs);
+        dist_result.nearest_points[0] = Eigen::Map<const Eigen::Vector3d>(contact.pos.data.vs);
+        dist_result.nearest_points[1] = Eigen::Map<const Eigen::Vector3d>(contact.pos.data.vs);
 #endif
 
         if (cdata->req->enable_nearest_points)
@@ -603,7 +607,7 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
 #if (MOVEIT_FCL_VERSION >= FCL_VERSION_CHECK(0, 6, 0))
           Eigen::Vector3d normal = contact.normal;
 #else
-          Eigen::Vector3d normal(contact.normal.data.vs);
+          Eigen::Vector3d normal = Eigen::Map<const Eigen::Vector3d>(contact.normal.data.vs);
 #endif
 
           // Check order of o1/o2 again, we might need to flip the normal

commit 1c2d3493b30a5ae951409c15af4b87532e81f7ca

Don't use std::pair of string refs as it triggers a stack-use-after-scope error
This optimization was originally introduced in #2698 to avoid string copying.

moveit_core/collision_detection_fcl/src/collision_common.cpp
@@ -495,9 +495,9 @@ bool distanceCallback(fcl::CollisionObjectd* o1, fcl::CollisionObjectd* o2, void
 
   double dist_threshold = cdata->req->distance_threshold;
 
-  const std::pair<const std::string&, const std::string&> pc = cd1->getID() < cd2->getID() ?
-                                                                   std::make_pair(cd1->getID(), cd2->getID()) :
-                                                                   std::make_pair(cd2->getID(), cd1->getID());
+  const std::pair<const std::string, const std::string> pc = cd1->getID() < cd2->getID() ?
+                                                                 std::make_pair(cd1->getID(), cd2->getID()) :
+                                                                 std::make_pair(cd2->getID(), cd1->getID());
 
   DistanceMap::iterator it = cdata->res->distances.find(pc);
 