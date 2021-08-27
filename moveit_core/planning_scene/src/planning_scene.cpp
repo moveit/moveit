7604617f816093cc4987437da3cf9cd5a10774d1
Mike Wiznitzer <mechanic.wiz@gmail.com>
2021-08-27 16:16:04 -0400

    identity matrix assumes normal quaternion. do an explicity check to verify all pose fields are 0

--- moveit_core/planning_scene/src/planning_scene.cpp
+++ moveit_core/planning_scene/src/planning_scene.cpp
@@ -1718,9 +1718,14 @@ bool PlanningScene::shapesAndPosesFromCollisionObjectMessage(const moveit_msgs::
 
   bool switch_object_pose_and_shape_pose = false;
   if (num_shapes == 1)
-    if (object_pose.isApprox(Eigen::Isometry3d::Identity()))
+    if (object.pose.position.x == 0 && object.pose.position.y == 0 &&
+        object.pose.position.z == 0 && object.pose.orientation.x == 0 &&
+        object.pose.orientation.y == 0 && object.pose.orientation.z == 0 &&
+        object.pose.orientation.w == 0)
+    {
       switch_object_pose_and_shape_pose = true;  // If the object pose is not set but the shape pose is,
                                                  // use the shape's pose as the object pose.
+    }
 
   auto append = [&object_pose, &shapes, &shape_poses,
                  &switch_object_pose_and_shape_pose](shapes::Shape* s, const geometry_msgs::Pose& pose_msg) {
