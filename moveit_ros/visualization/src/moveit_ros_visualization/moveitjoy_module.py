9748188fa5b22c1a334104dda3f5b40a15f805c5
lorepieri8 <lorepieri@hotmail.it>
2021-09-01 11:41:12 +0200

    Fix legacy Python2 lines
    Closes #2859

--- moveit_ros/visualization/src/moveit_ros_visualization/moveitjoy_module.py
+++ moveit_ros/visualization/src/moveit_ros_visualization/moveitjoy_module.py
@@ -582,7 +582,7 @@ class MoveitJoy:
             self.current_planning_group_index = next_index
         next_planning_group = None
         try:
-            next_planning_group = self.planning_groups_keys[
+            next_planning_group = list(self.planning_groups_keys)[
                 self.current_planning_group_index
             ]
         except IndexError:
@@ -593,7 +593,7 @@ class MoveitJoy:
         self.plan_group_pub.publish(next_planning_group)
 
     def updatePoseTopic(self, next_index, wait=True):
-        planning_group = self.planning_groups_keys[self.current_planning_group_index]
+        planning_group = list(self.planning_groups_keys)[self.current_planning_group_index]
         topics = self.planning_groups[planning_group]
         if next_index >= len(topics):
             self.current_eef_index = 0
@@ -764,7 +764,7 @@ class MoveitJoy:
             # when not initialized, we will force to change planning_group
             while True:
                 self.updatePlanningGroup(self.current_planning_group_index)
-                planning_group = self.planning_groups_keys[
+                planning_group = list(self.planning_groups_keys)[
                     self.current_planning_group_index
                 ]
                 topics = self.planning_groups[planning_group]
