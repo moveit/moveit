d1465a7dd55ffead03b35f9d0d3d878fc4757eeb
Michael Wiznitzer <31753650+mechwiz@users.noreply.github.com>
2021-10-27 09:36:41 -0400

    Servo: sync position limit enforcement with MoveIt2 (#2898)
    * fix enforce position bug
    
    * remove unnecessary variable
    
    * make clang tidy happy
    
    * Update my comment
    
    * implement same logic as in the moveit2! repo
    
    * fix copy-pase error
    
    Co-authored-by: Michael Wiznitzer <michael.wiznitzer@resquared.com>
    Co-authored-by: AndyZe <andyz@utexas.edu>

--- moveit_ros/moveit_servo/src/servo_calcs.cpp
+++ moveit_ros/moveit_servo/src/servo_calcs.cpp
@@ -580,7 +580,7 @@ bool ServoCalcs::convertDeltasToOutgoingCmd(trajectory_msgs::JointTrajectory& jo
 
   composeJointTrajMessage(internal_joint_state_, joint_trajectory);
 
-  if (!enforcePositionLimits())
+  if (!enforcePositionLimits(internal_joint_state_))
   {
     suddenHalt(joint_trajectory);
     status_ = StatusCode::JOINT_BOUND;
@@ -779,7 +779,7 @@ void ServoCalcs::enforceVelLimits(Eigen::ArrayXd& delta_theta)
   delta_theta = velocity_scaling_factor * velocity * parameters_.publish_period;
 }
 
-bool ServoCalcs::enforcePositionLimits()
+bool ServoCalcs::enforcePositionLimits(sensor_msgs::JointState& joint_state)
 {
   bool halting = false;
 
@@ -797,14 +797,18 @@ bool ServoCalcs::enforcePositionLimits()
     }
     if (!current_state_->satisfiesPositionBounds(joint, -parameters_.joint_limit_margin))
     {
-      const std::vector<moveit_msgs::JointLimits> limits = joint->getVariableBoundsMsg();
+      const std::vector<moveit_msgs::JointLimits>& limits = joint->getVariableBoundsMsg();
 
       // Joint limits are not defined for some joints. Skip them.
       if (!limits.empty())
       {
-        if ((current_state_->getJointVelocities(joint)[0] < 0 &&
+        // Check if pending velocity command is moving in the right direction
+        auto joint_itr = std::find(joint_state.name.begin(), joint_state.name.end(), joint->getName());
+        auto joint_idx = std::distance(joint_state.name.begin(), joint_itr);
+
+        if ((joint_state.velocity.at(joint_idx) < 0 &&
              (joint_angle < (limits[0].min_position + parameters_.joint_limit_margin))) ||
-            (current_state_->getJointVelocities(joint)[0] > 0 &&
+            (joint_state.velocity.at(joint_idx) > 0 &&
              (joint_angle > (limits[0].max_position - parameters_.joint_limit_margin))))
         {
           ROS_WARN_STREAM_THROTTLE_NAMED(ROS_LOG_THROTTLE_PERIOD, LOGNAME,
