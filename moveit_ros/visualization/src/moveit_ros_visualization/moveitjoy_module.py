dac4ecdfdc1a0bdfc9504288031df2b165aecb07
Job van Dieten <JvanDiet@its.jnj.com>
2022-01-13 23:28:16 +0100

    Removed all parentheses

--- moveit_ros/visualization/src/moveit_ros_visualization/moveitjoy_module.py
+++ moveit_ros/visualization/src/moveit_ros_visualization/moveitjoy_module.py
@@ -94,23 +94,23 @@ class JoyStatus:
 class XBoxStatus(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
-        self.center = (msg.buttons[8] == 1)
-        self.select = (msg.buttons[6] == 1)
-        self.start = (msg.buttons[7] == 1)
-        self.L3 = (msg.buttons[9] == 1)
-        self.R3 = (msg.buttons[10] == 1)
-        self.square = (msg.buttons[2] == 1)
-        self.circle = (msg.buttons[1] == 1)
-        self.up = (msg.axes[7] > 0.1)
-        self.down = (msg.axes[7] < -0.1)
-        self.left = (msg.axes[6] > 0.1)
-        self.right = (msg.axes[6] < -0.1)
-        self.triangle = (msg.buttons[3] == 1)
-        self.cross = (msg.buttons[0] == 1)
-        self.L1 = (msg.buttons[4] == 1)
-        self.R1 = (msg.buttons[5] == 1)
-        self.L2 = (msg.axes[2] < -0.5)
-        self.R2 = (msg.axes[5] < -0.5)
+        self.center = msg.buttons[8] == 1
+        self.select = msg.buttons[6] == 1
+        self.start = msg.buttons[7] == 1
+        self.L3 = msg.buttons[9] == 1
+        self.R3 = msg.buttons[10] == 1
+        self.square = msg.buttons[2] == 1
+        self.circle = msg.buttons[1] == 1
+        self.up = msg.axes[7] > 0.1
+        self.down = msg.axes[7] < -0.1
+        self.left = msg.axes[6] > 0.1
+        self.right = msg.axes[6] < -0.1
+        self.triangle = msg.buttons[3] == 1
+        self.cross = msg.buttons[0] == 1
+        self.L1 = msg.buttons[4] == 1
+        self.R1 = msg.buttons[5] == 1
+        self.L2 = msg.axes[2] < -0.5
+        self.R2 = msg.axes[5] < -0.5
         self.left_analog_x = msg.axes[0]
         self.left_analog_y = msg.axes[1]
         self.right_analog_x = msg.axes[3]
@@ -122,17 +122,17 @@ class PS3DualShockStatus(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
         # creating from sensor_msgs/Joy
-        self.cross = (msg.buttons[0] == 1)
-        self.circle = (msg.buttons[1] == 1)
-        self.triangle = (msg.buttons[2] == 1)
-        self.square = (msg.buttons[3] == 1)
-        self.L1 = (msg.buttons[4] == 1)
-        self.R1 = (msg.buttons[5] == 1)
-        self.L2 = (msg.buttons[6] == 1)
-        self.R2 = (msg.buttons[7] == 1)
-        self.select = (msg.buttons[8] == 1)
-        self.start = (msg.buttons[9] == 1)
-        self.center = (msg.buttons[10] == 1)
+        self.cross = msg.buttons[0] == 1
+        self.circle = msg.buttons[1] == 1
+        self.triangle = msg.buttons[2] == 1
+        self.square = msg.buttons[3] == 1
+        self.L1 = msg.buttons[4] == 1
+        self.R1 = msg.buttons[5] == 1
+        self.L2 = msg.buttons[6] == 1
+        self.R2 = msg.buttons[7] == 1
+        self.select = msg.buttons[8] == 1
+        self.start = msg.buttons[9] == 1
+        self.center = msg.buttons[10] == 1
         self.left_analog_x = msg.axes[0]
         self.left_analog_y = msg.axes[1]
         self.right_analog_x = msg.axes[3]
@@ -145,23 +145,23 @@ class PS3Status(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
         # creating from sensor_msgs/Joy
-        self.center = (msg.buttons[16] == 1)
-        self.select = (msg.buttons[0] == 1)
-        self.start = (msg.buttons[3] == 1)
-        self.L3 = (msg.buttons[1] == 1)
-        self.R3 = (msg.buttons[2] == 1)
-        self.square = (msg.axes[15] < 0)
-        self.up = (msg.axes[4] < 0)
-        self.down = (msg.axes[6] < 0)
-        self.left = (msg.axes[7] < 0)
-        self.right = (msg.axes[5] < 0)
-        self.triangle = (msg.axes[12] < 0)
-        self.cross = (msg.axes[14] < 0)
-        self.circle = (msg.axes[13] < 0)
-        self.L1 = (msg.axes[10] < 0)
-        self.R1 = (msg.axes[11] < 0)
-        self.L2 = (msg.axes[8] < 0)
-        self.R2 = (msg.axes[9] < 0)
+        self.center = msg.buttons[16] == 1
+        self.select = msg.buttons[0] == 1
+        self.start = msg.buttons[3] == 1
+        self.L3 = msg.buttons[1] == 1
+        self.R3 = msg.buttons[2] == 1
+        self.square = msg.axes[15] < 0
+        self.up = msg.axes[4] < 0
+        self.down = msg.axes[6] < 0
+        self.left = msg.axes[7] < 0
+        self.right = msg.axes[5] < 0
+        self.triangle = msg.axes[12] < 0
+        self.cross = msg.axes[14] < 0
+        self.circle = msg.axes[13] < 0
+        self.L1 = msg.axes[10] < 0
+        self.R1 = msg.axes[11] < 0
+        self.L2 = msg.axes[8] < 0
+        self.R2 = msg.axes[9] < 0
         self.left_analog_x = msg.axes[0]
         self.left_analog_y = msg.axes[1]
         self.right_analog_x = msg.axes[2]
@@ -173,23 +173,23 @@ class PS3WiredStatus(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
         # creating from sensor_msgs/Joy
-        self.center = (msg.buttons[16] == 1)
-        self.select = (msg.buttons[0] == 1)
-        self.start = (msg.buttons[3] == 1)
-        self.L3 = (msg.buttons[1] == 1)
-        self.R3 = (msg.buttons[2] == 1)
-        self.square = (msg.buttons[15] == 1)
-        self.up = (msg.buttons[4] == 1)
-        self.down = (msg.buttons[6] == 1)
-        self.left = (msg.buttons[7] == 1)
-        self.right = (msg.buttons[5] == 1)
-        self.triangle = (msg.buttons[12] == 1)
-        self.cross = (msg.buttons[14] == 1)
-        self.circle = (msg.buttons[13] == 1)
-        self.L1 = (msg.buttons[10] == 1)
-        self.R1 = (msg.buttons[11] == 1)
-        self.L2 = (msg.buttons[8] == 1)
-        self.R2 = (msg.buttons[9] == 1)
+        self.center = msg.buttons[16] == 1
+        self.select = msg.buttons[0] == 1
+        self.start = msg.buttons[3] == 1
+        self.L3 = msg.buttons[1] == 1
+        self.R3 = msg.buttons[2] == 1
+        self.square = msg.buttons[15] == 1
+        self.up = msg.buttons[4] == 1
+        self.down = msg.buttons[6] == 1
+        self.left = msg.buttons[7] == 1
+        self.right = msg.buttons[5] == 1
+        self.triangle = msg.buttons[12] == 1
+        self.cross = msg.buttons[14] == 1
+        self.circle = msg.buttons[13] == 1
+        self.L1 = msg.buttons[10] == 1
+        self.R1 = msg.buttons[11] == 1
+        self.L2 = msg.buttons[8] == 1
+        self.R2 = msg.buttons[9] == 1
         self.left_analog_x = msg.axes[0]
         self.left_analog_y = msg.axes[1]
         self.right_analog_x = msg.axes[2]
@@ -201,23 +201,23 @@ class PS4Status(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
         # creating from sensor_msg/Joy
-        self.center = (msg.buttons[12] == 1)
-        self.select = (msg.buttons[8] == 1)
-        self.start = (msg.buttons[9] == 1)
-        self.L3 = (msg.buttons[10] == 1)
-        self.R3 = (msg.buttons[11] == 1)
-        self.square = (msg.buttons[0] == 1)
-        self.up = (msg.axes[10] < 0)
-        self.down = (msg.axes[10] > 0)
-        self.left = (msg.axes[9] < 0)
-        self.right = (msg.axes[9] > 0)
-        self.triangle = (msg.buttons[3] == 1)
-        self.cross = (msg.buttons[1] == 1)
-        self.circle = (msg.buttons[2] == 1)
-        self.L1 = (msg.buttons[4] == 1)
-        self.R1 = (msg.buttons[5] == 1)
-        self.L2 = (msg.buttons[6] == 1)
-        self.R2 = (msg.buttons[7] == 1)
+        self.center = msg.buttons[12] == 1
+        self.select = msg.buttons[8] == 1
+        self.start = msg.buttons[9] == 1
+        self.L3 = msg.buttons[10] == 1
+        self.R3 = msg.buttons[11] == 1
+        self.square = msg.buttons[0] == 1
+        self.up = msg.axes[10] < 0
+        self.down = msg.axes[10] > 0
+        self.left = msg.axes[9] < 0
+        self.right = msg.axes[9] > 0
+        self.triangle = msg.buttons[3] == 1
+        self.cross = msg.buttons[1] == 1
+        self.circle = msg.buttons[2] == 1
+        self.L1 = msg.buttons[4] == 1
+        self.R1 = msg.buttons[5] == 1
+        self.L2 = msg.buttons[6] == 1
+        self.R2 = msg.buttons[7] == 1
         self.left_analog_x = msg.axes[0]
         self.left_analog_y = msg.axes[1]
         self.right_analog_x = msg.axes[5]
@@ -229,23 +229,23 @@ class PS4WiredStatus(JoyStatus):
     def __init__(self, msg):
         JoyStatus.__init__(self)
         # creating from sensor_msg/Joy
-        self.center = (msg.buttons[10] == 1)
-        self.select = (msg.buttons[8] == 1)
-        self.start = (msg.buttons[9] == 1)
-        self.L3 = (msg.buttons[11] == 1)
-        self.R3 = (msg.buttons[12] == 1)
-        self.square = (msg.buttons[3] == 1)
-        self.up = (msg.axes[7] < 0)
-        self.down = (msg.axes[7] > 0)
-        self.left = (msg.axes[6] < 0)
-        self.right = (msg.axes[6] > 0)
-        self.triangle = (msg.buttons[2] == 1)
-        self.cross = (msg.buttons[0] == 1)
-        self.circle = (msg.buttons[1] == 1)
-        self.L1 = (msg.buttons[4] == 1)
-        self.R1 = (msg.buttons[5] == 1)
-        self.L2 = (msg.buttons[6] == 1)
-        self.R2 = (msg.buttons[7] == 1)
+        self.center = msg.buttons[10] == 1
+        self.select = msg.buttons[8] == 1
+        self.start = msg.buttons[9] == 1
+        self.L3 = msg.buttons[11] == 1
+        self.R3 = msg.buttons[12] == 1
+        self.square = msg.buttons[3] == 1
+        self.up = msg.axes[7] < 0
+        self.down = msg.axes[7] > 0
+        self.left = msg.axes[6] < 0
+        self.right = msg.axes[6] > 0
+        self.triangle = msg.buttons[2] == 1
+        self.cross = msg.buttons[0] == 1
+        self.circle = msg.buttons[1] == 1
+        self.L1 = msg.buttons[4] == 1
+        self.R1 = msg.buttons[5] == 1
+        self.L2 = msg.buttons[6] == 1
+        self.R2 = msg.buttons[7] == 1
         self.left_analog_x = msg.axes[0]
         self.left_analog_y = msg.axes[1]
         self.right_analog_x = msg.axes[3]
