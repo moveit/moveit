e4b118eecd794aa09a695540aabf81a94e9a2961
Andy Zelenak <zelenak@picknik.ai>
2022-12-21 10:16:00 -0600

    Try a longer MAX_DURATION_EXTENSION_FACTOR

--- moveit_core/trajectory_processing/src/ruckig_traj_smoothing.cpp
+++ moveit_core/trajectory_processing/src/ruckig_traj_smoothing.cpp
@@ -48,7 +48,7 @@ const std::string LOGNAME = "moveit_trajectory_processing.ruckig_traj_smoothing"
 constexpr double DEFAULT_MAX_VELOCITY = 5;       // rad/s
 constexpr double DEFAULT_MAX_ACCELERATION = 10;  // rad/s^2
 constexpr double DEFAULT_MAX_JERK = 100;         // rad/s^3
-constexpr double MAX_DURATION_EXTENSION_FACTOR = 10.0;
+constexpr double MAX_DURATION_EXTENSION_FACTOR = 50.0;
 constexpr double DURATION_EXTENSION_FRACTION = 1.1;
 }  // namespace
 
