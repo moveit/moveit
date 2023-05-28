9475dfb24d8a342c7781f99bdbf49b4efa72d880
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2023-05-28 12:03:31 +0200

    Simplify test: no need for a ROS environment

--- moveit_core/robot_state/test/test_planar_joint_jacobian.cpp
+++ moveit_core/robot_state/test/test_planar_joint_jacobian.cpp
@@ -98,6 +98,5 @@ TEST_F(SimplePlanarRobot, testSimplePlanarRobot)
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "test_planar_joint_jacobian");
   return RUN_ALL_TESTS();
 }
